fb68388ff56e4ed183f8467f10fde33b8f7ca31a
Dan Royer <dan@marginallyclever.com>
2015-09-17 08:57:13 -0700

    fixed Doxygen

--- java/src/com/marginallyclever/evilOverlord/EvilOverlord.java
+++ java/src/com/marginallyclever/evilOverlord/EvilOverlord.java
@@ -1,5 +1,27 @@
 package com.marginallyclever.evilOverlord;
 
+/** \mainpage Evil Overlord
+ * \section intro_sec Introduction
+ * Evil Overloard is a GUI for controlling Marginally Clever "Evil Minion" robot arms.
+ * See https://marginallyclever.com/ for more details.
+ * \section install_sec Install instructions for developers
+ * \step 
+ * download the code from http://github.com/imakerobots/Evil-Overlord
+ * \step
+ * download and install Eclipse
+ * \step
+ * in Eclipse, add the project.
+ * \step
+ * in Eclipse, right click on the project and choose Maven > Update Project
+ * \step
+ * you should now be able to build and run the program
+ */
+
+/**
+ * main() entry point into the application.
+ * @author danroyer
+ *
+ */
 public class EvilOverlord {
 	public static MainGUI gui;
 	
