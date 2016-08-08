4ca4839a3ec07e0a8abda9164b3dd1e3a7959748
Dan Royer <dan@marginallyclever.com>
2016-08-08 10:04:30 -0700

    adjusted uArm dimensions for release 1.4.0

--- src/main/java/com/marginallyclever/robotOverlord/arm3/Arm3ControlPanel.java
+++ src/main/java/com/marginallyclever/robotOverlord/arm3/Arm3ControlPanel.java
@@ -8,7 +8,6 @@ import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JLabel;
-import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JSlider;
 import javax.swing.event.ChangeEvent;
