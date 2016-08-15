7b88eed1db80e1e420cb3cc8442c0751fc66e53c
Dan Royer <dan@marginallyclever.com>
2016-08-14 20:18:08 -0700

    Added uArm model parts (Thanks, Marc Andre Gray!)

--- src/main/java/com/marginallyclever/robotOverlord/ModelInWorld.java
+++ src/main/java/com/marginallyclever/robotOverlord/ModelInWorld.java
@@ -76,14 +76,15 @@ public class ModelInWorld extends Entity {
 			//gl2.glScaled(scaleX, scaleY, scaleZ);
 			if( model==null ) {
 				// draw placeholder
+				final float size=10;
 				gl2.glColor3f(1, 0, 0);
-				PrimitiveSolids.drawBox(gl2,  10.0f, 0.1f, 0.1f);
+				PrimitiveSolids.drawBox(gl2,  size, 0.1f, 0.1f);
 				gl2.glColor3f(0, 1, 0);
-				PrimitiveSolids.drawBox(gl2,   0.1f,10.0f, 0.1f);
+				PrimitiveSolids.drawBox(gl2,   0.1f,size, 0.1f);
 				gl2.glColor3f(0, 0, 1);
 				gl2.glPushMatrix();
-				gl2.glTranslatef(0, 0, -5);
-				PrimitiveSolids.drawBox(gl2,   0.1f, 0.1f,10.0f);
+				gl2.glTranslatef(0, 0, -size/2);
+				PrimitiveSolids.drawBox(gl2,   0.1f, 0.1f,size);
 				gl2.glPopMatrix();
 			} else {
 				material.render(gl2);
