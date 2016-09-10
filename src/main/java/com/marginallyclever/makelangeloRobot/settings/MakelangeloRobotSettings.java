80cd0577115bcb9570a9943b907a0f123a495ff9
Dan Royer <dan@marginallyclever.com>
2016-09-09 21:00:35 -0700

    fixed naming conflict in setHardwareVersion()

--- src/main/java/com/marginallyclever/makelangeloRobot/settings/MakelangeloRobotSettings.java
+++ src/main/java/com/marginallyclever/makelangeloRobot/settings/MakelangeloRobotSettings.java
@@ -658,7 +658,7 @@ public final class MakelangeloRobotSettings {
 		return hardwareProperties;
 	}
 
-	public void setHardwareVersion(int hardwareVersion) {
+	public void setHardwareVersion(int version) {
 		int newVersion = -1;
 
 		try {
@@ -667,9 +667,9 @@ public final class MakelangeloRobotSettings {
 			Iterator<MakelangeloHardwareProperties> i = knownHardware.iterator();
 			while(i.hasNext()) {
 				MakelangeloHardwareProperties hw = i.next();
-				if(hw.getVersion()==hardwareVersion) {
+				if(hw.getVersion()==version) {
 					hardwareProperties = hw.getClass().newInstance();
-					newVersion = hardwareVersion;
+					newVersion = version;
 				};
 			}
 		} catch(Exception e) {
@@ -683,8 +683,12 @@ public final class MakelangeloRobotSettings {
 			hardwareProperties = new Makelangelo2Properties();
 		}
 		
+		hardwareVersion = newVersion;
+		
 		if(!hardwareProperties.canChangeMachineSize()) {
 			this.setMachineSize(hardwareProperties.getWidth()*0.1f, hardwareProperties.getHeight()*0.1f);
 		}
+		
+		saveConfigToLocal();
 	}
 }
