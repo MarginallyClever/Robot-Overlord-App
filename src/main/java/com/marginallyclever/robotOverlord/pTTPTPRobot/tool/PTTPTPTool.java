11654cf06a913954bc6a0b0ee82ec906176497b0
Dan Royer <dan@marginallyclever.com>
2017-11-18 16:18:50 -0800

    Added PTTPTP robot arm

---
+++ src/main/java/com/marginallyclever/robotOverlord/pTTPTPRobot/tool/PTTPTPTool.java
@@ -0,0 +1,44 @@
+package com.marginallyclever.robotOverlord.pTTPTPRobot.tool;
+
+import com.jogamp.opengl.GL2;
+import com.marginallyclever.robotOverlord.PhysicalObject;
+import com.marginallyclever.robotOverlord.model.Model;
+import com.marginallyclever.robotOverlord.model.ModelFactory;
+import com.marginallyclever.robotOverlord.pTTPTPRobot.PTTPTPRobot;
+
+public abstract class PTTPTPTool extends PhysicalObject {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -5418173275880663460L;
+	protected Model visibleShape = null;
+	protected String shapeFile = null;
+	protected PTTPTPRobot attachedTo=null;
+
+		
+	public void attachTo(PTTPTPRobot robot) {
+		attachedTo=robot;
+	}
+	
+	public PTTPTPRobot getAttachedTo() {
+		return attachedTo;
+	}
+	
+	public void render(GL2 gl2) {
+		if( visibleShape==null && shapeFile!=null ) {
+			try {
+				visibleShape = ModelFactory.createModelFromFilename(shapeFile);
+			} catch(Exception e) {
+				e.printStackTrace();
+			}
+		}
+		if(!visibleShape.isLoaded()) return;
+
+		material.render(gl2);
+		visibleShape.render(gl2);
+	}
+	
+	public void updateGUI() {}
+	
+	public void update(float dt) {}
+}
