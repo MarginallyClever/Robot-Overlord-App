cdfc267fac6421072cfbbe50356fdb83773f1bed
Dan Royer <dan@marginallyclever.com>
2019-03-15 15:15:50 -0700

    Added new features to RobotKeyframe interface

--- src/main/java/com/marginallyclever/robotOverlord/robot/RobotKeyframe.java
+++ src/main/java/com/marginallyclever/robotOverlord/robot/RobotKeyframe.java
@@ -2,10 +2,31 @@ package com.marginallyclever.robotOverlord.robot;
 
 import java.io.Serializable;
 
+import com.jogamp.opengl.GL2;
+
 /**
  * A RobotMotionState is a snapshot of a robot at a moment in time.
  * @author Dan Royer
  *
  */
 public interface RobotKeyframe extends Serializable {
+	
+	/**
+	 * Fill this instance with the interpolated value of (b-a)*t+a, where t={0..1}.  
+	 *  
+	 * @param a The starting at t=0.
+	 * @param b The keyframe at t=1.
+	 * @param t {0..1}
+	 */
+	public void interpolate(RobotKeyframe a,RobotKeyframe b,float t);
+	
+	/**
+	 * visualize this keyframe
+	 */
+	public void render(GL2 gl2);
+	
+	/**
+	 * visualize the change between two keyframes
+	 */
+	static public void renderInterpolation(GL2 gl2,RobotKeyframe a,RobotKeyframe b) {}
 }
