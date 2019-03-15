e2cb69f410b48d4015420ec85ffeb1a1697db476
Dan Royer <dan@marginallyclever.com>
2019-03-15 15:14:30 -0700

    new

--- src/main/java/com/marginallyclever/robotOverlord/lines/Bezier3.java
+++ src/main/java/com/marginallyclever/robotOverlord/lines/Bezier3.java
@@ -3,17 +3,20 @@ package com.marginallyclever.robotOverlord.lines;
 import javax.vecmath.Vector3f;
 
 /**
- * 3D Bezier curve implementation
+ * 3D Bezier curve implementation.  All points are in the same space - p1 and p2 are not relative to p0 and p3, respectively.
  * @author Dan Royer
  * @see https://en.wikipedia.org/wiki/B%C3%A9zier_curve
  *
  */
-public class LineBezier {
-	public Vector3f p0,p1,p2,p3;
+public class Bezier3 {
+	public Vector3f p0 = new Vector3f();
+	public Vector3f p1 = new Vector3f();
+	public Vector3f p2 = new Vector3f();
+	public Vector3f p3 = new Vector3f();
 	
 	
 	/**
-	 * interpolate along the path
+	 * interpolate along the path.  This code is slow, intuitive, and it works.
 	 * @param i 0...1 inclusive
 	 * @return the interpolated value along the path.
 	 */
@@ -37,10 +40,10 @@ public class LineBezier {
 	 * @return
 	 */
 	protected Vector3f interpolate(Vector3f a,Vector3f b,float i) {
-		Vector3f c = new Vector3f(a);
-		c.sub(b);
+		Vector3f c = new Vector3f(b);
+		c.sub(a);
 		c.scale(i);
-		c.add(b);
+		c.add(a);
 		return c;
 	}
 }
