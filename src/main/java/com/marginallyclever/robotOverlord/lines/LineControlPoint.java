f5c895b80a4074d9341d058e3eff1ad19045039a
Dan Royer <dan@marginallyclever.com>
2019-03-15 15:17:44 -0700

    fix uninitialized variable

--- src/main/java/com/marginallyclever/robotOverlord/lines/LineControlPoint.java
+++ src/main/java/com/marginallyclever/robotOverlord/lines/LineControlPoint.java
@@ -13,7 +13,7 @@ import com.marginallyclever.convenience.MatrixHelper;
  *
  */
 public class LineControlPoint {
-	public LineBezier position;
+	public Bezier3 position = new Bezier3();
 	//public LineBezier4 angle;
 	
 	//Quaternion q = new Quaternion;
@@ -22,18 +22,36 @@ public class LineControlPoint {
 	 * visualize the line in opengl
 	 * @param gl2
 	 */
-	void render(GL2 gl2) {
+	public void render(GL2 gl2) {
 		Vector3f u,v,w;
 		
 		//MatrixHelper.drawMatrix(gl2, position.interpolate(0), u, v, w);
 		//MatrixHelper.drawMatrix(gl2, position.interpolate(1), u, v, w);
+
+		gl2.glDisable(gl2.GL_LIGHTING);
+		gl2.glDisable(GL2.GL_COLOR_MATERIAL);
 		
+		/*
+		gl2.glColor4f(0, 0, 1, 1);
 		gl2.glBegin(GL2.GL_LINES);
-		final float NUM_STEPS=10;
+		gl2.glVertex3f(position.p0.x,position.p0.y,position.p0.z);
+		gl2.glVertex3f(position.p1.x,position.p1.y,position.p1.z);
+		
+		gl2.glVertex3f(position.p2.x,position.p2.y,position.p2.z);
+		gl2.glVertex3f(position.p3.x,position.p3.y,position.p3.z);
+		gl2.glEnd();
+		*/
+		
+		gl2.glColor4f(0, 1, 0, 1);
+		gl2.glBegin(GL2.GL_LINE_STRIP);
+		final float NUM_STEPS=20;
 		for(float i=0;i<=NUM_STEPS;++i) {
 			Vector3f ipos = position.interpolate(i/NUM_STEPS);
 			gl2.glVertex3f(ipos.x,ipos.y,ipos.z);
 		}
 		gl2.glEnd();
+		
+		gl2.glEnable(gl2.GL_LIGHTING);
+		gl2.glEnable(GL2.GL_COLOR_MATERIAL);
 	}
 }
