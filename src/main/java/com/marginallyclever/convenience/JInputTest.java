fe51ba1dfd417f76defa69f1a8132050d2fe4db0
Dan Royer <dan@marginallyclever.com>
2019-03-25 16:05:57 -0700

    Create JInputTest.java

---
+++ src/main/java/com/marginallyclever/convenience/JInputTest.java
@@ -0,0 +1,29 @@
+package com.marginallyclever.convenience;
+
+import org.junit.Test;
+
+import net.java.games.input.Component;
+import net.java.games.input.Controller;
+import net.java.games.input.ControllerEnvironment;
+
+public class JInputTest {
+	@Test
+	public void testControllers() {
+		Controller[] ca = ControllerEnvironment.getDefaultEnvironment().getControllers();
+        for(int i =0;i<ca.length;i++){
+            System.out.println("Controller:"+ca[i].getName());
+            System.out.println("Type: "+ca[i].getType().toString());
+
+            // Get this controllers components (buttons and axis)
+            Component[] components = ca[i].getComponents();
+            System.out.println("Component Count: "+components.length);
+            for(int j=0;j<components.length;j++){
+                System.out.println("	Component "+j+": "+components[j].getName());
+                System.out.println("    Identifier: "+ components[j].getIdentifier().getName());
+                System.out.print("    Type: ");
+                System.out.print(components[j].isRelative() ? "Relative" : "Absolute");
+                System.out.print(components[j].isAnalog()? "Analog" : "Digital");
+            }
+        }
+	}
+}
