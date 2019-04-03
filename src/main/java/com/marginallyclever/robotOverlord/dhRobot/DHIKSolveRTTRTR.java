6078cd6a79663d5e012947318334940cf2ffebe3
Dan Royer <dan@marginallyclever.com>
2019-04-02 18:09:00 -0700

    moved rendering from DHRobot to DHRSixi2

--- src/main/java/com/marginallyclever/robotOverlord/dhRobot/DHIKSolveRTTRTR.java
+++ src/main/java/com/marginallyclever/robotOverlord/dhRobot/DHIKSolveRTTRTR.java
@@ -1,5 +1,8 @@
 package com.marginallyclever.robotOverlord.dhRobot;
 
+import java.util.Iterator;
+
+import javax.vecmath.Matrix4d;
 import javax.vecmath.Point3d;
 import javax.vecmath.Vector3d;
 
@@ -11,10 +14,20 @@ public class DHIKSolveRTTRTR {
 	public double theta0;
 	public double alpha1;
 	public double alpha2;
-	// joint 3 is a dummy joint to draw the skeleton correctly.
+	// link 3 is a dummy to draw the skeleton correctly.
 	public double theta4;
 	public double alpha5;
 	public double theta6;
+	// link 7 is the final output that we started with.
+
+	/**
+	 * {@value #solutionFlag} Can be either NO_SOLUTIONS, ONE_SOLUTION, or MANY_SOLUTIONS.
+	 */
+	public int solutionFlag;
+	public static final int NO_SOLUTIONS=0;
+	public static final int ONE_SOLUTION=1;
+	public static final int MANY_SOLUTIONS=2;
+	
 	
 	public DHIKSolveRTTRTR() {}
 	
@@ -24,6 +37,8 @@ public class DHIKSolveRTTRTR {
 	 */
 	@SuppressWarnings("unused")
 	public void solve(DHRobot robot) {
+		solutionFlag = ONE_SOLUTION;
+		
 		DHLink link0 = robot.links.get(0);
 		DHLink link1 = robot.links.get(1);
 		DHLink link2 = robot.links.get(2);
@@ -35,7 +50,7 @@ public class DHIKSolveRTTRTR {
 
 		//link7.poseCumulative.set(robot.endMatrix);
 		
-		Vector3d p7 = new Vector3d(
+		Point3d p7 = new Point3d(
 				link7.poseCumulative.m03,
 				link7.poseCumulative.m13,
 				link7.poseCumulative.m23);
@@ -91,7 +106,12 @@ public class DHIKSolveRTTRTR {
 		double b1 = link3.d;
 		double b = Math.sqrt(b2*b2+b1*b1);
 		if(false) System.out.println("b="+b+"\t");
-		
+
+		if( e > a+b ) {
+			solutionFlag = NO_SOLUTIONS;
+			if(true) System.out.println("NO_SOLUTIONS (1) "+e+" vs "+(a+b));
+			return;
+		}
 		double phi = Math.acos( (b*b-a*a-e*e) / (-2*a*e) );
 		if(true) System.out.println("phi="+Math.toDegrees(phi)+"\t");
 		
@@ -105,63 +125,77 @@ public class DHIKSolveRTTRTR {
 		
 		// (8) omega = acos( (a^2-b^2-e^2) / (-2be) )
 		double omega = Math.acos( (a*a-b*b-e*e) / (-2*b*e) );
-		if(true) System.out.println("omega="+Math.toDegrees(omega)+"\t");
+		if(false) System.out.println("omega="+Math.toDegrees(omega)+"\t");
 
 		// (9) phi3 = phi + omega
 		double phi3 = phi+omega;
-		if(true) System.out.println("phi3="+Math.toDegrees(phi3)+"\t");
+		if(false) System.out.println("phi3="+Math.toDegrees(phi3)+"\t");
 				
 		// angle of triangle j3-j2-j5 is ph4.
 		// b2^2 = b^+b1^2-2*b*b1*cos(phi4)
 		double phi4 = Math.acos( (b2*b2-b1*b1-b*b) / (-2*b1*b) );
-		if(true) System.out.println("phi4="+Math.toDegrees(phi4)+"\t");
+		if(false) System.out.println("phi4="+Math.toDegrees(phi4)+"\t");
 		
 		// (10) alpha2 - phi3-phi4
 		alpha2 = Math.toDegrees(phi3 - phi4);
-		if(true) System.out.println("alpha2="+alpha2+"\t");
+		if(false) System.out.println("alpha2="+alpha2+"\t");
 		
-		/*
-		//(1) theta0 = tan(x05/y05)
-		double x05 = p5.x - p0.x;
-		double y05 = p5.y - p0.y;
-		theta0 = Math.toDegrees(Math.atan2(x05,-y05));  // TODO explain why this is -y05 instead of y05
-		//System.out.println("t0="+theta0+"\t");
+		// FIRST HALF DONE
 		
-		//(2) A = sqrt(x12^2 + y12^2)
-		double x12 = p2.x - p1.x;
-		double y12 = p2.y - p1.y;
-		double A = Math.sqrt(x12*x12 + y12*y12);
-		// Correct sign of A with dot product
-		double dot = x05*x12 + y05*x12;
-		if(dot<0) A=-A;
-		
-		//(3) B = z12
-		double B = p2.z-p1.z;
+		// Now to a partial DHRobot.poseRefresh() to find several joint poses.
+		link0.theta = theta0; 	link0.refreshPoseMatrix();
+		link1.alpha = alpha1;	link1.refreshPoseMatrix();
+		link2.alpha = alpha2;	link2.refreshPoseMatrix();
+		link3.refreshPoseMatrix();
 
-		//(4)
-		alpha1 = Math.toDegrees(Math.atan2(A,B));
-		//System.out.println("A="+A+"\t"+"B="+B+"\t"+"a1="+alpha1+"\t");
-		
-		//(5) C = z23
-		double C = p3.z-p2.z; 
+		Matrix4d endMatrix = new Matrix4d();
+		endMatrix.setIdentity();
+		endMatrix.mul(link0.pose);		link0.poseCumulative.set(endMatrix);
+		endMatrix.mul(link1.pose);		link1.poseCumulative.set(endMatrix);
+		endMatrix.mul(link2.pose);		link2.poseCumulative.set(endMatrix);
+		endMatrix.mul(link3.pose);		link3.poseCumulative.set(endMatrix);
+		//endMatrix.mul(link4.pose);		link4.poseCumulative.set(endMatrix);
 
-		//(6) D = sqrt(x23^2 + y23^2)
-		double x23 = p3.x-p2.x;
-		double y23 = p3.y-p2.y;
-		double D = Math.sqrt(x23*x23 + y23*y23);
-		// Correct sign of D
-		dot = x05*x23 + y05*x23;
-		if(dot<0) D=-D;
+		// endMatrix is now at j4, but the rotation is unknown.
+		Point3d p4 = new Point3d(0,0,0);
+		endMatrix.transform(p4);
 		
-		//(7) phi1 = atan(C,D)
-		double phi1 = Math.toDegrees(Math.atan2(D,C));
+		// test to see if we are near the singularity (when j6-j4=j4.d+j5.d+j6.d)
+		double f = link5.d;  // aka z45
+		double g = link6.d+link7.d;  // aka z57
+		double maximumReach = f+g;
+		double h = p4.distance(p7);
 
-		//(8) alpha2 = alpha1 - phi1
-		alpha2 = phi1-alpha1;	// TODO why is this backwards?
-		//System.out.println("C="+C+"\t"+"D="+D+"\t"+"p1="+phi1+"\t"+"a1="+alpha1+"\t"+"a2="+alpha2+"\t");
+		if(true) System.out.println("p7="+p7+"\t");
+		if(true) System.out.println("p5="+p5+"\t");
+		if(true) System.out.println("p4="+p4+"\t");
+		if(true) System.out.println("f="+f+"\t");
+		if(true) System.out.println("g="+g+"\t");
+		if(true) System.out.println("h="+h+"\t");
 		
-		System.out.println("t0="+formatDouble(theta0)+"\ta1="+formatDouble(alpha1)+"\ta2="+formatDouble(alpha2)+"\t");
-		*/
+		if( h>maximumReach ) {
+			// out of reach
+			solutionFlag = NO_SOLUTIONS;
+			if(true) System.out.println("NO_SOLUTIONS (2) "+h+" vs "+maximumReach);
+			theta4=alpha5=theta6=0;
+			return;
+		}
+		if( Math.abs(h-maximumReach) < 0.00001 ) {
+			// singularity
+			solutionFlag = MANY_SOLUTIONS;
+			if(true) System.out.println("MANY_SOLUTIONS");
+			theta4=alpha5=theta6=0;
+			return;
+		}
+		
+		// So alpha5 is not 0, which means we can calculate theta4
+		// Triangle p4-p5-p7 gives us phi6
+		// h^2 = f^2+g^2 +2fg * cos(phi6)
+		double phi6 = Math.acos( (h*h-f*f-g*g) / (-2*f*g) );
+		theta4 = 180 - Math.toDegrees(phi6);
+		if(true) System.out.println("t4="+theta4+"\t");
+		
+		//System.out.println("t0="+formatDouble(theta0)+"\ta1="+formatDouble(alpha1)+"\ta2="+formatDouble(alpha2)+"\t");
 	}
 	
 	/**
@@ -230,7 +264,8 @@ public class DHIKSolveRTTRTR {
 		alpha2 = phi1-alpha1;	// TODO why is this backwards?
 		//System.out.println("C="+C+"\t"+"D="+D+"\t"+"p1="+phi1+"\t"+"a1="+alpha1+"\t"+"a2="+alpha2+"\t");
 		
-		System.out.println("t0="+formatDouble(theta0)+"\ta1="+formatDouble(alpha1)+"\ta2="+formatDouble(alpha2)+"\t");
+		
+		//System.out.println("t0="+formatDouble(theta0)+"\ta1="+formatDouble(alpha1)+"\ta2="+formatDouble(alpha2)+"\t");
 	}
 	
 	protected String formatDouble(double arg0) {
