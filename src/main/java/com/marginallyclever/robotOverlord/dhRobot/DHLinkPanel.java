e42e0d3364e1d16710a196637243dca73654ab16
Dan Royer <dan@marginallyclever.com>
2019-04-02 18:09:10 -0700

    added comments

--- src/main/java/com/marginallyclever/robotOverlord/dhRobot/DHLinkPanel.java
+++ src/main/java/com/marginallyclever/robotOverlord/dhRobot/DHLinkPanel.java
@@ -4,12 +4,40 @@ import com.marginallyclever.robotOverlord.RobotOverlord;
 import com.marginallyclever.robotOverlord.commands.UserCommandSelectBoolean;
 import com.marginallyclever.robotOverlord.commands.UserCommandSelectNumber;
 
+/**
+ * Subsection of the DHPanel dealing with a single link in the kinematic chain of a DHRobot.
+ * @author Dan Royer
+ *
+ */
 public class DHLinkPanel {
+	/**
+	 * {@value #link} the DHLink referenced by this DHLinkPanel
+	 */
 	public DHLink link;
+
+	/**
+	 * {@value #isRotation} true if this a rotary joint, false if this is a translation joint. 
+	 */
 	public UserCommandSelectBoolean isRotation;
+
+	/**
+	 * {@value #d} the displayed value for link.d
+	 */
 	public UserCommandSelectNumber d;
+
+	/**
+	 * {@value #theta} the displayed value for link.theta
+	 */
 	public UserCommandSelectNumber theta;
+
+	/**
+	 * {@value #r} the displayed value for link.r
+	 */
 	public UserCommandSelectNumber r;
+
+	/**
+	 * {@value #alpha} the displayed value for link.alpha
+	 */
 	public UserCommandSelectNumber alpha;
 	
 	public DHLinkPanel(RobotOverlord gui,DHLink link,int k) {
