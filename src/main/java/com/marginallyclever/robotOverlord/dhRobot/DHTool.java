2fea5c53739bceb5ed68635eceec2ee7ff437ec7
Dan Royer <dan@marginallyclever.com>
2019-04-07 12:13:03 -0700

    Attempt at DHTool work

--- src/main/java/com/marginallyclever/robotOverlord/dhRobot/DHTool.java
+++ src/main/java/com/marginallyclever/robotOverlord/dhRobot/DHTool.java
@@ -16,8 +16,20 @@ public class DHTool extends ModelInWorld {
 	 */
 	private static final long serialVersionUID = 1L;
 	
+	/**
+	 * A DHLink representation of this tool for kinematic solving.
+	 */
 	DHLink dhlinkEquivalent;
+	
+	/**
+	 * A PhysicalObject, if any, being held by the tool.  Assumes only one object can be held.
+	 */
 	PhysicalObject subjectBeingHeld;
+	
+	/**
+	 * Who, if anyone, is holding this tool?
+	 */
+	DHRobot heldBy;
 	
 	@Override
 	public void render(GL2 gl2) {
