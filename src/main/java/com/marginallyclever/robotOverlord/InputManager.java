1932acf0bcc4024e424537d0a1caba010fa81569
Dan Royer <dan@marginallyclever.com>
2019-04-08 18:11:13 -0700

    Create InputManager.java

---
+++ src/main/java/com/marginallyclever/robotOverlord/InputManager.java
@@ -0,0 +1,33 @@
+package com.marginallyclever.robotOverlord;
+
+import net.java.games.input.Component;
+import net.java.games.input.Controller;
+import net.java.games.input.ControllerEnvironment;
+
+public class InputManager {
+	static public void start() {
+
+		Controller[] ca = ControllerEnvironment.getDefaultEnvironment().getControllers();
+        for(int i =0;i<ca.length;i++){
+            System.out.print("Controller:"+ca[i].getName()+" ("+ca[i].getType().toString()+")");
+            if(ca[i].getType()==Controller.Type.UNKNOWN) continue;
+            
+            Component[] components = ca[i].getComponents();
+            for(int j=0;j<components.length;j++){
+            	System.out.println("\t"+components[j].getName()+
+            			":"+(components[j].isAnalog()?"Abs":"Rel")+
+            			":"+(components[j].isAnalog()?"An":"Di")
+            				);
+            }
+        }
+	}
+	
+	static public void update() {
+		Controller[] ca = ControllerEnvironment.getDefaultEnvironment().getControllers();
+        for(int i =0;i<ca.length;i++){
+        	if(!ca[i].poll()) {
+        		// 
+        	}
+        }
+	}
+}
