59c99ea47df9e29be454659cc9ab74a42e32bf27
Dan Royer <dan@marginallyclever.com>
2019-04-11 11:54:05 -0700

    moved drawTargetPose to DHRobot

--- src/main/java/com/marginallyclever/robotOverlord/dhRobot/DHRobot_Mantis.java
+++ src/main/java/com/marginallyclever/robotOverlord/dhRobot/DHRobot_Mantis.java
@@ -22,7 +22,6 @@ public class DHRobot_Mantis extends DHRobot {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	public Matrix4d targetPose;
 
 	public DHRobot_Mantis() {
 		super();
@@ -31,8 +30,6 @@ public class DHRobot_Mantis extends DHRobot {
 	
 	@Override
 	public void setupLinks() {
-		targetPose = new Matrix4d();
-		
 		setNumLinks(8);
 
 		// roll
@@ -102,7 +99,6 @@ public class DHRobot_Mantis extends DHRobot {
 		}
 		
 		this.refreshPose();
-		targetPose.set(endMatrix);
 	}
 	
 	@Override
@@ -140,44 +136,6 @@ public class DHRobot_Mantis extends DHRobot {
 					link.renderModel(gl2);
 				}
 			gl2.glPopMatrix();
-		
-			if(drawSkeleton) {
-				// draw targetPose
-				gl2.glPushMatrix();
-				
-				double[] mat = new double[16];
-				mat[ 0] = targetPose.m00;
-				mat[ 1] = targetPose.m10;
-				mat[ 2] = targetPose.m20;
-				mat[ 3] = targetPose.m30;
-				mat[ 4] = targetPose.m01;
-				mat[ 5] = targetPose.m11;
-				mat[ 6] = targetPose.m21;
-				mat[ 7] = targetPose.m31;
-				mat[ 8] = targetPose.m02;
-				mat[ 9] = targetPose.m12;
-				mat[10] = targetPose.m22;
-				mat[11] = targetPose.m32;
-				mat[12] = targetPose.m03;
-				mat[13] = targetPose.m13;
-				mat[14] = targetPose.m23;
-				mat[15] = targetPose.m33;
-				gl2.glMultMatrixd(mat, 0);
-		
-				boolean isDepth = gl2.glIsEnabled(GL2.GL_DEPTH_TEST);
-				boolean isLit = gl2.glIsEnabled(GL2.GL_LIGHTING);
-				gl2.glDisable(GL2.GL_DEPTH_TEST);
-				gl2.glDisable(GL2.GL_LIGHTING);
-				MatrixHelper.drawMatrix(gl2, 
-						new Vector3d(0,0,0),
-						new Vector3d(1,0,0),
-						new Vector3d(0,1,0),
-						new Vector3d(0,0,1));
-				if(isDepth) gl2.glEnable(GL2.GL_DEPTH_TEST);
-				if(isLit) gl2.glEnable(GL2.GL_LIGHTING);
-				gl2.glPopMatrix();
-			}
-
 		gl2.glPopMatrix();
 		
 		super.render(gl2);
