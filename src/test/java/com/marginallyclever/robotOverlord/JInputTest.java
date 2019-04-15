bb864b4a5c99874d7caa55405c40e431ceba19d0
Dan Royer <dan@marginallyclever.com>
2019-04-15 16:06:07 -0700

    added test to detect single button/axis move

--- src/test/java/com/marginallyclever/robotOverlord/JInputTest.java
+++ src/test/java/com/marginallyclever/robotOverlord/JInputTest.java
@@ -7,6 +7,9 @@ import net.java.games.input.Controller;
 import net.java.games.input.ControllerEnvironment;
 
 public class JInputTest {
+	/**
+	 * output the current java.library.path and the current working directory.
+	 */
 	@Test
 	public void reportJavaLibraryPath() {
 		String property = System.getProperty("java.library.path");
@@ -14,7 +17,10 @@ public class JInputTest {
 		
 		System.out.println("Working Directory = " + System.getProperty("user.dir"));
 	}
-	
+
+	/**
+	 * output all controllers, their components, and some details about those components.
+	 */
 	@Test
 	public void testControllers() {
 		Controller[] ca = ControllerEnvironment.getDefaultEnvironment().getControllers();
@@ -33,5 +39,86 @@ public class JInputTest {
                 System.out.println(")");
             }
         }
+	}
+	
+
+	/**
+	 * Figure out which input just changed.  Requires human input.
+	 */
+	@Test
+	public void detectSingleInputChange() {
+		int i,j,k;
+		
+		while(true) {
+			// count all devices and components
+			int totalComponents=0;
+			Controller[] ca = ControllerEnvironment.getDefaultEnvironment().getControllers();
+			int totalControllers = ca.length;
+	        for(i=0;i<totalControllers;i++) {
+	        	totalComponents += ca[i].getComponents().length;
+	        }
+	        float [] stateOld = new float[totalComponents];
+	        float [] stateNew = new float[totalComponents];
+	
+	        // get initial state
+	        InputManager.update();
+	        k=0;
+	        for(i=0;i<totalControllers;i++) {
+	            Component[] components = ca[i].getComponents();
+	            for(j=0;j<components.length;j++) {
+	            	stateOld[k] = components[j].getPollData();
+	            	k++;
+	            }
+	        }
+	
+	        while(true) {
+	        	// get the latest state
+	            InputManager.update();
+	            ca = ControllerEnvironment.getDefaultEnvironment().getControllers();
+	            if(ca.length != totalControllers) {
+	            	// uh oh!  devide added/removed!  restart!
+	            	break;
+	            }
+	            k=0;
+	            for(i=0;i<totalControllers;i++) {
+	                Component[] components = ca[i].getComponents();
+	                for(j=0;j<components.length;j++) {
+	                	stateNew[k] = components[j].getPollData();
+	                	float diff = stateNew[k]-stateOld[k];
+	                	if(Math.abs(diff)>0.5) {
+	                		// change to state
+	                		System.out.println("Found "+ca[i].getName()+"."+components[j].getName()+"="+diff);
+	                		//return;
+	                	}
+	                	k++;
+	                }
+	            }
+	        }
+		}
+	}
+	
+	/**
+	 * Figure out which input just changed.  Requires human input.
+	 */
+	@Test
+	public void detectSingleInputChangeB() {
+		int i,j;
+		
+        while(true) {
+        	// get the latest state
+            InputManager.update();
+			Controller[] ca = ControllerEnvironment.getDefaultEnvironment().getControllers();
+            for(i=0;i<ca.length;i++) {
+                Component[] components = ca[i].getComponents();
+                for(j=0;j<components.length;j++) {
+                	float diff = components[j].getPollData();
+                	if(diff>0.5) {
+                		// change to state
+                		System.out.println("Found "+ca[i].getName()+"."+components[j].getName()+"="+diff);
+                		//return;
+                	}
+                }
+            }
+		}
 	}
 }
