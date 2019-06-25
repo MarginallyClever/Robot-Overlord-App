39c9a74ae79eef77df9261eb6c176d2752604956
Dan Royer <dan@marginallyclever.com>
2019-06-25 10:05:32 -0700

    added load and orient tool correctly

--- src/main/java/com/marginallyclever/robotOverlord/dhRobot/DHTool_GoProCamera.java
+++ src/main/java/com/marginallyclever/robotOverlord/dhRobot/DHTool_GoProCamera.java
@@ -1,5 +1,8 @@
 package com.marginallyclever.robotOverlord.dhRobot;
 
+import javax.vecmath.Matrix4d;
+import javax.vecmath.Vector3d;
+
 import com.jogamp.opengl.GL2;
 
 /**
@@ -14,18 +17,29 @@ public class DHTool_GoProCamera extends DHTool {
 	 */
 	private static final long serialVersionUID = 1L;
 	
-	
 	public DHTool_GoProCamera() {
 		super();
 		dhLinkEquivalent.d=8;  // cm
 		dhLinkEquivalent.r=50;  // cm
 		dhLinkEquivalent.refreshPoseMatrix();
 		setDisplayName("GoPro Camera");
+		
+		setFilename("/gopro.stl");
+		setScale(0.1f);	
+		this.setPosition(new Vector3d(0,0,dhLinkEquivalent.d));
+		Matrix4d m = new Matrix4d();
+		m.setIdentity();
+		m.rotY(Math.toRadians(90));
+		Matrix4d m2 = new Matrix4d();
+		m2.setIdentity();
+		m.rotX(Math.toRadians(-90));
+		m2.rotZ(Math.toRadians(180));
+		m.mul(m2);
+		this.setRotation(m);
 	}
 	
 	@Override
-	public void render(GL2 gl2) {
+	public void render(GL2 gl2) {	
 		super.render(gl2);
-		// TODO render model file
 	}
 }
