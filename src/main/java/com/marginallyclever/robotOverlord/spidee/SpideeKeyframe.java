79f20728c1de9465c74b61e1fb02b74ed253467c
Dan Royer <dan@marginallyclever.com>
2019-06-27 13:07:55 -0700

    Create SpideeKeyframe.java
    compiles, probably won't run.

---
+++ src/main/java/com/marginallyclever/robotOverlord/spidee/SpideeKeyframe.java
@@ -0,0 +1,26 @@
+package com.marginallyclever.robotOverlord.spidee;
+
+import com.jogamp.opengl.GL2;
+import com.marginallyclever.robotOverlord.robot.RobotKeyframe;
+
+public class SpideeKeyframe implements RobotKeyframe {
+
+	@Override
+	public void interpolate(RobotKeyframe a, RobotKeyframe b, double t) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void render(GL2 gl2) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void renderInterpolation(GL2 gl2, RobotKeyframe arg1) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
