19faa3f07e08dbd76635287a49fab75d3b6934a4
Dan Royer <dan@marginallyclever.com>
2019-08-19 15:23:13 -0700

    added single block, loop, and cycle start

--- src/main/java/com/marginallyclever/robotOverlord/dhRobot/dhRobotPlayer/DHRobotPlayerPanel.java
+++ src/main/java/com/marginallyclever/robotOverlord/dhRobot/dhRobotPlayer/DHRobotPlayerPanel.java
@@ -1,40 +1,25 @@
 package com.marginallyclever.robotOverlord.dhRobot.dhRobotPlayer;
 
-import java.awt.Dimension;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
-import java.awt.GridLayout;
-import java.awt.Toolkit;
-import java.awt.datatransfer.Clipboard;
-import java.awt.datatransfer.StringSelection;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.ServiceLoader;
 
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
-import javax.swing.JComboBox;
 import javax.swing.JFileChooser;
 import javax.swing.JLabel;
-import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JSeparator;
 import javax.swing.JTextField;
 import javax.swing.border.EmptyBorder;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
-import javax.swing.event.UndoableEditEvent;
 
-import com.marginallyclever.convenience.StringHelper;
+import com.marginallyclever.robotOverlord.CollapsiblePanel;
 import com.marginallyclever.robotOverlord.RobotOverlord;
-import com.marginallyclever.robotOverlord.actions.UndoableActionSetDHTool;
-import com.marginallyclever.robotOverlord.commands.UserCommandSelectNumber;
 
 /**
  * Control Panel for a DHRobot
@@ -52,6 +37,10 @@ public class DHRobotPlayerPanel extends JPanel implements ActionListener, Change
 	
 	protected JTextField fileToPlay;
 	protected JButton chooseFile;
+	protected JButton reset;
+	protected JCheckBox buttonSingleBlock;
+	protected JCheckBox buttonLoop; 
+	protected JCheckBox buttonCycleStart; 
 
 	public DHRobotPlayerPanel(RobotOverlord gui,DHRobotPlayer arg0) {
 		this.player = arg0;
@@ -62,8 +51,22 @@ public class DHRobotPlayerPanel extends JPanel implements ActionListener, Change
 	
 	protected void buildPanel() {
 		this.removeAll();
-		this.setBorder(new EmptyBorder(0,0,0,0));
+
 		this.setLayout(new GridBagLayout());
+		GridBagConstraints c = new GridBagConstraints();
+		c.gridx=0;
+		c.gridy=0;
+		c.weightx=1;
+		c.weighty=1;
+		c.anchor=GridBagConstraints.NORTHWEST;
+		c.fill=GridBagConstraints.HORIZONTAL;
+
+		CollapsiblePanel oiwPanel = new CollapsiblePanel("DHRobotPlayer");
+		this.add(oiwPanel,c);
+		JPanel contents = oiwPanel.getContentPane();		
+		
+		contents.setBorder(new EmptyBorder(0,0,0,0));
+		contents.setLayout(new GridBagLayout());
 		GridBagConstraints con1 = new GridBagConstraints();
 		con1.gridx=0;
 		con1.gridy=0;
@@ -76,16 +79,34 @@ public class DHRobotPlayerPanel extends JPanel implements ActionListener, Change
 		//con1.gridy++;
 		//numLinks.addChangeListener(this);
 		
-		this.add(new JLabel("File to play"), con1);
+		contents.add(new JLabel("File to play"), con1);
 		con1.gridy++;
-		this.add(fileToPlay=new JTextField(""), con1);
+		contents.add(fileToPlay=new JTextField(""), con1);
 		con1.gridy++;
 		fileToPlay.setEditable(false);
-		this.add(chooseFile=new JButton("Choose"),con1);
+		contents.add(chooseFile=new JButton("..."),con1);
 		con1.gridy++;
 		chooseFile.addActionListener(this);
 
-		this.add(new JSeparator(JSeparator.VERTICAL), con1);
+		contents.add(reset = new JButton("Reset"),con1);
+		con1.gridy++;
+		contents.add(buttonSingleBlock = new JCheckBox("Single block"),con1);
+		con1.gridy++;
+		contents.add(buttonLoop = new JCheckBox("Loop at end"),con1);
+		con1.gridy++;
+		contents.add(buttonCycleStart = new JCheckBox("Cycle start"),con1);
+		con1.gridy++; 
+		
+		reset.addActionListener(this);
+		buttonSingleBlock.addItemListener(this);
+		buttonLoop.addItemListener(this);
+		buttonCycleStart.addItemListener(this);
+		
+		buttonSingleBlock.setSelected(player.isSingleBlock());
+		buttonLoop.setSelected(player.isLoop());
+		buttonCycleStart.setSelected(player.isCycleStart());
+		
+		contents.add(new JSeparator(JSeparator.VERTICAL), con1);
 		con1.gridy++;
 	}
 	
@@ -96,20 +117,25 @@ public class DHRobotPlayerPanel extends JPanel implements ActionListener, Change
 			JFileChooser fc = new JFileChooser();
 			int returnVal = fc.showOpenDialog(ro.getMainFrame());
 			if (returnVal == JFileChooser.APPROVE_OPTION) {
-				player.setFileToPlay(fc.getSelectedFile().getAbsolutePath());
+				String t = fc.getSelectedFile().getAbsolutePath();
+				fileToPlay.setText(t);
+				player.setFileToPlay(t);
 			}
 		}
+		if(source == reset) player.reset();
 	}
 
 	@Override
 	public void itemStateChanged(ItemEvent e) {
 		// for checkboxes
 		Object source = e.getItemSelectable();
+		if(source==buttonSingleBlock) player.setSingleBlock	(buttonSingleBlock	.isSelected());
+		if(source==buttonLoop		) player.setLoop		(buttonLoop			.isSelected());
+		if(source==buttonCycleStart	) player.setCycleStart	(buttonCycleStart	.isSelected());
 	}
 
 	@Override
 	public void stateChanged(ChangeEvent e) {
-		// TODO Auto-generated method stub
 		
 	}
 }
