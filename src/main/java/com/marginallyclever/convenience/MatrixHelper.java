372b1719268e17943081fc5e1674dbafb83a89d9
Dan Royer <dan@marginallyclever.com>
2019-08-22 10:48:20 -0700

    cleaned up matrix drawing

--- src/main/java/com/marginallyclever/convenience/MatrixHelper.java
+++ src/main/java/com/marginallyclever/convenience/MatrixHelper.java
@@ -9,21 +9,7 @@ import org.junit.Test;
 
 import com.jogamp.opengl.GL2;
 
-public class MatrixHelper {
-
-	
-	/**
-	 * @see drawMatrix(gl2,p,u,v,w,1)
-	 * @param gl2
-	 * @param p
-	 * @param u
-	 * @param v
-	 * @param w
-	 */
-	public static void drawMatrix(GL2 gl2,Vector3d p,Vector3d u,Vector3d v,Vector3d w) {
-		drawMatrix(gl2,p,u,v,w,1);
-	}
-	
+public class MatrixHelper {	
 	/**
 	 * @see drawMatrix(gl2,p,u,v,w,1)
 	 * @param gl2
@@ -52,6 +38,42 @@ public class MatrixHelper {
 		if(lightWasOn) gl2.glEnable(GL2.GL_LIGHTING);
 		if(depthWasOn) gl2.glEnable(GL2.GL_DEPTH_TEST);
 	}
+	/**
+	 * @see drawMatrix(gl2,p,u,v,w,1)
+	 * @param gl2
+	 * @param p
+	 * @param u
+	 * @param v
+	 * @param w
+	 */
+	public static void drawMatrix2(GL2 gl2,Matrix4d m,double scale) {
+		boolean depthWasOn = gl2.glIsEnabled(GL2.GL_DEPTH_TEST);
+		gl2.glDisable(GL2.GL_DEPTH_TEST);
+		boolean lightWasOn = gl2.glIsEnabled(GL2.GL_LIGHTING);
+		gl2.glDisable(GL2.GL_LIGHTING);
+		
+		gl2.glPushMatrix();
+			gl2.glTranslated(m.m03,m.m13,m.m23);
+			gl2.glScaled(scale, scale, scale);
+			
+			gl2.glBegin(GL2.GL_LINES);
+			gl2.glColor3f(1,0,0);		gl2.glVertex3f(0,0,0);		gl2.glVertex3d(m.m00,m.m10,m.m20);  // 1,1,0 = yellow
+			gl2.glColor3f(0,1,0);		gl2.glVertex3f(0,0,0);		gl2.glVertex3d(m.m01,m.m11,m.m21);  // 0,1,1 = teal 
+			gl2.glColor3f(0,0,1);		gl2.glVertex3f(0,0,0);		gl2.glVertex3d(m.m02,m.m12,m.m22);  // 1,0,1 = magenta
+			gl2.glEnd();
+	
+		gl2.glPopMatrix();
+		if(lightWasOn) gl2.glEnable(GL2.GL_LIGHTING);
+		if(depthWasOn) gl2.glEnable(GL2.GL_DEPTH_TEST);
+	}
+
+	
+	/**
+	 * @see drawMatrix(gl2,p,u,v,w,1)
+	 */
+	public static void drawMatrix(GL2 gl2,Vector3d p,Vector3d u,Vector3d v,Vector3d w) {
+		drawMatrix(gl2,p,u,v,w,1);
+	}
 	
 	/**
 	 * Draw the three vectors of a matrix at a point
@@ -63,22 +85,13 @@ public class MatrixHelper {
 	 * @param scale nominally 1
 	 */
 	public static void drawMatrix(GL2 gl2,Vector3d p,Vector3d u,Vector3d v,Vector3d w,double scale) {
-		//boolean depthWasOn = gl2.glIsEnabled(GL2.GL_DEPTH_TEST);
-		//gl2.glDisable(GL2.GL_DEPTH_TEST);
-			
-		gl2.glPushMatrix();
-			gl2.glTranslated(p.x, p.y, p.z);
-			gl2.glScaled(scale, scale, scale);
-			
-			gl2.glBegin(GL2.GL_LINES);
-			gl2.glColor3f(1,1,0);		gl2.glVertex3f(0,0,0);		gl2.glVertex3d(u.x,u.y,u.z);  // 1,1,0 = yellow
-			gl2.glColor3f(0,1,1);		gl2.glVertex3f(0,0,0);		gl2.glVertex3d(v.x,v.y,v.z);  // 0,1,1 = teal 
-			gl2.glColor3f(1,0,1);		gl2.glVertex3f(0,0,0);		gl2.glVertex3d(w.x,w.y,w.z);  // 1,0,1 = magenta
-			gl2.glEnd();
-
-		gl2.glPopMatrix();
-		
-		//if(depthWasOn) gl2.glEnable(GL2.GL_DEPTH_TEST);
+		Matrix4d m = new Matrix4d(
+				u.x,u.y,u.z,p.x,
+				v.x,v.y,v.z,p.y,
+				w.x,w.y,w.z,p.z,
+				0,0,0,1.0
+				);
+		drawMatrix(gl2,m,scale);
 	}
 
 	/**
@@ -105,22 +118,13 @@ public class MatrixHelper {
 	 * @param scale nominally 1
 	 */
 	public static void drawMatrix2(GL2 gl2,Vector3d p,Vector3d u,Vector3d v,Vector3d w,double scale) {
-		boolean depthWasOn = gl2.glIsEnabled(GL2.GL_DEPTH_TEST);
-		gl2.glDisable(GL2.GL_DEPTH_TEST);
-			
-		gl2.glPushMatrix();
-			gl2.glTranslated(p.x, p.y, p.z);
-			gl2.glScaled(scale, scale, scale);
-			
-			gl2.glBegin(GL2.GL_LINES);
-			gl2.glColor3f(1,0,0);		gl2.glVertex3f(0,0,0);		gl2.glVertex3d(u.x,u.y,u.z);  // 1,0,0 = red
-			gl2.glColor3f(0,1,0);		gl2.glVertex3f(0,0,0);		gl2.glVertex3d(v.x,v.y,v.z);  // 0,1,0 = green 
-			gl2.glColor3f(0,0,1);		gl2.glVertex3f(0,0,0);		gl2.glVertex3d(w.x,w.y,w.z);  // 0,0,1 = blue
-			gl2.glEnd();
-
-		gl2.glPopMatrix();
-		
-		if(depthWasOn) gl2.glEnable(GL2.GL_DEPTH_TEST);
+		Matrix4d m = new Matrix4d(
+				u.x,u.y,u.z,p.x,
+				v.x,v.y,v.z,p.y,
+				w.x,w.y,w.z,p.z,
+				0,0,0,1.0
+				);
+		drawMatrix2(gl2,m,scale);
 	}
 	
 	/**
@@ -243,5 +247,27 @@ public class MatrixHelper {
 		result.setTranslation(tStart);
 		// report ok
 		return true;
+	}
+	
+	public static void applyMatrix(GL2 gl2,Matrix4d pose) {
+		double[] mat = new double[16];
+		mat[ 0] = pose.m00;
+		mat[ 1] = pose.m10;
+		mat[ 2] = pose.m20;
+		mat[ 3] = pose.m30;
+		mat[ 4] = pose.m01;
+		mat[ 5] = pose.m11;
+		mat[ 6] = pose.m21;
+		mat[ 7] = pose.m31;
+		mat[ 8] = pose.m02;
+		mat[ 9] = pose.m12;
+		mat[10] = pose.m22;
+		mat[11] = pose.m32;
+		mat[12] = pose.m03;
+		mat[13] = pose.m13;
+		mat[14] = pose.m23;
+		mat[15] = pose.m33;
+		
+		gl2.glMultMatrixd(mat, 0);	
 	}
 }
