54e9a56ac25455263ac6996c949c3d21f37b61ce
Dan Royer <dan@marginallyclever.com>
2019-08-29 13:50:06 -0700

    Update Sixi2Calibrator.java

--- src/main/java/com/marginallyclever/robotOverlord/dhRobot/robots/sixi2/Sixi2Calibrator.java
+++ src/main/java/com/marginallyclever/robotOverlord/dhRobot/robots/sixi2/Sixi2Calibrator.java
@@ -5,7 +5,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
-import javax.swing.JCheckBox;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
