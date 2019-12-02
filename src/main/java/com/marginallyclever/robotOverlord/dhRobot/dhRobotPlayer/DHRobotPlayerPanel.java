e9a0008145417fac4aa7d24344b6bfbcfffd9e91
Dan Royer <dan@marginallyclever.com>
2019-12-02 12:35:00 -0800

    Update DHRobotPlayerPanel.java

--- src/main/java/com/marginallyclever/robotOverlord/dhRobot/dhRobotPlayer/DHRobotPlayerPanel.java
+++ src/main/java/com/marginallyclever/robotOverlord/dhRobot/dhRobotPlayer/DHRobotPlayerPanel.java
@@ -7,8 +7,6 @@ import java.awt.event.ActionListener;
 import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
 import java.util.ArrayList;
-import java.util.Arrays;
-
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
 import javax.swing.JComboBox;
