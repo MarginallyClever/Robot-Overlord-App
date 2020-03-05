091aa092e1b86cc483c32824b4616a4060f1fa95
Dan Royer <dan@marginallyclever.com>
2020-03-05 13:54:29 -0800

    made abstract

--- src/main/java/com/marginallyclever/robotOverlord/entity/robot/sixi2/Sixi2Model.java
+++ src/main/java/com/marginallyclever/robotOverlord/entity/robot/sixi2/Sixi2Model.java
@@ -7,7 +7,7 @@ import com.marginallyclever.robotOverlord.engine.dhRobot.DHLink;
 import com.marginallyclever.robotOverlord.engine.dhRobot.DHRobot;
 import com.marginallyclever.robotOverlord.engine.dhRobot.solvers.DHIKSolver_RTTRTR;
 
-public class Sixi2Model extends DHRobot {	
+public abstract class Sixi2Model extends DHRobot {	
 	double ELBOW_TO_ULNA_Y = -28.805;
 	double ELBOW_TO_ULNA_Z = 4.7201;
 	double ULNA_TO_WRIST_Y = -11.800;
@@ -45,7 +45,7 @@ public class Sixi2Model extends DHRobot {
 		links.get(0).setD(188.452/10+0.9);
 		links.get(0).flags = DHLink.READ_ONLY_D | DHLink.READ_ONLY_R | DHLink.READ_ONLY_ALPHA;
 		links.get(0).setRange(-120, 120);
-		links.get(0).setName("X");
+		links.get(0).setLetter("X");
 		links.get(0).setFilename("/Sixi2/shoulder.stl");
 		links.get(0).setModelOrigin(0, 0, -188.452/10);
 		links.get(0).setModelRotation(new Vector3d(90,-90,0));
@@ -54,7 +54,7 @@ public class Sixi2Model extends DHRobot {
 		links.get(1).setTheta(90);
 		links.get(1).flags = DHLink.READ_ONLY_D | DHLink.READ_ONLY_THETA | DHLink.READ_ONLY_R;
 		links.get(1).setRangeMin(-72);
-		links.get(1).setName("Y");
+		links.get(1).setLetter("Y");
 		links.get(1).setFilename("/Sixi2/bicep.stl");
 		links.get(1).setModelOrigin(0, 0, -188.452/10);
 		links.get(1).setModelRotation(new Vector3d(90,180,0));
@@ -63,7 +63,7 @@ public class Sixi2Model extends DHRobot {
 		links.get(2).setD(357.96/10);
 		links.get(2).flags = DHLink.READ_ONLY_D | DHLink.READ_ONLY_THETA | DHLink.READ_ONLY_R;
 		links.get(2).setRange(-83.369, 86);
-		links.get(2).setName("Z");
+		links.get(2).setLetter("Z");
 		links.get(2).setFilename("/Sixi2/forearm.stl");
 		links.get(2).setModelOrigin(0,0,(-188.452-357.96)/10);
 		links.get(2).setModelRotation(new Vector3d(-90,0,180));
@@ -77,7 +77,7 @@ public class Sixi2Model extends DHRobot {
 		links.get(4).setD(293.55/10);
 		links.get(4).flags = DHLink.READ_ONLY_D | DHLink.READ_ONLY_R | DHLink.READ_ONLY_ALPHA;
 		links.get(4).setRange(-175, 175);
-		links.get(4).setName("U");
+		links.get(4).setLetter("U");
 		links.get(4).setFilename("/Sixi2/tuningFork.stl");
 		links.get(4).setModelOrigin(0,(-188.452-357.96-64.259)/10,(-293.55)/10);
 		links.get(4).setModelRotation(new Vector3d(0,180,0));
@@ -87,7 +87,7 @@ public class Sixi2Model extends DHRobot {
 		links.get(5).setAlpha(25);
 		links.get(5).flags = DHLink.READ_ONLY_D | DHLink.READ_ONLY_THETA | DHLink.READ_ONLY_R;
 		links.get(5).setRange(-120, 125);
-		links.get(5).setName("Z");
+		links.get(5).setLetter("V");
 		links.get(5).setFilename("/Sixi2/picassoBox.stl");
 		links.get(5).setModelOrigin(0,(-188.452-357.96-64.259)/10,(-293.55-93.50)/10);
 		links.get(5).setModelRotation(new Vector3d(180,0,180));
@@ -96,7 +96,7 @@ public class Sixi2Model extends DHRobot {
 		links.get(6).setD(57.95/10);
 		links.get(6).flags = DHLink.READ_ONLY_D | DHLink.READ_ONLY_R | DHLink.READ_ONLY_ALPHA;
 		links.get(6).setRange(-180, 180);
-		links.get(6).setName("W");
+		links.get(6).setLetter("W");
 		links.get(6).setFilename("/Sixi2/hand.stl");
 		links.get(6).setModelOrigin(0,(-188.452-357.96-64.259)/10,(-293.55-93.50-57.95)/10);
 		links.get(6).setModelRotation(new Vector3d(180,0,180));
@@ -111,13 +111,13 @@ public class Sixi2Model extends DHRobot {
 		this.refreshPose();
 	}
 	
-	public void update(double dt) {}
+	abstract public void update(double dt);
 
 	/**
 	 * send a command to this model
 	 * @param command
 	 */
-	public void sendCommand(String command) {}
+	abstract public void sendCommand(String command);
 
 	/**
 	 * get the command for this model
@@ -125,8 +125,10 @@ public class Sixi2Model extends DHRobot {
 	 */
 	public String getCommand() {
 		String gcode = "G0";
-		for (int i = 0; i < links.size(); ++i) {
-			gcode += " " + links.get(i).getName() + links.get(i).getAdjustableValue();
+		for( DHLink link : links ) {
+			if(!link.getLetter().isEmpty()) {
+				gcode += " " + link.getLetter() + StringHelper.formatDouble(link.getAdjustableValue());
+			}
 		}
 		gcode += " F"+(StringHelper.formatDouble(feedRate));
 		gcode += " A"+(StringHelper.formatDouble(acceleration));
