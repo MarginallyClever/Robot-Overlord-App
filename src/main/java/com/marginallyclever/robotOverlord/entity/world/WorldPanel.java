7ea94530130e4b05ef77c0ae30a48a273296a94f
Dan Royer <dan@marginallyclever.com>
2020-03-07 18:30:17 -0800

    Update WorldPanel.java

--- src/main/java/com/marginallyclever/robotOverlord/entity/world/WorldPanel.java
+++ src/main/java/com/marginallyclever/robotOverlord/entity/world/WorldPanel.java
@@ -16,7 +16,10 @@ public class WorldPanel extends JPanel {
 	private static final long serialVersionUID = 1L;
 
 	private RobotOverlord gui;
+	
+	@SuppressWarnings("unused")
 	private World world;
+	
 	protected JList<?> entityList;
 	protected UserCommandAddEntity addButton;
 
