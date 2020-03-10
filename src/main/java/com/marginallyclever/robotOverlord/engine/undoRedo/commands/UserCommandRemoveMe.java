31045546ca57358e2740f47d29e4dfc6613bf570
Dan Royer <dan@marginallyclever.com>
2020-03-10 16:43:28 -0700

    Update UserCommandRemoveMe.java

--- src/main/java/com/marginallyclever/robotOverlord/engine/undoRedo/commands/UserCommandRemoveMe.java
+++ src/main/java/com/marginallyclever/robotOverlord/engine/undoRedo/commands/UserCommandRemoveMe.java
@@ -1,9 +1,12 @@
 package com.marginallyclever.robotOverlord.engine.undoRedo.commands;
 
+import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
 import javax.swing.event.UndoableEditEvent;
 
 import com.marginallyclever.robotOverlord.RobotOverlord;
@@ -15,20 +18,27 @@ import com.marginallyclever.robotOverlord.entity.Entity;
  * @author Admin
  *
  */
-public class UserCommandRemoveMe extends JButton implements ActionListener {
+public class UserCommandRemoveMe extends JPanel implements ActionListener {
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
 	protected RobotOverlord ro;
 	protected Entity entity;
+	JButton theButton;
 	
 	public UserCommandRemoveMe(RobotOverlord ro,Entity entity) {
-		super("Remove Me");
+		super();
+		
 		this.entity = entity;
-        getAccessibleContext().setAccessibleDescription("Remove an entity from the world.");
 		this.ro = ro;
-		addActionListener(this);
+
+		theButton = new JButton("Remove Me");
+		theButton.getAccessibleContext().setAccessibleDescription("Remove me from the world.");
+		theButton.addActionListener(this);
+		this.setLayout(new BorderLayout());
+		this.setBorder(new EmptyBorder(5,0,5,0));
+		this.add(theButton,BorderLayout.CENTER);
 	}
 
 	public void actionPerformed(ActionEvent e) {
