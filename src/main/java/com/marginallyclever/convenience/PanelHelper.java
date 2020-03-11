dabf7c97f035babae1f80f2ffb817d6f420580bb
Dan Royer <dan@marginallyclever.com>
2020-03-10 21:49:23 -0700

    added formatEntityPanels helper

--- src/main/java/com/marginallyclever/convenience/PanelHelper.java
+++ src/main/java/com/marginallyclever/convenience/PanelHelper.java
@@ -1,9 +1,16 @@
 package com.marginallyclever.convenience;
 
+import java.awt.BorderLayout;
 import java.awt.Component;
 import java.awt.GridBagConstraints;
+import java.util.ArrayList;
 
+import javax.swing.BoxLayout;
 import javax.swing.JComponent;
+import javax.swing.JPanel;
+import javax.swing.JTabbedPane;
+
+import com.marginallyclever.robotOverlord.uiElements.CollapsiblePanel;
 
 /**
  * Consistent gridBagConstraints for easier style tweaking
@@ -30,5 +37,54 @@ public class PanelHelper {
 		Component child = parent.getComponent(parent.getComponentCount()-1);
 		last.weighty=1;
 		parent.add(child,last);
+	}
+	
+	/**
+	 * Add a list of panels to a parent panel and control the layout. 
+	 * @param list
+	 * @param parent
+	 */
+	static public void formatEntityPanels(ArrayList<JPanel> list,JPanel parent) {
+		parent.removeAll();
+		
+		//System.out.println("formatEntityPanels "+parent.getName());
+		
+		// fill in the selectedEntityPanel
+		GridBagConstraints con1 = PanelHelper.getDefaultGridBagConstraints();
+		
+		// true to use tab
+		boolean tabbedLayout=true;
+		if(tabbedLayout==false) {
+			// single page layout
+			JPanel sum = new JPanel();
+			BoxLayout layout = new BoxLayout(sum, BoxLayout.PAGE_AXIS);
+			sum.setLayout(layout);
+			for( JPanel p : list ) {				
+				CollapsiblePanel oiwPanel = new CollapsiblePanel(p.getName());
+				oiwPanel.getContentPane().add(p);
+				sum.add(oiwPanel);
+			}
+
+			JPanel b = new JPanel(new BorderLayout());
+			b.add(sum, BorderLayout.PAGE_START);
+
+			parent.add(b,con1);
+		} else {
+			boolean reverseOrderOfTabs = false;
+			// tabbed layout
+			JTabbedPane b = new JTabbedPane();
+			for( JPanel p : list ) {				
+				if( reverseOrderOfTabs ) {
+					b.insertTab(p.getName(), null, p, null, 0);
+				} else {
+					b.addTab(p.getName(), p);
+				}
+			}
+			b.setSelectedIndex( reverseOrderOfTabs ? 0 : b.getTabCount()-1 );
+
+			parent.add(b,con1);
+		}
+		
+		PanelHelper.ExpandLastChild(parent, con1);
 	}
 }
