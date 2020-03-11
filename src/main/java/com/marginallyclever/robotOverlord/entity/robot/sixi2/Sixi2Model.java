5e183e4efaed405bf755cf574ed16f9bb706f1ff
Dan Royer <dan@marginallyclever.com>
2020-03-11 03:17:58 -0700

    Fixed D-H parameters and removed dumb DHLinks

--- src/main/java/com/marginallyclever/robotOverlord/entity/robot/sixi2/Sixi2Model.java
+++ src/main/java/com/marginallyclever/robotOverlord/entity/robot/sixi2/Sixi2Model.java
@@ -1,7 +1,5 @@
 package com.marginallyclever.robotOverlord.entity.robot.sixi2;
 
-import javax.vecmath.Vector3d;
-
 import com.marginallyclever.convenience.StringHelper;
 import com.marginallyclever.robotOverlord.engine.dhRobot.DHLink;
 import com.marginallyclever.robotOverlord.engine.dhRobot.DHLink.LinkAdjust;
@@ -31,75 +29,77 @@ public abstract class Sixi2Model extends DHRobot {
 		this.setModelRotation(90,-90,0);
 		
 		// setup children
-		this.setNumLinks(8);
+		this.setNumLinks(6);
 		
 		// pan shoulder
-		links.get(0).setD(188.452/10+0.9);
-		links.get(0).flags = LinkAdjust.THETA;
-		links.get(0).setRange(-120, 120);
 		links.get(0).setLetter("X");
 		links.get(0).setModelFilename("/Sixi2/shoulder.stl");
-		links.get(0).setModelOrigin(0, 0, -188.452/10);
-		links.get(0).setModelRotation(new Vector3d(90,-90,0));
+		links.get(0).setD(18.8452+0.9);
+		links.get(0).setR(0);
+		links.get(0).setAlpha(-90);
+		links.get(0).setModelRotation(0,-90,180);
+		links.get(0).setModelOrigin(0,18.84520,0);
+		links.get(1).setRange(-120,120);
 	
 		// tilt shoulder
-		links.get(1).setTheta(90);
-		links.get(1).flags = LinkAdjust.ALPHA;
-		links.get(1).setRangeMin(-72);
 		links.get(1).setLetter("Y");
 		links.get(1).setModelFilename("/Sixi2/bicep.stl");
-		links.get(1).setModelOrigin(0, 0, -188.452/10);
-		links.get(1).setModelRotation(new Vector3d(90,180,0));
+		links.get(1).setD(0);
+		links.get(1).setR(35.796);
+		links.get(1).setAlpha(0);
+		links.get(1).setModelRotation(90,0,-90);
+		links.get(1).setModelOrigin(-18.84520-35.796,0,0);
+		links.get(1).setRange(-180,0);
+		links.get(1).setTheta(-90);
 	
 		// tilt elbow
-		links.get(2).setD(357.96/10);
-		links.get(2).flags = LinkAdjust.ALPHA;
-		links.get(2).setRange(-83.369, 86);
 		links.get(2).setLetter("Z");
 		links.get(2).setModelFilename("/Sixi2/forearm.stl");
-		links.get(2).setModelOrigin(0,0,(-188.452-357.96)/10);
-		links.get(2).setModelRotation(new Vector3d(-90,0,180));
+		links.get(2).setD(0);
+		links.get(2).setR(6.4259);
+		links.get(2).setAlpha(-90);
+		links.get(2).setModelRotation(180,0,-90);
+		links.get(2).setModelOrigin(-18.84520-35.796-6.4259,0,0);
+		links.get(2).setRange(-83.369, 86);
 	
-		// interim point
-		links.get(3).setD(64.259/10);
-		links.get(3).setAlpha(90);
-		links.get(3).flags = LinkAdjust.NONE;
-		
 		// roll ulna
-		links.get(4).setD(293.55/10);
-		links.get(4).flags = LinkAdjust.THETA;
-		links.get(4).setRange(-175, 175);
-		links.get(4).setLetter("U");
-		links.get(4).setModelFilename("/Sixi2/tuningFork.stl");
-		links.get(4).setModelOrigin(0,(-188.452-357.96-64.259)/10,(-293.55)/10);
-		links.get(4).setModelRotation(new Vector3d(0,180,0));
+		links.get(3).setLetter("U");
+		links.get(3).setModelFilename("/Sixi2/tuningFork.stl");
+		links.get(3).setD(29.355+9.350);
+		links.get(3).setR(0);
+		links.get(3).setAlpha(90);
+		links.get(3).setModelRotation(90,0,-90);
+		links.get(3).setModelOrigin(-18.84520-35.796-6.4259,-(29.355+9.350),0);
+		links.get(3).setRange(-175, 175);
 	
 		// tilt picassobox
-		links.get(5).setD(93.50/10);
-		links.get(5).setAlpha(25);
-		links.get(5).flags = LinkAdjust.ALPHA;
-		links.get(5).setRange(-120, 125);
-		links.get(5).setLetter("V");
-		links.get(5).setModelFilename("/Sixi2/picassoBox.stl");
-		links.get(5).setModelOrigin(0,(-188.452-357.96-64.259)/10,(-293.55-93.50)/10);
-		links.get(5).setModelRotation(new Vector3d(180,0,180));
+		links.get(4).setLetter("V");
+		links.get(4).setModelFilename("/Sixi2/picassoBox.stl");
+		links.get(4).setD(0);
+		links.get(4).setR(0);
+		links.get(4).setAlpha(-90);
+		links.get(4).setModelRotation(180,0,-90);
+		links.get(4).setModelOrigin((-18.84520-35.796-6.4259),0,-(29.355+9.350));
+		links.get(4).setRange(-120, 120);
+		links.get(4).setTheta(20);
 	
 		// roll hand
-		links.get(6).setD(57.95/10);
-		links.get(6).flags = LinkAdjust.THETA;
-		links.get(6).setRange(-180, 180);
-		links.get(6).setLetter("W");
-		links.get(6).setModelFilename("/Sixi2/hand.stl");
-		links.get(6).setModelOrigin(0,(-188.452-357.96-64.259)/10,(-293.55-93.50-57.95)/10);
-		links.get(6).setModelRotation(new Vector3d(180,0,180));
+		links.get(5).setLetter("W");
+		links.get(5).setModelFilename("/Sixi2/hand.stl");
+		links.get(5).setD(5.795);
+		links.get(5).setR(0);
+		links.get(5).setAlpha(0);
+		links.get(5).setModelRotation(0,180,0);
+		links.get(5).setModelOrigin(0,-18.84520-35.796-6.4259,-(29.355+9.350+5.795));
+		links.get(5).setRange(-170, 170);
 		
-		links.get(7).flags = LinkAdjust.NONE;
 
 		setModelScale(0.1f);
 		for( DHLink link : links ) {
 			link.setModelScale(0.1f);
+			link.flags = LinkAdjust.THETA;
 		}
-		
+
 		this.refreshPose();
 	}
 	
