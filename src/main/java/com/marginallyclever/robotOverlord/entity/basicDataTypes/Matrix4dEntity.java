a3f726e4d8a55eb0893f5f987a1e7f5a34bd9f8b
Dan Royer <dan@marginallyclever.com>
2020-03-18 16:00:25 -0700

    more

--- src/main/java/com/marginallyclever/robotOverlord/entity/basicDataTypes/Matrix4dEntity.java
+++ src/main/java/com/marginallyclever/robotOverlord/entity/basicDataTypes/Matrix4dEntity.java
@@ -4,7 +4,9 @@ import javax.vecmath.Matrix3d;
 import javax.vecmath.Matrix4d;
 import javax.vecmath.Vector3d;
 
+import com.marginallyclever.convenience.MatrixHelper;
 import com.marginallyclever.robotOverlord.entity.AbstractEntity;
+import com.marginallyclever.robotOverlord.swingInterface.view.View;
 
 /**
  * @author Dan Royer
@@ -12,6 +14,9 @@ import com.marginallyclever.robotOverlord.entity.AbstractEntity;
  *
  */
 public class Matrix4dEntity extends AbstractEntity<Matrix4d> {
+	Vector3dEntity pos = new Vector3dEntity("Position");
+	Vector3dEntity rot = new Vector3dEntity("Rotation");
+	
 	/**
 	 * 
 	 */
@@ -37,16 +42,33 @@ public class Matrix4dEntity extends AbstractEntity<Matrix4d> {
 		setChanged();
 		t.setTranslation(trans);
 		notifyObservers();
+		pos.set(trans);
 	}
 	
 	public void getTranslation(Vector3d trans) {
 		t.get(trans);
 	}
 	
-	public void setRotation(Matrix3d rot) {
+	public void setRotation(Matrix3d m) {
 		setChanged();
-		t.setRotation(rot);
+		t.setRotation(m);
 		notifyObservers();
+		
+		Vector3d r = MatrixHelper.matrixToEuler(t);
+		rot.set(Math.toDegrees(r.x),
+				Math.toDegrees(r.y),
+				Math.toDegrees(r.z));
+	}
+	
+	public void set(Matrix4d m) {
+		super.set(m);
+
+		Vector3d trans = MatrixHelper.getPosition(t);
+		pos.set(trans);
+		Vector3d r = MatrixHelper.matrixToEuler(t);
+		rot.set(Math.toDegrees(r.x),
+				Math.toDegrees(r.y),
+				Math.toDegrees(r.z));
 	}
 	
 	public void getRotation(Matrix3d rot) {
@@ -63,5 +85,10 @@ public class Matrix4dEntity extends AbstractEntity<Matrix4d> {
 	
 	public String toString() {
 		return getName()+"="+t.toString();
+	}
+	
+	public void getView(View view) {
+		view.addVector3(pos);
+		view.addVector3(rot);
 	}
 }
---
+++ src/main/java/com/marginallyclever/robotOverlord/swingInterface/view/ViewComponent.java
@@ -0,0 +1,5 @@
+package com.marginallyclever.robotOverlord.swingInterface.view;
+
+public class ViewComponent {
+
+}
