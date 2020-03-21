67cdbb65a1c98225d8f6743c4ec6d1d144c16926
Dan Royer <dan@marginallyclever.com>
2020-03-21 12:15:59 -0700

    more correct DH parameters

--- src/main/java/com/marginallyclever/robotOverlord/entity/scene/robotEntity/dhRobotEntity/sixi2/Sixi2Model.java
+++ src/main/java/com/marginallyclever/robotOverlord/entity/scene/robotEntity/dhRobotEntity/sixi2/Sixi2Model.java
@@ -1,6 +1,7 @@
 package com.marginallyclever.robotOverlord.entity.scene.robotEntity.dhRobotEntity.sixi2;
 
 import javax.vecmath.Matrix4d;
+import javax.vecmath.Vector3d;
 
 import com.marginallyclever.convenience.StringHelper;
 import com.marginallyclever.robotOverlord.entity.basicDataTypes.DoubleEntity;
@@ -31,18 +32,109 @@ public abstract class Sixi2Model extends DHRobotEntity {
 		addChild(feedRate);
 		addChild(acceleration);
 		addChild(endEffector);
-		
+
 		this.setIKSolver(new DHIKSolver_RTTRTR());
 		
-		// setup children
-		this.setNumLinks(6);
-
 		ModelEntity anchor = new ModelEntity();
 		addChild(anchor);
 		anchor.setName("Base");
 		anchor.setModelFilename("/Sixi2/anchor.stl");
 		anchor.setModelOrigin(0, 0, 0.9);
+
+		// setup children
+		this.setNumLinks(6);
 		
+		//ORIGINAL_FORM();
+		NEW_FORM();
+		
+		links.get(links.size()-1).addChild(endEffector);
+		
+		// If I have the poseWorld for each DHLink then I can use that to adjust the model values.
+		// The bone.Z axis is the axis of rotation for that kinematic link.
+		
+		// update this world pose and all my children's poses all the way down.
+		this.updatePoseWorld();
+		
+		for(int i=0;i<links.size();++i) {
+			DHLink bone=links.get(i);
+			if(bone.getModel()!=null) {
+				Matrix4d iWP = bone.getPoseWorld();
+				iWP.invert();
+				bone.getModel().adjustMatrix(iWP);
+			}
+		}
+	}
+	
+	// https://www.youtube.com/watch?v=f79suBeEObs
+	public void NEW_FORM() {
+		boolean attach=true;
+		if(!attach) {
+		ModelEntity anchor1 = new ModelEntity();	addChild(anchor1);	anchor1.setModelFilename("/Sixi2/shoulder.stl");
+		ModelEntity anchor2 = new ModelEntity();	addChild(anchor2);	anchor2.setModelFilename("/Sixi2/bicep.stl");
+		ModelEntity anchor3 = new ModelEntity();	addChild(anchor3);	anchor3.setModelFilename("/Sixi2/forearm.stl");
+		ModelEntity anchor4 = new ModelEntity();	addChild(anchor4);	anchor4.setModelFilename("/Sixi2/tuningFork.stl");
+		ModelEntity anchor5 = new ModelEntity();	addChild(anchor5);	anchor5.setModelFilename("/Sixi2/picassoBox.stl");
+		ModelEntity anchor6 = new ModelEntity();	addChild(anchor6);	anchor6.setModelFilename("/Sixi2/hand.stl");
+		}
+		
+		// pan shoulder
+		links.get(0).setLetter("X");
+		if(attach) links.get(0).setModelFilename("/Sixi2/shoulder.stl");
+		links.get(0).setD(18.8452);
+		links.get(0).setTheta(0);
+		links.get(0).setR(0);
+		links.get(0).setAlpha(0);
+		links.get(0).setRange(-120,120);
+		
+		// tilt shoulder
+		links.get(1).setLetter("Y");
+		if(attach) links.get(1).setModelFilename("/Sixi2/bicep.stl");
+		links.get(1).setD(0);
+		links.get(1).setTheta(0);
+		links.get(1).setR(0);
+		links.get(1).setAlpha(-90);
+		links.get(1).setRange(-90,90);
+
+		// tilt elbow
+		links.get(2).setLetter("Z");
+		if(attach) links.get(2).setModelFilename("/Sixi2/forearm.stl");
+		links.get(2).setD(0);
+		links.get(2).setTheta(-90);
+		links.get(2).setR(35.796);
+		links.get(2).setAlpha(0);
+		links.get(2).setRange(-83.369-90, 86-90);
+	
+		// roll ulna
+		links.get(3).setLetter("U");
+		if(attach) links.get(3).setModelFilename("/Sixi2/tuningFork.stl");
+		links.get(3).setD(0);
+		links.get(3).setTheta(0);
+		links.get(3).setR(6.4259);
+		links.get(3).setAlpha(-90);
+		links.get(3).setRange(-175, 175);
+	
+		// tilt picassobox
+		links.get(4).setLetter("V");
+		if(attach) links.get(4).setModelFilename("/Sixi2/picassoBox.stl");
+		links.get(4).setD(29.355+9.350);
+		links.get(4).setTheta(0);
+		links.get(4).setR(0);
+		links.get(4).setAlpha(-90);
+		links.get(4).setRange(-120, 120);
+	
+		// roll hand
+		links.get(5).setLetter("W");
+		if(attach) links.get(5).setModelFilename("/Sixi2/hand.stl");
+		links.get(5).setTheta(0);
+		links.get(5).setD(0);
+		links.get(5).setR(0);
+		links.get(5).setAlpha(90);
+		links.get(5).setRange(-170, 170);
+		
+		endEffector.setPosition(new Vector3d(0,0,5.795));
+	}
+	
+	public void ORIGINAL_FORM() {
 		// pan shoulder
 		links.get(0).setLetter("X");
 		links.get(0).setModelFilename("/Sixi2/shoulder.stl");
@@ -50,7 +142,7 @@ public abstract class Sixi2Model extends DHRobotEntity {
 		links.get(0).setR(0);
 		links.get(0).setAlpha(-90);
 		links.get(0).setRange(-120,120);
-	
+		
 		// tilt shoulder
 		links.get(1).setLetter("Y");
 		links.get(1).setModelFilename("/Sixi2/bicep.stl");
@@ -59,7 +151,7 @@ public abstract class Sixi2Model extends DHRobotEntity {
 		links.get(1).setAlpha(0);
 		links.get(1).setRange(-180,0);
 		links.get(1).setTheta(-90);
-	
+
 		// tilt elbow
 		links.get(2).setLetter("Z");
 		links.get(2).setModelFilename("/Sixi2/forearm.stl");
@@ -91,29 +183,6 @@ public abstract class Sixi2Model extends DHRobotEntity {
 		links.get(5).setR(0);
 		links.get(5).setAlpha(0);
 		links.get(5).setRange(-170, 170);
-
-		links.get(5).addChild(endEffector);
-		
-		this.refreshPose();
-
-		// Now I have the poseWorld for each DHLink, I can use that to adjust the model values.
-		// I need the world pose of every 'bone' DHLink.  I'll keep a running total in 'worldPose'.
-		// The bone.Z axis is the axis of rotation for that kinematic link.
-		Matrix4d worldPose = new Matrix4d();
-		worldPose.setIdentity();
-		Matrix4d iWP = new Matrix4d();
-		Matrix4d m2 = new Matrix4d();
-		
-		for(int i=0;i<links.size();++i) {
-			DHLink bone=links.get(i);
-			Matrix4d m = bone.getPose();
-			worldPose.mul(m);
-			m2.sub(m,bone.getPoseWorld());
-			iWP.set(worldPose);
-			iWP.invert();
-			bone.getModel().adjustMatrix(iWP);
-		}
-		
 	}
 	
 	abstract public void update(double dt);
