5f5772b25f7cb29f52e38e5118ad8455b63e27f1
Dan Royer <dan@marginallyclever.com>
2020-03-23 10:09:23 -0700

    make joystick work again

--- src/main/java/com/marginallyclever/robotOverlord/entity/scene/dhRobotEntity/sixi2/SixiJoystick.java
+++ src/main/java/com/marginallyclever/robotOverlord/entity/scene/dhRobotEntity/sixi2/SixiJoystick.java
@@ -1,17 +1,17 @@
 package com.marginallyclever.robotOverlord.entity.scene.dhRobotEntity.sixi2;
 
+import java.util.Observable;
 import java.util.StringTokenizer;
 import java.util.concurrent.locks.ReentrantLock;
 
-import com.marginallyclever.communications.NetworkConnection;
-import com.marginallyclever.communications.NetworkConnectionListener;
 import com.marginallyclever.convenience.StringHelper;
 import com.marginallyclever.robotOverlord.entity.Entity;
 import com.marginallyclever.robotOverlord.entity.basicDataTypes.RemoteEntity;
 import com.marginallyclever.robotOverlord.entity.scene.dhRobotEntity.DHKeyframe;
 import com.marginallyclever.robotOverlord.entity.scene.modelEntity.ModelEntity;
+import com.marginallyclever.robotOverlord.swingInterface.view.ViewPanel;
 
-public class SixiJoystick extends ModelEntity implements NetworkConnectionListener {
+public class SixiJoystick extends ModelEntity {
 	/**
 	 * 
 	 */
@@ -20,14 +20,13 @@ public class SixiJoystick extends ModelEntity implements NetworkConnectionListen
 	private Sixi2 target;
 	
 	private RemoteEntity connection = new RemoteEntity();
-	
 	private ReentrantLock lock = new ReentrantLock();
 	
 	DHKeyframe keyframe;
 	
 	public SixiJoystick() {
 		setName("Sixi Joystick");
-		addChild(connection);
+		connection.addObserver(this);
 	}
 	
 	// TODO this is trash.  if robot is deleted this link would do what, exactly?
@@ -42,47 +41,58 @@ public class SixiJoystick extends ModelEntity implements NetworkConnectionListen
 	}
 	
 	@Override
-	public void lineError(NetworkConnection arg0, int lineNumber) {}
-	
-	@Override
-	public void sendBufferEmpty(NetworkConnection arg0) {}
-	
-	@Override
-	// data arrives asynchronously, so make sure to 
-	public void dataAvailable(NetworkConnection arg0, String data) {
-		if(lock.isLocked()) return;
-		lock.lock();
-		try {
-			if(target==null) {
-				target = findRobot();
-			}
-			if(target!=null) {
-				if(keyframe == null) {
-					 keyframe = target.sim.getIKSolver().createDHKeyframe();
+	public void update(Observable obs,Object obj) {
+		if(obs == connection) {
+			if(lock.isLocked()) return;
+			lock.lock();
+			try {
+				if(target==null) {
+					target = findRobot();
 				}
-				StringTokenizer tokenizer = new StringTokenizer(data);
-				if(tokenizer.countTokens()<6) return;
-				
-				for(int i=0;i<6;++i) {
-					double d = StringHelper.parseNumber(tokenizer.nextToken());
-					keyframe.fkValues[i]=Math.max(Math.min(d,180),-180);
+				if(target!=null) {
+					String message = (String)obj;
+					System.out.println("JOY: "+message.trim());
+					
+					if(keyframe==null) {
+						keyframe = target.sim.getIKSolver().createDHKeyframe();
+					}
+
+					StringTokenizer tokenizer = new StringTokenizer(message);
+					if(tokenizer.countTokens()<6) return;
+					
+					for(int i=0;i<6;++i) {
+						double d = StringHelper.parseNumber(tokenizer.nextToken());
+						keyframe.fkValues[i]=Math.max(Math.min(d,180),-180);
+					}
+					keyframe.fkValues[1]*=-1;
+					keyframe.fkValues[4]*=-1;
 				}
-				keyframe.fkValues[1]*=-1;
-				keyframe.fkValues[4]*=-1;
 			}
-		}
-		finally {
-			lock.unlock();
+			finally {
+				lock.unlock();
+			}
 		}
 	}
 
 	@Override
 	public void update(double dt) {
 		if(target==null) return;
+		
 		if(lock.isLocked()) return;
-
 		lock.lock();
-		target.sim.setPoseFK(keyframe);
-		lock.unlock();
+		try {
+			target.sim.setPoseFK(keyframe);
+		}
+		finally {
+			lock.unlock();
+		}
+	}
+	
+	@Override
+	public void getView(ViewPanel view) {
+		view.pushStack("Sj", "Sixi Joystick");
+		view.add(connection);
+		view.popStack();
+		super.getView(view);
 	}
 }
