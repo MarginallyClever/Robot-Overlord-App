47ca29805f15d9db39a54f650a2794be0e3057d9
Dan Royer <dan@marginallyclever.com>
2020-03-28 00:52:58 -0700

    Update DHRobotEntity.java

--- src/main/java/com/marginallyclever/robotOverlord/entity/scene/dhRobotEntity/DHRobotEntity.java
+++ src/main/java/com/marginallyclever/robotOverlord/entity/scene/dhRobotEntity/DHRobotEntity.java
@@ -4,10 +4,8 @@ import java.util.List;
 import java.util.ArrayList;
 
 import javax.vecmath.Matrix4d;
-import javax.vecmath.Vector3d;
 
 import com.marginallyclever.convenience.IntersectionTester;
-import com.marginallyclever.convenience.MatrixHelper;
 import com.marginallyclever.robotOverlord.RobotOverlord;
 import com.marginallyclever.robotOverlord.entity.Entity;
 import com.marginallyclever.robotOverlord.entity.scene.PoseEntity;
