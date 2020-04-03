f32b35e996d9623d2965af835b712ab42134a0f0
Dan Royer <dan@marginallyclever.com>
2020-04-02 17:28:27 -0700

    Create DHIKSolver_FABRIK.java

---
+++ src/main/java/com/marginallyclever/robotOverlord/entity/scene/dhRobotEntity/solvers/DHIKSolver_FABRIK.java
@@ -0,0 +1,32 @@
+package com.marginallyclever.robotOverlord.entity.scene.dhRobotEntity.solvers;
+
+import javax.vecmath.Matrix4d;
+import javax.vecmath.Vector3d;
+
+import com.marginallyclever.convenience.MatrixHelper;
+import com.marginallyclever.robotOverlord.entity.scene.dhRobotEntity.DHKeyframe;
+import com.marginallyclever.robotOverlord.entity.scene.dhRobotEntity.DHLink;
+import com.marginallyclever.robotOverlord.entity.scene.dhRobotEntity.DHRobotEntity;
+
+/**
+ * TODO FABRIK solver (http://www.andreasaristidou.com/publications/papers/FABRIK.pdf)
+ * @author Dan Royer
+ * @since 1.6.0
+ *
+ */
+public class DHIKSolver_FABRIK extends DHIKSolver {
+	/**
+	 * @return the number of double values needed to store a valid solution from this DHIKSolver.
+	 */
+	public int getSolutionSize() {
+		return 6;
+	}
+	
+	/**
+	 * 
+	 */
+	@Override
+	public SolutionType solveWithSuggestion(DHRobotEntity robot,Matrix4d targetMatrix,DHKeyframe keyframe,DHKeyframe suggestion) {
+		return SolutionType.NO_SOLUTIONS;
+	}
+}
