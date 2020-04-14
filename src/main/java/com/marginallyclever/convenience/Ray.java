3bce619ee6f5ff69d6054f490a2f097a13f4b9ff
Dan Royer <dan@marginallyclever.com>
2020-04-14 16:26:08 -0700

    added ray

---
+++ src/main/java/com/marginallyclever/convenience/Ray.java
@@ -0,0 +1,28 @@
+package com.marginallyclever.convenience;
+
+import javax.vecmath.Point3d;
+import javax.vecmath.Vector3d;
+
+import com.jogamp.opengl.GL2;
+
+/**
+ * 
+ * @author Dan Royer
+ * @since 1.6.0
+ *
+ */
+public class Ray {
+	public Point3d start = new Point3d();
+	public Vector3d direction = new Vector3d();
+	
+	public void render(GL2 gl2) {
+		gl2.glBegin(GL2.GL_LINES);
+		gl2.glColor3d(1,0,0); 
+		gl2.glVertex3d(start.x, start.y, start.z);
+		gl2.glColor3d(0,1,0);
+		gl2.glVertex3d(
+				start.x+direction.x*50,
+				start.y+direction.y*50, 
+				start.z+direction.z*50);
+	}
+}
