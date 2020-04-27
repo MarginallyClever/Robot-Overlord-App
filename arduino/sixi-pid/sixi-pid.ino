6b0b3a95825d2c726951edfd717472bfffa5456f
Dan Royer <dan@marginallyclever.com>
2020-04-27 15:55:47 -0700

    set PIDs to 0,0,0 by default

--- arduino/sixi-pid/sixi-pid.ino
+++ arduino/sixi-pid/sixi-pid.ino
@@ -88,12 +88,12 @@ void setup() {
   motors[4].setPID(1.25, 0.5, 0.25);        //OFFSET IS HAPPENING 
   motors[5].setPID(1.25, 0.5, 0.25);        //OFFSET IS HAPPENING 
 /*/
-  motors[0].setPID(50.0, 1.0, 0.01);
-  motors[1].setPID(50.0, 1.0, 0.01);
-  motors[2].setPID(50.0, 1.0, 0.01);
-  motors[3].setPID(50.0, 1.0, 0.01);
-  motors[4].setPID(50.0, 1.0, 0.01);
-  motors[5].setPID(50.0, 1.0, 0.01);
+  motors[0].setPID(0.0, 0.0, 0.0);
+  motors[1].setPID(0.0, 0.0, 0.0);
+  motors[2].setPID(0.0, 0.0, 0.0);
+  motors[3].setPID(0.0, 0.0, 0.0);
+  motors[4].setPID(0.0, 0.0, 0.0);
+  motors[5].setPID(0.0, 0.0, 0.0);
 //*/
   //clockISRProfile();
   
