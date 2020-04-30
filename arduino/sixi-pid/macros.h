b4da0dd688fda111a6c3e1d5d58e5065b1747d2f
Dan Royer <dan@marginallyclever.com>
2020-04-29 17:28:58 -0700

    Added wraps

--- arduino/sixi-pid/macros.h
+++ arduino/sixi-pid/macros.h
@@ -23,3 +23,9 @@
 #define SET_BIT_OFF(NN,BB)  CBI(NN,BB)
 #define SET_BIT(NN,BB,TF)   do { if(TF) SBI(NN,BB); else CBI(NN,BB); } while(0);
 #define FLIP_BIT(NN,BB)     (NN ^= (1<<BB))
+
+
+// wrap all degrees to within -180...180.
+#define WRAP_DEGREES(NN)     (fmod( (NN+180), 360 ) - 180)
+// wrapp all radians within -PI...PI
+#define WRAP_RADIANS(NN)     (fmod( (NN+PI), PI*2 ) - PI)
