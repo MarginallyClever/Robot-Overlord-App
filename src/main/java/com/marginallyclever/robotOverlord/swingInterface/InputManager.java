7f69bb43360843749ff3b34f85153f215e63173c
Dan Royer <dan@marginallyclever.com>
2020-05-06 10:52:05 -0700

    Update InputManager.java

--- src/main/java/com/marginallyclever/robotOverlord/swingInterface/InputManager.java
+++ src/main/java/com/marginallyclever/robotOverlord/swingInterface/InputManager.java
@@ -3,8 +3,6 @@ package com.marginallyclever.robotOverlord.swingInterface;
 import net.java.games.input.Component;
 import net.java.games.input.Controller;
 import net.java.games.input.ControllerEnvironment;
-import net.java.games.input.Event;
-import net.java.games.input.EventQueue;
 import net.java.games.input.Component.Identifier;
 
 /**
