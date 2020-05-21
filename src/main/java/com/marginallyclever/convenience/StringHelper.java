dd3b314c85b6304f2a76bb8954df2849ca802e53
Dan Royer <dan@marginallyclever.com>
2020-05-21 14:25:05 -0700

    Update StringHelper.java

--- src/main/java/com/marginallyclever/convenience/StringHelper.java
+++ src/main/java/com/marginallyclever/convenience/StringHelper.java
@@ -56,4 +56,26 @@ public class StringHelper {
 
 		return calculatedChecksum == deliveredChecksum;
 	}
+	
+	/**
+	 * 
+	 * @param value 
+	 * @return byte array
+	 */
+	public static byte[] floatToByteArray(double value) {
+	    int intBits =  Float.floatToIntBits((float)value);
+	    return new byte[] {
+	      (byte) (intBits >> 24), (byte) (intBits >> 16), (byte) (intBits >> 8), (byte) (intBits) };
+	}
+
+	/**
+	 * 
+	 * @param value byte array
+	 * @return double value
+	 */
+	public static double byteArrayToFloat(byte[] bytes) {
+	    int intBits = 
+	      bytes[0] << 24 | (bytes[1] & 0xFF) << 16 | (bytes[2] & 0xFF) << 8 | (bytes[3] & 0xFF);
+	    return (double)Float.intBitsToFloat(intBits);  
+	}
 }
