e1e70ae9051d709edaae2662ad0c199d7aa78e20
Dan Royer <dan@marginallyclever.com>
2020-05-21 14:25:09 -0700

    Create MathHelperTest.java

---
+++ src/test/java/com/marginallyclever/robotOverlord/MathHelperTest.java
@@ -0,0 +1,19 @@
+package com.marginallyclever.robotOverlord;
+
+import org.junit.Test;
+
+import com.marginallyclever.convenience.MathHelper;
+
+public class MathHelperTest {
+
+	@Test
+	public void testWrapDegrees() {
+		System.out.println("testWrapDegrees start");
+		for(double i=-360*2;i<=360*2;i+=10) {
+			double v = MathHelper.wrapDegrees(i);
+			System.out.println(i+"\t"+v);
+			//assert(v>=-180 && v<=180);
+		}
+		System.out.println("testWrapDegrees end");
+	}
+}
