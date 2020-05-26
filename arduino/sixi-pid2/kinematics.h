9dd5444b735755263ce28d57680f336780e2783d
Dan Royer <dan@marginallyclever.com>
2020-05-25 19:03:45 -0700

    added joint limits

--- arduino/sixi-pid2/kinematics.h
+++ arduino/sixi-pid2/kinematics.h
@@ -5,31 +5,43 @@
 #define DH_0_ALPHA -90
 #define DH_0_D     19.745
 #define DH_0_R     0
+#define DH_0_MAX   120
+#define DH_0_MIN   -120
 
 #define DH_1_THETA -90
 #define DH_1_ALPHA 0
 #define DH_1_D     0
 #define DH_1_R     35.796
+#define DH_1_MAX   0
+#define DH_1_MIN   -170
 
 #define DH_2_THETA 0
 #define DH_2_ALPHA -90
 #define DH_2_D     0
 #define DH_2_R     6.426
+#define DH_2_MAX   86
+#define DH_2_MIN   -83.369
 
 #define DH_3_THETA 0
 #define DH_3_ALPHA 90
 #define DH_3_D     38.705
 #define DH_3_R     0
+#define DH_3_MAX   175
+#define DH_3_MIN   -175
 
 #define DH_4_THETA 0
 #define DH_4_ALPHA -90
 #define DH_4_D     0
 #define DH_4_R     0
+#define DH_4_MAX   120
+#define DH_4_MIN   -120
 
 #define DH_5_THETA 0
 #define DH_5_ALPHA 0
 #define DH_5_D     5.795
 #define DH_5_R     0
+#define DH_5_MAX   170
+#define DH_5_MIN   -170
 
 
 class Kinematics {
