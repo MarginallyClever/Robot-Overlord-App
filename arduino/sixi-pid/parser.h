d68c9d6cb5c224b60ea6f0bd3600993214e116f4
Dan Royer <dan@marginallyclever.com>
2020-06-09 16:01:52 -0700

    updated descriptions

--- arduino/sixi-pid/parser.h
+++ arduino/sixi-pid/parser.h
@@ -37,31 +37,33 @@ public:
   // called by update when an entire command is received.
   void processCommand();
 
-  void M110();  // set and report line number.  M110 [Nn] sets next expected line number to n.
-  void M111();  // set and report debug level.  M111 [Sn] sets flags to n.  Combine any valid flags: 1 (echo)
-  void M112();  // M112 emergency stop.  Set all PIDS to zero.
-  void M114();  // report current target position
-  void M206();  // set home offsets M206 [Xn] [Yn] [Zn] [Un] [Vn] [Wn]
-  void M306();  // adjust PID  M306 L[0...5] [Pn] [In] [Dn] and report new values.
-  void M428();  // set home position to the current raw angle values (don't use home position to adjust home position!)
-  void M500();  // save home offsets
-  void M501();  // load home offsets
-  void M502();  // reset the home offsets
-  void M503();  // report the home offsets
+  // COMMANDS
+  // w means whole number.  b means binary (0/1).  d means decimal number. [] means optional.
   
-  void D17();   // report sensor values
-  void D18();   // copy sensor values to motor step positions
-  //void D19();   // toggle continuous D17 reporting
-  //void D20();
-  //void D21();  
-  void D22();   // Save all PID values to EEPROM
+  void M110();  // M110 [Nw] - Set and report line number.  If no number given, report only.
+  void M111();  // M111 [Sw] - Ssets flags to n.  If no number given, report only.  Combine any valid flags: 1 (echo)
+  void M112();  // M112 - Emergency stop.  Set all PIDS to zero.
+  void M114();  // - Report current target position, max feedrate, and max acceleration.
+  void M206();  // M206 [Xd] [Yd] [Zd] [Ud] [Vd] [Wd] - Set and report home offsets.  If no number given, report only.
+  void M306();  // M306 L(0...5) [Pd] [Id] [Dd] - Adjust PID and report new values.  If no number given, report only.  L is the joint number where L0 is X L5 is W.  
+  void M428();  // - Set home position to the current raw angle values.  As soon as this executes the robot will believe the joint angles are X0 Y-90 Z0 U0 V0 W0.
+  void M500();  // - Save home offsets.
+  void M501();  // - Load home offsets.
+  void M502();  // - Reset the home offsets to zero.
+  void M503();  // - Report the home offsets.
   
-  void D50();   // Set and report strict mode.  D50 [Sn] where n=0 for off and 1 for on.
+  void D17();  // - Report adjusted sensor values.
+  void D18();  // - Copy sensor values to motor step positions.  Deprecated?
+  void D19();  // - Toggle continuous D17 reporting.
+  void D20();  // - Turn off position error.
+  void D21();  // D21 [Pb] - Set and report limit checking.
+  void D22();  // - Save all PID values to EEPROM.
+  void D50();  // D50 [Sb] - Set and report strict mode.  When strict mode is on all commands MUST have a checksum.
   
-  void G01();  // G0/G1 [Xn] [Yn] [Zn] [Un] [Vn] [Wn]
-  void G28();  // go home
-  void G90();  // set absolute mode
-  void G91();  // set relative mode
+  void G01();  // G0/G1 [Xd] [Yd] [Zd] [Ud] [Vd] [Wd] - Set the target adjusted joint angles to the values provided.  If a value is not given, the target angle for that joint does not change.
+  void G28();  // - Go to home position
+  void G90();  // - Set absolute mode
+  void G91();  // - Set relative mode
 };
 
 extern Parser parser;
