99cd55f985ba6dec4335e224a518ad96a452b15b
Dan Royer <dan@marginallyclever.com>
2020-08-05 09:45:59 -0700

    added convenience methods

--- src/main/java/com/marginallyclever/convenience/MatrixHelper.java
+++ src/main/java/com/marginallyclever/convenience/MatrixHelper.java
@@ -676,10 +676,14 @@ public class MatrixHelper {
 	}
 
 	public static Vector3d getPosition(Matrix4d m) {	return new Vector3d(m.m03, m.m13, m.m23);	}
-	
 	public static Vector3d getXAxis(Matrix4d m) {	return new Vector3d(m.m00, m.m10, m.m20);	}
 	public static Vector3d getYAxis(Matrix4d m) {	return new Vector3d(m.m01, m.m11, m.m21);	}
 	public static Vector3d getZAxis(Matrix4d m) {	return new Vector3d(m.m02, m.m12, m.m22);	}
+
+	public static void setPosition(Matrix4d m,Vector3d v) {	m.m03=v.x;  m.m13=v.y;  m.m23=v.z;	}
+	public static void setXAxis(Matrix4d m,Vector3d v) {	m.m00=v.x;  m.m10=v.y;  m.m20=v.z;	}
+	public static void setYAxis(Matrix4d m,Vector3d v) {	m.m01=v.x;  m.m11=v.y;  m.m21=v.z;	}
+	public static void setZAxis(Matrix4d m,Vector3d v) {	m.m02=v.x;  m.m12=v.y;  m.m22=v.z;	}
 
 	/**
 	 * normalize the 3x3 component of the mTarget matrix.  Do not affect position. 
