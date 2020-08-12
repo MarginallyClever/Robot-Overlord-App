4a225a5fa36a22577d7cfc4e4b4a98636133cb87
Dan Royer <dan@marginallyclever.com>
2020-08-12 13:41:19 -0700

    recording tracks are now children of recording entity

--- src/main/java/com/marginallyclever/robotOverlord/entity/scene/recording/RecordingEntity.java
+++ src/main/java/com/marginallyclever/robotOverlord/entity/scene/recording/RecordingEntity.java
@@ -1,27 +1,30 @@
 package com.marginallyclever.robotOverlord.entity.scene.recording;
 
 import java.io.File;
+
+import javax.sound.midi.Track;
 import javax.swing.filechooser.FileFilter;
 import javax.swing.filechooser.FileNameExtensionFilter;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.Observable;
 import java.util.Observer;
 
 import com.marginallyclever.robotOverlord.entity.Entity;
 import com.marginallyclever.robotOverlord.entity.basicDataTypes.BooleanEntity;
+import com.marginallyclever.robotOverlord.entity.basicDataTypes.DoubleEntity;
 import com.marginallyclever.robotOverlord.entity.basicDataTypes.StringEntity;
 import com.marginallyclever.robotOverlord.entity.scene.PoseEntity;
 import com.marginallyclever.robotOverlord.log.Log;
-import com.marginallyclever.robotOverlord.swingInterface.view.ViewElement;
+import com.marginallyclever.robotOverlord.swingInterface.view.ViewElementButton;
 import com.marginallyclever.robotOverlord.swingInterface.view.ViewPanel;
 
 /**
- * Records (or plays back) one target entity
- * 
+ * Multitrack recording/playback for a target entity.  Child RecordingTrack entities are the tracks in the recording.
  * @author Dan Royer
- *
+ * TODO make this system undoable.
  */
 public class RecordingEntity extends Entity {
 	/**
@@ -35,14 +38,22 @@ public class RecordingEntity extends Entity {
 	public BooleanEntity validTarget = new BooleanEntity("Valid target",false);
 	private PoseEntity targetEntity = null;
 	
-	public LinkedList<RecordingTrack> trackList = new LinkedList<RecordingTrack>(); 
+	// where is the playhead in the sequence of tracks?
+	public DoubleEntity playHead = new DoubleEntity("Play head (s)",0);
+	// what is the total sequence length?
+	public DoubleEntity sequenceLength = new DoubleEntity("Sequence length (s)",0);
+	public boolean isPlaying=false;
+	
+	// needed as a reference for other button actions.
+	protected ViewElementButton playStop;
 	
 	public RecordingEntity() {
 		super("Recording");
 		addChild(pathToFile);
 		addChild(targetName);
 		addChild(validTarget);
-		//addChild(keyframe);
+		addChild(playHead);
+		addChild(sequenceLength);
 		
 		targetName.addObserver(new Observer() {
 			@Override
@@ -68,6 +79,7 @@ public class RecordingEntity extends Entity {
 				} else if(o==targetEntity) {
 					// target has changed, set current keyframe to the new pose.
 					Log.message("Change to "+targetEntity.getFullPath());
+					
 				}
 			}
 		});
@@ -82,38 +94,165 @@ public class RecordingEntity extends Entity {
 		
 		view.add(targetName);
 		view.add(validTarget).setReadOnly(true);
-		view.addButton("New").addObserver(new Observer() {
+		ViewElementButton toStart  = view.addButton("|◄");
+		ViewElementButton keyPrev  = view.addButton("◄◄");
+						  playStop = view.addButton("||");
+		ViewElementButton keyNext  = view.addButton("►►");
+		ViewElementButton toEnd    = view.addButton("►|");
+
+		ViewElementButton bAddTrack = view.addButton("Add track");
+		ViewElementButton bNew     = view.addButton("New");
+		ViewElementButton bLoad    = view.addButton("Load");
+		ViewElementButton bSave    = view.addButton("Save");
+
+		view.popStack();
+		
+		toStart.addObserver(new Observer() {
+			@Override
+			public void update(Observable o, Object arg) {
+				stop();
+				playHead.set(0.0);
+			}
+		});
+		keyPrev.addObserver(new Observer() {
+			@Override
+			public void update(Observable o, Object arg) {
+				stop();
+				playHead.set(findEventBefore(playHead.get()));
+			}
+		});
+		playStop.addObserver(new Observer() {
+			@Override
+			public void update(Observable o, Object arg) {
+				if(isPlaying) stop();
+				else          start();
+			}
+		});
+		keyNext.addObserver(new Observer() {
+			@Override
+			public void update(Observable o, Object arg) {
+				stop();
+				playHead.set(findEventAfter(playHead.get()));
+			}
+		});
+		toEnd.addObserver(new Observer() {
+			@Override
+			public void update(Observable o, Object arg) {
+				stop();
+				playHead.set(sequenceLength.get());
+			}
+		});
+		
+		bNew.addObserver(new Observer() {
 			@Override
 			public void update(Observable o, Object arg) {
 				resetTrackList();
 				
 			}
 		});;
-		view.addButton("Load").addObserver(new Observer() {
+		bLoad.addObserver(new Observer() {
 			@Override
 			public void update(Observable o, Object arg) {
 				loadTrackList();
 			}
 		});;
-		view.addButton("Save").addObserver(new Observer() {
+		bSave.addObserver(new Observer() {
 			@Override
 			public void update(Observable o, Object arg) {
 				saveTrackList();
 			}
 		});
-		view.popStack();
+		
+		bAddTrack.addObserver(new Observer() {
+			@Override
+			public void update(Observable o, Object arg) {
+				addTrack();
+			}
+		});
 		super.getView(view);
+	}
+
+	/**
+	 * Scan all RecordingTrack, find the AbstractRecordingEvent X where X.time &lt; t. 
+	 * @param t
+	 * @return X.time, as described.
+	 */
+	protected double findEventBefore(double t) {
+		double nearestTime = 0.0;
+		
+		for(Entity track : children) {
+			if(track instanceof RecordingTrack) {
+				Iterator<AbstractRecordingEvent> iter = ((RecordingTrack)track).events.iterator();
+				while(iter.hasNext()) {
+					AbstractRecordingEvent are = iter.next();
+					if(are.time>=t) break;
+					nearestTime = Math.max(nearestTime,t);
+				}
+			}
+		}
+		return nearestTime;
+	}
+	
+	/**
+	 * Scan all RecordingTrack, find the AbstractRecordingEvent X where X.time &gt; t. 
+	 * @param t
+	 * @return X.time, as described.
+	 */
+	protected double findEventAfter(double t) {
+		double nearestTime = Double.MAX_VALUE;
+
+		for(Entity track : children) {
+			if(track instanceof RecordingTrack) {
+				Iterator<AbstractRecordingEvent> iter = ((RecordingTrack)track).events.descendingIterator();
+				while(iter.hasNext()) {
+					AbstractRecordingEvent are = iter.next();
+					if(are.time<=t) break;
+					nearestTime = Math.min(nearestTime,t);
+				}
+			}
+		}
+		return nearestTime;
+	}
+	
+	protected void stop() {
+		isPlaying=false;
+		playStop.setText(isPlaying?"■":"►");
+	}
+	
+	protected void start() {
+		isPlaying=true;
+		playStop.setText(isPlaying?"■":"►");
 	}
 	
 	protected void resetTrackList() {
-		trackList = new LinkedList<RecordingTrack>(); 
+		stop();
+		// remove all children
+		while(!this.children.isEmpty()) {
+			removeChild(this.children.get(0));
+		}
 	}
 	
 	protected void saveTrackList() {
-		
+		stop();
+		// TODO open file; write out contents; close file
 	}
 	
 	protected void loadTrackList() {
+		stop();
+		// TODO open file; read in contents; close file
+	}
+	
+	protected void addTrack() {
+		int f = 0;
+		for( Entity track : children ) {
+			if(track instanceof RecordingTrack) {
+				String [] parts = track.getName().split("\\s+");
+				int id = Integer.parseInt(parts[1]);
+				f = Math.max(f,id);
+			}
+		}
+		RecordingTrack newTrack = new RecordingTrack("Track "+(f+1));
 		
+		addChild(newTrack);
 	}
 }
