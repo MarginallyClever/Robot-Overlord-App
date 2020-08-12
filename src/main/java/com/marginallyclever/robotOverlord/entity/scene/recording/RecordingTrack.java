c1cecb87bda1e7489a2b95989632e4cd58ecda84
Dan Royer <dan@marginallyclever.com>
2020-08-12 13:15:57 -0700

    Update RecordingTrack.java

--- src/main/java/com/marginallyclever/robotOverlord/entity/scene/recording/RecordingTrack.java
+++ src/main/java/com/marginallyclever/robotOverlord/entity/scene/recording/RecordingTrack.java
@@ -2,11 +2,15 @@ package com.marginallyclever.robotOverlord.entity.scene.recording;
 
 import java.util.LinkedList;
 
+/**
+ * A single track in a multi-track sequence.  A track has a name and a list of events.
+ * @author aggra
+ *
+ */
 public class RecordingTrack {
 	// name of track
 	public String trackName;
 	public LinkedList<AbstractRecordingEvent> events = new LinkedList<AbstractRecordingEvent>();
-	public String extraCommands;
 	
 	public RecordingTrack() {}
 }
