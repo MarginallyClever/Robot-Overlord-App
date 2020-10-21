0d9cda20120917013dbeb620c4e379efc9a39f5b
Dan Royer <dan@marginallyclever.com>
2020-10-21 13:01:51 -0700

    Create MementoDoubleArray.java

---
+++ src/main/java/com/marginallyclever/convenience/memento/MementoDoubleArray.java
@@ -0,0 +1,20 @@
+package com.marginallyclever.convenience.memento;
+
+public class MementoDoubleArray implements Memento, Cloneable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -2161449213837224638L;
+	public double [] values; 
+	
+	public MementoDoubleArray(int size) {
+		values = new double[size];
+	}
+	
+	@Override
+	protected Object clone() throws CloneNotSupportedException {
+		MementoDoubleArray a = (MementoDoubleArray)super.clone();
+		a.values = values.clone();
+		return a;
+	}
+}
