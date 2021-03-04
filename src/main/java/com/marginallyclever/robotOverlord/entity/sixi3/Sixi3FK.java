8fc2d91e7dee0f3df8c235330c9fc4439afe4062
Dan Royer <dan@marginallyclever.com>
2021-03-04 14:01:53 -0800

    better separation of fk and ik models

--- src/main/java/com/marginallyclever/robotOverlord/entity/sixi3/Sixi3FK.java
+++ src/main/java/com/marginallyclever/robotOverlord/entity/sixi3/Sixi3FK.java
@@ -1,6 +1,10 @@
 package com.marginallyclever.robotOverlord.entity.sixi3;
 
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import java.security.InvalidParameterException;
 import java.util.ArrayList;
+import java.util.concurrent.locks.ReentrantLock;
 
 import javax.vecmath.Matrix4d;
 import javax.vecmath.Vector3d;
@@ -10,15 +14,15 @@ import com.marginallyclever.convenience.Cuboid;
 import com.marginallyclever.convenience.MatrixHelper;
 import com.marginallyclever.convenience.OpenGLHelper;
 import com.marginallyclever.convenience.PrimitiveSolids;
-import com.marginallyclever.robotOverlord.entity.basicDataTypes.DoubleEntity;
 import com.marginallyclever.robotOverlord.entity.scene.Collidable;
 import com.marginallyclever.robotOverlord.entity.scene.PoseEntity;
 import com.marginallyclever.robotOverlord.entity.scene.shapeEntity.ShapeEntity;
+import com.marginallyclever.robotOverlord.swingInterface.view.ViewElementButton;
 import com.marginallyclever.robotOverlord.swingInterface.view.ViewPanel;
 
 /**
  * Simulation of a Sixi3 robot arm with Forward Kinematics based on Denavit Hartenberg parameters.
- * It manages a set of {@link Sixi3Link}.
+ * It manages a set of {@link Sixi3Bone}.
  * @see <a href='https://en.wikipedia.org/wiki/Denavit%E2%80%93Hartenberg_parameters'>DH parameters</a>
  * @see <a href='https://en.wikipedia.org/wiki/Forward_kinematics'>Forward Kinematics</a>
  * @author Dan Royer
@@ -47,60 +51,43 @@ public class Sixi3FK extends PoseEntity implements Collidable {
 	private static final String HAND_MODEL       = "/Sixi3/Sixi3 Hand DIN EN ISO 9409-1-50-4-M6 v6.obj";
 	private static final String BASE_MODEL       = "/Sixi3/base v7.obj";
 	private static final String ACTUATOR_TEXTURE = "/Sixi3/actuator-texture.png";
-	
-	protected Sixi3Link [] links = new Sixi3Link[6];
 
+	public static final int NUM_BONES = 6;
+	
 	// unmoving model of the robot base.
 	private ShapeEntity base;
+	
+	// DH parameters, meshes, physical limits.
+	private Sixi3Bone [] bones = new Sixi3Bone[NUM_BONES];
 
 	// end effector
-	protected Matrix4d ee = new Matrix4d();
-
-	// forward kinematics sliders
-	protected DoubleEntity J0 = new DoubleEntity("J0",0);
-	protected DoubleEntity J1 = new DoubleEntity("J1",0);
-	protected DoubleEntity J2 = new DoubleEntity("J2",0);
-	protected DoubleEntity J3 = new DoubleEntity("J3",0);
-	protected DoubleEntity J4 = new DoubleEntity("J4",0);
-	protected DoubleEntity J5 = new DoubleEntity("J5",0);
+	private Matrix4d ee = new Matrix4d();
 	
 	
 	public Sixi3FK() {
 		super();
-		setName("Sixi 3");
+		setName("Sixi3FK");
 		
 		setupModel();
 		
-		J0.addPropertyChangeListener(this);
-		J1.addPropertyChangeListener(this);
-		J2.addPropertyChangeListener(this);
-		J3.addPropertyChangeListener(this);
-		J4.addPropertyChangeListener(this);
-		J5.addPropertyChangeListener(this);
-		
-		J0.set(180.0);
-		J1.set(180.0);
-		J2.set(180.0);
-		J3.set(180.0);
-		J4.set(180.0);
-		J5.set(180.0);
-
-		for( Sixi3Link bone : links ) {
-			bone.updateMatrix();
+		int i=0;
+		for( Sixi3Bone b : bones ) {
+			b.slider.setName("J"+(i++));
+			b.slider.addPropertyChangeListener(this);
+			b.slider.set(180.0);
+			b.updateMatrix();
 		}
-		
-		getEndEffector(ee);
 	}
 
 	/**
 	 * Set up the DH link hierarchy according to the DH parameters.  Also load the shapes.  
 	 * The physical location of the shapes does not match the DH linkage description of the robot, 
-	 * so adjust the {@link Sixi3Link.shapeOffset} of each bone to compensate.
+	 * so adjust the {@link Sixi3Bone.shapeOffset} of each bone to compensate.
 	 */
 	private void setupModel() {
 		// memory allocation
-		for( int i=0;i<links.length;++i ) {
-			links[i] = new Sixi3Link();
+		for( int i=0;i<bones.length;++i ) {
+			bones[i] = new Sixi3Bone();
 		}
 		
 		// The DH parameters
@@ -109,12 +96,12 @@ public class Sixi3FK extends PoseEntity implements Collidable {
 		double d3=LENGTH_A+LENGTH_B;
 		double d4=d3;
 		double d5=LENGTH_A+HAND_HEIGHT;
-		links[0].set(0 ,d0,-90,0,ACTUATOR_MODEL);
-		links[1].set(r2, 0,  0,0,BICEP_MODEL   );
-		links[2].set(0 , 0, 90,0,ACTUATOR_MODEL);
-		links[3].set(0 ,d3, 90,0,ACTUATOR_MODEL);
-		links[4].set(0 ,d4, 90,0,ACTUATOR_MODEL);
-		links[5].set(0 ,d5,  0,0,HAND_MODEL    );
+		bones[0].set(0 ,d0,-90,0,350,10,ACTUATOR_MODEL);
+		bones[1].set(r2, 0,  0,0,350,10,BICEP_MODEL   );
+		bones[2].set(0 , 0, 90,0,350,10,ACTUATOR_MODEL);
+		bones[3].set(0 ,d3, 90,0,350,10,ACTUATOR_MODEL);
+		bones[4].set(0 ,d4,-90,0,350,10,ACTUATOR_MODEL);
+		bones[5].set(0 ,d5,  0,0,361,-1,HAND_MODEL    );  // can turn forever
 
 		// load the base shape.
 		base = new ShapeEntity(BASE_MODEL);
@@ -128,39 +115,39 @@ public class Sixi3FK extends PoseEntity implements Collidable {
 		m1.rotY(Math.toRadians(-90));
 		m2.mul(m1,m0);
 		m2.m13=LENGTH_A;
-		links[0].shape.setPose(m2);
+		bones[0].shape.setPose(m2);
 
 		m2.setIdentity();
 		m2.rotX(Math.toRadians(-90));
 		m2.m03=-(LENGTH_B+CONNECTOR_HEIGHT)*2;
 		m2.m23=LENGTH_B;
-		links[1].shape.setPose(m2);
-		
+		bones[1].shape.setPose(m2);
+
 		m0.rotX(Math.toRadians(180));
-		m1.rotZ(Math.toRadians(-90));
+		m1.rotZ(Math.toRadians(90));
 		m2.mul(m1,m0);
 		m2.m23=LENGTH_A;
-		links[2].shape.setPose(m2);
+		bones[2].shape.setPose(m2);
 
 		m0.rotX(Math.toRadians(180));
 		m1.rotZ(Math.toRadians(-90));
 		m2.mul(m1,m0);
 		m2.m23=LENGTH_A;
-		links[3].shape.setPose(m2);
+		bones[3].shape.setPose(m2);
 
-		m0.rotZ(Math.toRadians(90));
+		m0.rotZ(Math.toRadians(-90));
 		m1.rotX(Math.toRadians(180));
 		m2.mul(m1,m0);
 		m2.m23=LENGTH_A;
-		links[4].shape.setPose(m2);
+		bones[4].shape.setPose(m2);
 
 		m2.rotZ(Math.toRadians(-90));
 		m2.m23=0;
-		links[5].shape.setPose(m2);
+		bones[5].shape.setPose(m2);
 		
 		// set material properties for each part of this model.
 		base.getMaterial().setTextureFilename(ACTUATOR_TEXTURE);
-		for( Sixi3Link bone : links ) {
+		for( Sixi3Bone bone : bones ) {
 			bone.shape.getMaterial().setTextureFilename(ACTUATOR_TEXTURE);
 		}
 	}
@@ -168,32 +155,14 @@ public class Sixi3FK extends PoseEntity implements Collidable {
 	@Override
 	public void update(double dt) {
 		super.update(dt);
-
-		Matrix4d m0=new Matrix4d();
-		Matrix4d m1=new Matrix4d();
-		Matrix4d m2=new Matrix4d();
-		m0.rotX(Math.toRadians(180));
-		m1.rotZ(Math.toRadians(90));
-		m2.mul(m1,m0);
-		m2.m23=LENGTH_A;
-		links[2].shape.setPose(m2);
-		
-		// update the end effector
-		getEndEffector(ee);
 	}
 	
 	@Override
 	public void render(GL2 gl2) {
 		gl2.glPushMatrix();
 			MatrixHelper.applyMatrix(gl2, pose);
-
-			gl2.glPushMatrix();
-				// draw the meshes
-				drawMeshes(gl2);
-				drawExtras(gl2);
-			gl2.glPopMatrix();
-		
-			MatrixHelper.drawMatrix2(gl2, ee, 6);
+			drawMeshes(gl2);
+			drawExtras(gl2);
 		gl2.glPopMatrix();
 		
 		super.render(gl2);
@@ -203,7 +172,7 @@ public class Sixi3FK extends PoseEntity implements Collidable {
 		base.render(gl2);
 
 		gl2.glPushMatrix();
-		for( Sixi3Link bone : links ) {
+		for( Sixi3Bone bone : bones ) {
 			// draw model with local shape offset
 			bone.updateMatrix();
 			MatrixHelper.applyMatrix(gl2, bone.pose);
@@ -223,38 +192,44 @@ public class Sixi3FK extends PoseEntity implements Collidable {
 		gl2.glDisable(GL2.GL_LIGHTING);
 		// draw on top of everything else
 		int wasOver=OpenGLHelper.drawAtopEverythingStart(gl2);
-		gl2.glPushMatrix();
+		
 		if(showLineage.get()) {
 			// then the bones, overtop and unlit.
-			int j=links.length+1;
-			for(int i=0;i<links.length;++i) {
-				Sixi3Link bone = links[i];
-				bone.updateMatrix();
-				// draw bone origin
-				PrimitiveSolids.drawStar(gl2,j--);
-	
-				bone.pose.get(v);
-				gl2.glColor3d(1, 0, 1);
-				gl2.glBegin(GL2.GL_LINES);
-				gl2.glVertex3d(0, 0, 0);
-				gl2.glVertex3d(v.x,v.y,v.z);
-				gl2.glEnd();
-				MatrixHelper.applyMatrix(gl2, bone.pose);
-			}
+			gl2.glPushMatrix();
+				int j=bones.length+1;
+				for(int i=0;i<bones.length;++i) {
+					Sixi3Bone bone = bones[i];
+					bone.updateMatrix();
+					// draw bone origin
+					PrimitiveSolids.drawStar(gl2,j--);
+		
+					bone.pose.get(v);
+					gl2.glColor3d(1, 0, 1);
+					gl2.glBegin(GL2.GL_LINES);
+					gl2.glVertex3d(0, 0, 0);
+					gl2.glVertex3d(v.x,v.y,v.z);
+					gl2.glEnd();
+					MatrixHelper.applyMatrix(gl2, bone.pose);
+				}
+			gl2.glPopMatrix();
 		}
-		gl2.glPopMatrix();
 		
 		// bounding boxes are always relative to base?
 		if(showBoundingBox.get()) {
 			Matrix4d w = new Matrix4d();
 			ArrayList<Cuboid> list = getCuboidList();
+			gl2.glPushMatrix();
+			getPoseWorld(w);
+			w.invert();
+			MatrixHelper.applyMatrix(gl2, w);
 			for(Cuboid c : list) {
-				gl2.glPushMatrix();
-				c.getPose(w);
-				MatrixHelper.applyMatrix(gl2, w);
 				c.render(gl2);
-				gl2.glPopMatrix();
 			}
+			gl2.glPopMatrix();
+		}
+		
+		if(showLocalOrigin.get()) {
+			MatrixHelper.drawMatrix2(gl2, ee, 6);
 		}
 
 		// return state if needed
@@ -269,7 +244,7 @@ public class Sixi3FK extends PoseEntity implements Collidable {
 	 */
 	protected void getEndEffector(Matrix4d m) {
 		m.setIdentity();
-		for( Sixi3Link bone : links ) {
+		for( Sixi3Bone bone : bones ) {
 			m.mul(bone.pose);
 		}
 	}
@@ -277,23 +252,110 @@ public class Sixi3FK extends PoseEntity implements Collidable {
 	@Override
 	public void getView(ViewPanel view) {
 		view.pushStack("FK","Forward Kinematics");
-		view.addRange(J0, 350, 10);
-		view.addRange(J1, 350, 10);
-		view.addRange(J2, 350, 10);
-		view.addRange(J3, 350, 10);
-		view.addRange(J4, 350, 10);
-		view.addRange(J5, 350, 10);
+		for( Sixi3Bone b : bones ) {
+			view.addRange(b.slider,(int)b.angleMax,(int)b.angleMin);
+		}
+		ViewElementButton b = view.addButton("Center all");
+		b.addPropertyChangeListener(new PropertyChangeListener() {
+			@Override
+			public void propertyChange(PropertyChangeEvent evt) {
+				//*
+				double [] v = getFKValues();
+				for(int i=0;i<v.length;++i) {
+					v[i]=180;
+				}
+				setFKValues(v);/*/
+				for( Sixi3Bone b : bones ) {
+					b.slider.set((b.angleMax+b.angleMin)/2);
+				}*/
+			}
+		});
 		view.popStack();
 		
 		super.getView(view);
 	}
+
+	public double [] getFKValues() {
+		double [] list = new double[bones.length];
+		int i=0;
+		for( Sixi3Bone bone : bones ) {
+			list[i++] = bone.theta;
+		}
+		return list;
+	}
+	
+	private ReentrantLock setFKLock = new ReentrantLock();
 	
 	/**
-	 * The elements in this {@link Cuboid} list are relative the base of this robot.
-	 * Since the {@link ShapeEntity} of a {@link @Sixi3Link} is not the same place as the bone represented by that link,
-	 * The {@link Cuboid} for each {@link @ShapeEntity} has to be adjusted by both the bone pose (relative to base) and 
-	 * then the shape entity pose.
+	 * Update the theta angles of each bone in the robot and the FK sliders on the panel.
+	 * @param list new theta values.
+	 * @return true if new values are different from old values.
+	 * @throws InvalidParameterException list is the wrong length.
 	 */
+	public boolean setFKValues(double [] list) throws InvalidParameterException {
+		if(bones.length != list.length) {
+			throw new InvalidParameterException("list length must match number of links ("+bones.length+")");
+		}
+
+		boolean changed=false;
+		
+		if(!setFKLock.isLocked()) {
+			setFKLock.lock();
+			
+			int i=0;
+			for( Sixi3Bone b : bones ) {
+				double v = list[i++];
+				// prevent pushing the arm to an illegal angle
+				v = Math.max(Math.min(v, b.angleMax), b.angleMin);
+				
+				if( b.theta != v ) {
+					b.theta = v;
+					changed=true;
+				}
+				b.slider.set(b.theta);
+			}
+	
+			if(changed) {
+				// theta values actually changed so update matrixes and get the new end effector position.
+				for( Sixi3Bone b : bones ) {
+					b.updateMatrix();
+				}
+				Matrix4d oldee = new Matrix4d(ee);
+				getEndEffector(ee);
+				notifyPropertyChangeListeners(new PropertyChangeEvent(this,"ee",oldee,ee));
+			}
+			
+			setFKLock.unlock();
+		}		
+		return changed;
+	}
+	
+	// When GUI elements are changed they each cause a {@link PropertyChangeEvent}.
+	@Override
+	public void propertyChange(PropertyChangeEvent evt) {
+		super.propertyChange(evt);
+		Object src = evt.getSource();
+
+		double [] v = getFKValues();
+		
+		boolean changed=false;
+		int i=0;
+		for( Sixi3Bone b : bones ) {
+			if(src == b.slider) {
+				double d = b.slider.get();
+				if( v[i] != d ) {
+					v[i] = d;
+					changed=true;
+				}
+			}
+			i++;
+		}
+
+		if(changed) {
+			setFKValues(v);
+		}
+	}
+	
 	@Override
 	public ArrayList<Cuboid> getCuboidList() {
 		ArrayList<Cuboid> list = new ArrayList<Cuboid>();
@@ -311,7 +373,7 @@ public class Sixi3FK extends PoseEntity implements Collidable {
 			c.setPose(cuboidAfter);
 		}
 		
-		for( Sixi3Link bone : links ) {
+		for( Sixi3Bone bone : bones ) {
 			// add bone to current pose
 			currentBonePose.mul(bone.pose);
 			
--- src/main/java/com/marginallyclever/robotOverlord/entity/sixi3/Sixi3IK.java
+++ src/main/java/com/marginallyclever/robotOverlord/entity/sixi3/Sixi3IK.java
@@ -1,14 +1,14 @@
 package com.marginallyclever.robotOverlord.entity.sixi3;
 
 import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
 
 import javax.vecmath.Matrix4d;
 import javax.vecmath.Vector3d;
 
 import com.marginallyclever.convenience.MatrixHelper;
-import com.marginallyclever.robotOverlord.entity.basicDataTypes.DoubleEntity;
-import com.marginallyclever.robotOverlord.entity.basicDataTypes.IntEntity;
 import com.marginallyclever.robotOverlord.entity.scene.PoseEntity;
+import com.marginallyclever.robotOverlord.swingInterface.view.ViewElementButton;
 import com.marginallyclever.robotOverlord.swingInterface.view.ViewPanel;
 
 /**
@@ -25,84 +25,44 @@ public class Sixi3IK extends Sixi3FK {
 	 */
 	private static final long serialVersionUID = -7778520191789995554L;
 
-	// axis for ik
-	static private final String [] axisLabels = new String[] { "X","Y","Z","Xr","Yr","Zr"};
+	private double THRESHOLD = 0.001;
 	
-	// which axis do we want to move?
-	private IntEntity axisChoice = new IntEntity("Jog direction",0);
-	// how fast do we want to move?
-	private DoubleEntity axisAmount = new DoubleEntity("Jog speed",0);
 	// target end effector pose
-	private PoseEntity ee2 = new PoseEntity();
-	
-	private boolean applyingIK;
-
-	// how big a step to take with each partial descent?
-	private double [] samplingDistances;
-	
+	private PoseEntity eeTarget = new PoseEntity("Target");
+		
 	public Sixi3IK() {
 		super();
-		setName("Sixi 3");
+		setName("Sixi3IK");
 
-		samplingDistances = new double[links.length];
+		addChild(eeTarget);
+		eeTarget.addPropertyChangeListener(this);
 		
-		axisChoice.addPropertyChangeListener(this);
-
-		addChild(ee2);
-		ee2.addPropertyChangeListener(this);
-		
-		applyingIK=true;
-		ee2.setPose(ee);
-		
-		applyingIK=false;
+		Matrix4d m = new Matrix4d();
+		getEndEffector(m);
+		eeTarget.setPose(m);
 	}
 	
 	@Override
 	public void update(double dt) {
 		super.update(dt);
 		
-		if(axisAmount.get()!=0) {
-			double aa = axisAmount.get();
-			int ac = axisChoice.get();
-
-			aa*=dt;
-
-			Matrix4d target = ee2.getPose();
-			
-			Vector3d p = new Vector3d(target.m03,target.m13,target.m23);
-			target.setTranslation(new Vector3d(0,0,0));
-			Matrix4d r = new Matrix4d();
-			r.setIdentity();
-			
-			switch(ac) {
-			case 0:				p.x+=aa;					break;
-			case 1:				p.y+=aa;					break;
-			case 2:				p.z+=aa;					break;
-			case 3:				r.rotX(Math.toRadians(aa));	break;
-			case 4:				r.rotY(Math.toRadians(aa));	break;
-			case 5:				r.rotZ(Math.toRadians(aa));	break;
-			default: 										break;
-			}
-			target.mul(r);
-			target.setTranslation(p);
-			ee2.setPose(target);
-			// which will cause a propertyChange event
-			
-			axisAmount.set(0.0);
+		if(distanceToTarget() > THRESHOLD) {
+			// gradient descent towards eeTarget
+			gradientDescent(50, THRESHOLD, 0.125, 0.005);
 		}
 	}
 	
 	/**
 	 * Measures the difference beween the latest end effector matrix and the target matrix
-	 * @return
+	 * @return the error term.
 	 */
-	private double gradientDescentErrorTerm() {
+	public double distanceToTarget() {
 		// Scale the "handles" used.  Bigger scale, greater rotation compensation.
 		final double GRADIENT_DESCENT_ERROR_TERM_ROTATION_SCALE = 100;
 		
 		Matrix4d m = new Matrix4d();
 		getEndEffector(m);
-		Matrix4d target = ee2.getPose();
+		Matrix4d target = eeTarget.getPose();
 		
 		// linear difference in centers
 		Vector3d c0 = new Vector3d();
@@ -132,25 +92,25 @@ public class Sixi3IK extends Sixi3FK {
 		return dC+dX+dY;
 	}
 	
-	private double partialGradientDescent(int i) {
-		Sixi3Link bone = links[i];
+	/**/
+	private double partialGradientDescent(double [] fk,double [] samplingDistances, int i) {
 		// get the current error term F.
-		double oldValue = bone.theta;
-		double Fx = gradientDescentErrorTerm();
+		double oldValue = fk[i];
+		double Fx = distanceToTarget();
 
 		// move F+D, measure again.
-		bone.theta = oldValue + samplingDistances[i];
-		bone.updateMatrix();
-		double FxPlusD = gradientDescentErrorTerm();
+		fk[i] = oldValue + samplingDistances[i];
+		setFKValues(fk);
+		double FxPlusD = distanceToTarget();
 
 		// move F-D, measure again.
-		bone.theta = oldValue - samplingDistances[i];
-		bone.updateMatrix();
-		double FxMinusD = gradientDescentErrorTerm();
+		fk[i] = oldValue - samplingDistances[i];
+		setFKValues(fk);
+		double FxMinusD = distanceToTarget();
 
 		// restore the old value
-		bone.theta = oldValue;
-		bone.updateMatrix();
+		fk[i] = oldValue;
+		setFKValues(fk);
 
 		// if F+D and F-D have more error than F, try smaller step size next time. 
 		if( FxMinusD > Fx && FxPlusD > Fx ) {
@@ -167,101 +127,75 @@ public class Sixi3IK extends Sixi3FK {
 
 	/**
 	 * Use gradient descent to move the end effector closer to the target. 
-	 * @return true if the margin of error is within the threshold.
+	 * @return true if the margin of error is within the threshold (we have arrived).
+	 * @param iterations How many times should I try to get closer?
+	 * @param threshold When error term is within threshold then stop. 
+	 * @param learningRate how much of that partial descent to actually apply each step?
+	 * @param initialSampleSize How many times should I try to get closer?
 	 */
-	private boolean gradientDescent() {
-		// How many times should I try to get closer?
-		final int iterations = 50;
-		
-		// When distanceToTarget() score is within threshold then stop. 
-		final double threshold = 0.001;
-		
-		// how much of that partial descent to actually apply?
-		double learningRate=0.125;
-		
-		// the sensor resolution is 2^15 (32768 bits, 0.011 degree)
-		final double initialSampleSize = 0.005;
-		samplingDistances[0]=initialSampleSize; 
-		samplingDistances[1]=initialSampleSize;
-		samplingDistances[2]=initialSampleSize;
-		samplingDistances[3]=initialSampleSize;
-		samplingDistances[4]=initialSampleSize;
-		samplingDistances[5]=initialSampleSize;
-		
-		double dtt=gradientDescentErrorTerm();
-		if(dtt<threshold) return true;
+	private void gradientDescent(final double iterations, final double threshold, final double learningRate, final double initialSampleSize) {
+		double dtt=distanceToTarget();
 
+		// pose before gradient descent starts
+		double [] fk = getFKValues();
+
+		// how big a step to take with each partial descent?
+		double [] samplingDistances = new double[Sixi3FK.NUM_BONES];
+		for(int i=0;i<Sixi3FK.NUM_BONES;++i) {
+			samplingDistances[i]=initialSampleSize;
+		}
+		
 		for(int j=0;j<iterations;++j) {
 			// seems to work better descending from the finger than ascending from the base.
 			//for( int i=0; i<links.length; ++i ) {  // ascending mode
-			for( int i=links.length-1; i>=0; --i ) {  // descending mode
-				Sixi3Link bone = links[i];
-
-				double oldValue = bone.theta;
-				double gradient = partialGradientDescent(i);
-				double newValue = oldValue - gradient * learningRate; 
-				// cap the value to something sane
-				bone.theta = Math.max(Math.min(newValue, 350-1e-6), 10+1e-6);
-				bone.updateMatrix();
-		
-				dtt=gradientDescentErrorTerm();
-				if(dtt<threshold) return true;
+			for( int i=Sixi3FK.NUM_BONES-1; i>=0; --i ) {  // descending mode
+				double oldValue = fk[i];
+				double gradient = partialGradientDescent(fk,samplingDistances,i);
+				fk[i] = oldValue - gradient * learningRate; 
+				setFKValues(fk);
+						
+				dtt=distanceToTarget();
+				if(dtt<threshold) {
+					// we hit the target, stop early.
+					return;
+				}
 			}
 		}
-		
-		// Probably always false?
-		return dtt<threshold;
+		// if you get here the robot did not reach its target within 'iteration' steps.
+		// try tweaking your input parameters for better results.
 	}
 	
 	@Override
 	public void getView(ViewPanel view) {
 		view.pushStack("IK","Inverse Kinematics");
-		view.addComboBox(axisChoice, axisLabels);
-		view.addRange(axisAmount, 5, -5);
+
+		// TODO add gradient descent parameters here
+		
+		ViewElementButton b = view.addButton("Reset GoTo");
+		b.addPropertyChangeListener(new PropertyChangeListener() {
+			@Override
+			public void propertyChange(PropertyChangeEvent evt) {
+				Matrix4d m = new Matrix4d();
+				getEndEffector(m);
+				eeTarget.setPose(m);
+			}
+		});
+		
 		view.popStack();
 		
 		super.getView(view);
 	}
+
 	
-	/**
-	 * When GUI elements are changed they each cause a {@link PropertyChangeEvent}.
-	 */
+	// When GUI elements are changed they each cause a {@link PropertyChangeEvent}.
 	@Override
 	public void propertyChange(PropertyChangeEvent evt) {
 		super.propertyChange(evt);
 		Object src = evt.getSource();
-
-		if(src == axisChoice) {
-			if(axisAmount != null) axisAmount.set(0.0);
-		}
-		if(src == ee2 && evt.getPropertyName().contentEquals("pose")) {
-			// gradient descent towards ee2
-			gradientDescent();
-
-			if(!applyingIK) {
-				applyingIK=true;
-				J0.set(links[0].theta);
-				J1.set(links[1].theta);
-				J2.set(links[2].theta);
-				J3.set(links[3].theta);
-				J4.set(links[4].theta);
-				J5.set(links[5].theta);
-				applyingIK=false;
-			}
-		} else {
-			if(!applyingIK) {
-				applyingIK=true;
-				if(axisAmount != null) axisAmount.set(0.0);
-				if(src == J0) links[0].theta=J0.get();
-				if(src == J1) links[1].theta=J1.get();
-				if(src == J2) links[2].theta=J2.get();
-				if(src == J3) links[3].theta=J3.get();
-				if(src == J4) links[4].theta=J4.get();
-				if(src == J5) links[5].theta=J5.get();
-				getEndEffector(ee);
-				if(ee2 != null) ee2.setPose(ee);
-				applyingIK=false;
-			}
+		
+		if(src == eeTarget && evt.getPropertyName().contentEquals("pose")) {
+			// gradient descent towards eeTarget
+			gradientDescent(50, 0.001, 0.125, 0.005);
 		}
 	}
 }
