e460f5cced3b19969308d54d3b7951bee6e01910
Dan Royer <dan@marginallyclever.com>
2021-03-08 17:17:22 -0800

    added test (move target to see print out of velocity data)

--- src/main/java/com/marginallyclever/robotOverlord/entity/sixi3/Sixi3IK.java
+++ src/main/java/com/marginallyclever/robotOverlord/entity/sixi3/Sixi3IK.java
@@ -7,6 +7,7 @@ import javax.vecmath.Matrix4d;
 import javax.vecmath.Vector3d;
 
 import com.marginallyclever.convenience.MatrixHelper;
+import com.marginallyclever.convenience.StringHelper;
 import com.marginallyclever.robotOverlord.entity.basicDataTypes.DoubleEntity;
 import com.marginallyclever.robotOverlord.entity.scene.PoseEntity;
 import com.marginallyclever.robotOverlord.swingInterface.view.ViewElementButton;
@@ -29,7 +30,7 @@ public class Sixi3IK extends Sixi3FK {
 	// target end effector pose
 	private PoseEntity eeTarget = new PoseEntity("Target");
 
-	private DoubleEntity threshold = new DoubleEntity("Threshold",0.001); 
+	private DoubleEntity threshold = new DoubleEntity("Threshold",0.01); 
 	private DoubleEntity stepSize = new DoubleEntity("Step size",0.125); 
 	private DoubleEntity learningRate = new DoubleEntity("Leaning rate",0.005); 
 	
@@ -124,16 +125,15 @@ public class Sixi3IK extends Sixi3FK {
 	}
 
 	/**
-	 * Use gradient descent to move the end effector closer to the target. 
-	 * @return true if the margin of error is within the threshold (we have arrived).
+	 * Use gradient descent to move the end effector closer to the target.  The process is iterative, might not reach the target,
+	 * and changes depending on the position when gradient descent began. 
+	 * @return distance to target
 	 * @param iterations How many times should I try to get closer?
 	 * @param threshold When error term is within threshold then stop. 
 	 * @param learningRate how much of that partial descent to actually apply each step?
 	 * @param initialSampleSize How many times should I try to get closer?
 	 */
-	private void gradientDescent(final Matrix4d target,final double iterations, final double threshold, final double learningRate, final double initialSampleSize) {
-		double dtt=distanceToTarget(target);
-
+	private boolean gradientDescent(final Matrix4d target,final double iterations, final double threshold, final double learningRate, final double initialSampleSize) {
 		// pose before gradient descent starts
 		double [] fk = new double [Sixi3FK.NUM_BONES];
 		getFKValues(fk);
@@ -152,16 +152,18 @@ public class Sixi3IK extends Sixi3FK {
 				double gradient = partialGradientDescent(target,fk,samplingDistances,i);
 				fk[i] = oldValue - gradient * learningRate; 
 				setFKValues(fk);
-						
-				dtt=distanceToTarget(target);
-				if(dtt<threshold) {
+
+				double dtt=distanceToTarget(target);
+				if(dtt<=threshold) {
 					// we hit the target, stop early.
-					return;
+					return true;
 				}
 			}
 		}
+		
 		// if you get here the robot did not reach its target within 'iteration' steps.
 		// try tweaking your input parameters for better results.
+		return false;
 	}
 	
 	@Override
@@ -187,7 +189,6 @@ public class Sixi3IK extends Sixi3FK {
 		
 		super.getView(view);
 	}
-
 	
 	// When GUI elements are changed they each cause a {@link PropertyChangeEvent}.
 	@Override
@@ -196,9 +197,74 @@ public class Sixi3IK extends Sixi3FK {
 		Object src = evt.getSource();
 		
 		if(src == eeTarget && evt.getPropertyName().contentEquals("pose")) {
-			// gradient descent towards eeTarget
-			gradientDescent(eeTarget.getPose(),50, threshold.get(), stepSize.get(), learningRate.get());
-			updateSliders();
+			double [] v = new double[Sixi3FK.NUM_BONES];
+			double [] jBefore = new double[Sixi3FK.NUM_BONES];
+			double [] jAfter = new double[Sixi3FK.NUM_BONES];
+			getFKValues(v);
+			Matrix4d start = new Matrix4d();
+			getEndEffector(start);
+			Matrix4d end = eeTarget.getPose();
+			Matrix4d interpolated = new Matrix4d();
+			Matrix4d old = new Matrix4d(start);
+			double [][] jacobian = new double[6][6];
+			double [] cartesianDistance = new double[6];
+			double [] cartesianDistanceCompare = new double[6];
+			double [] jointDistance = new double[Sixi3FK.NUM_BONES];
+			final double STEPS=20;
+
+			//System.out.print("S"+start.toString()+"E"+end.toString());
+			
+			for(double alpha=1;alpha<=STEPS;++alpha) {
+				MatrixHelper.interpolate(start,end,alpha/STEPS,interpolated);
+
+				getFKValues(jBefore);
+				// move arm towards result to get future pose
+				gradientDescent(interpolated,30, threshold.get(), stepSize.get(), learningRate.get());
+				getFKValues(jAfter);
+
+				getApproximateJacobian(jacobian);
+				
+				getCartesianBetweenTwoMatrixes(old, interpolated, cartesianDistance);
+				old.set(interpolated);
+
+				boolean ok=getJointFromCartesian(jacobian, cartesianDistance, jointDistance);
+				getCartesianFromJoint(jacobian, jointDistance, cartesianDistanceCompare);
+				// cartesianDistance and cartesianDistanceCompare should always match
+				// jointDistance[n] should match jAfter[n]-jBefore[n]
+
+				System.out.print(alpha
+						+","
+						//+"R"+result.toString()
+						);
+				if(ok) {
+					/*
+					for(int i=0;i<6;++i) {
+						String add="";
+						for(int j=0;j<6;++j) {
+							System.out.print(add+jacobian[i][j]);
+							add="\t";
+						}
+						System.out.println();
+					}*/
+					System.out.println(
+							+jointDistance[0]+","
+							+jointDistance[1]+","
+							+jointDistance[2]+","
+							+jointDistance[3]+","
+							+jointDistance[4]+","
+							+jointDistance[5]+","
+							+(jAfter[0]-jBefore[0])+","
+							+(jAfter[1]-jBefore[1])+","
+							+(jAfter[2]-jBefore[2])+","
+							+(jAfter[3]-jBefore[3])+","
+							+(jAfter[4]-jBefore[4])+","
+							+(jAfter[5]-jBefore[5])+",");
+				} else System.out.println(" not ok");
+			}
+			System.out.println();
+			
+			setFKValues(v);
+			//updateSliders();
 		}
 	}
 }
