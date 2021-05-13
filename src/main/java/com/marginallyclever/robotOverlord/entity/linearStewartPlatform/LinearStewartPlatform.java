0a6a491ba3b977d9d3bf68ede5fb28d47b5a6b99
Dan Royer <dan@marginallyclever.com>
2021-05-13 15:59:00 -0700

    added models of arms

--- src/main/java/com/marginallyclever/robotOverlord/entity/linearStewartPlatform/LinearStewartPlatform.java
+++ src/main/java/com/marginallyclever/robotOverlord/entity/linearStewartPlatform/LinearStewartPlatform.java
@@ -33,6 +33,7 @@ public class LinearStewartPlatform  extends PoseEntity {
 
 	private ShapeEntity baseModel;
 	private ShapeEntity eeModel;
+	private ShapeEntity armModel;
 	
 	private double [] linearPosition = new double[6];
 	private PoseEntity ee = new PoseEntity("ee");
@@ -74,16 +75,32 @@ public class LinearStewartPlatform  extends PoseEntity {
 			new Point3d(),
 			new Point3d(),
 	};
+	private MaterialEntity me = new MaterialEntity();
 
 	public LinearStewartPlatform() {
 		super("Linear Stewart Platform");
 		addChild(ee);
-		
+		addChild(me);
+
+		// load models and fix scale/orientation.
 		baseModel = new ShapeEntity("/linearStewartPlatform/base.stl");
 		baseModel.setShapeScale(0.1);
 		eeModel = new ShapeEntity("/linearStewartPlatform/endEffector.stl");
 		eeModel.setShapeScale(0.1);
 		eeModel.setShapeRotation(new Vector3d(0,0,-30));
+		armModel = new ShapeEntity("/linearStewartPlatform/arm.stl");
+		armModel.setShapeScale(0.1);
+
+		// apply some default materials.
+		me.setAmbientColor(0, 0, 0, 1);
+		me.setDiffuseColor(0.1f,0.1f,0.1f,1);
+		me.setEmissionColor(0, 0, 0, 1);
+		me.setLit(true);
+		me.setShininess(0);
+		baseModel.setMaterial(me);
+		eeModel.setMaterial(me);
+		armModel.setMaterial(me);
+		
 		
 		for(int i=0;i<linearPosition.length;++i) {
 			linearPosition[i]=0;
@@ -96,8 +113,8 @@ public class LinearStewartPlatform  extends PoseEntity {
 		Vector3d tx = new Vector3d();
 		Vector3d ty = new Vector3d();
 
-		// calculate end effector points ONE TIME.
-		// end effector points are ordered counter clockwise
+		// calculate end effector points - the center of each magnetic ball at the end effector
+		// end effector points are ordered counter clockwise, looking down on the machine.
 		//       1
 		//  2       0 <-- first
 		//      x     <-- center
@@ -118,8 +135,8 @@ public class LinearStewartPlatform  extends PoseEntity {
 			pEE[i].z=EE_Z;
 		}
 
-		// calculate base of linear slides ONE TIME.
-		// linear slides are ordered counter clockwise
+		// calculate base of linear slides.
+		// linear slides are ordered counter clockwise, looking down on the machine.
 		//     1
 		//  2       0 <-- first
 		//      x     <-- center
@@ -136,107 +153,158 @@ public class LinearStewartPlatform  extends PoseEntity {
 			tx.scale( BASE_X,vx);
 			ty.scale(-BASE_Y,vy);
 			pBase[indexes[i]].add(tx,ty);
-		}		
-		
-	}
-	
-	@Override
-	public void render(GL2 gl2) {
-		super.render(gl2);
-		
-		MaterialEntity me = new MaterialEntity();
-		me.setAmbientColor(0, 0, 0, 1);
-		me.setDiffuseColor(0.1f,0.1f,0.1f,1);
-		me.setEmissionColor(0, 0, 0, 1);
-		me.setLit(true);
-		me.setShininess(0);
-		me.render(gl2);
-		
-		gl2.glPushMatrix();
-			MatrixHelper.applyMatrix(gl2, pose);
-			baseModel.setMaterial(me);
-			baseModel.render(gl2);
-			
-			gl2.glPushMatrix();
-			MatrixHelper.applyMatrix(gl2, ee.getPose());
-			eeModel.setMaterial(me);
-			eeModel.render(gl2);
-			gl2.glPopMatrix();
-
-			boolean wasLit = OpenGLHelper.disableLightingStart(gl2);
-
-			// use calculated end effector points to find same points after EE moves.
-			Matrix4d eeMatrix = ee.getPose();
-			for(int i=0;i<6;++i) {
-				eeMatrix.transform(pEE[i], pEE2[i]);
-			}
-
-			// We have pEE2 and pBase.  one end of the rod is at pEE2[n].  
-			// The sphere formed by pDD2[n] and ARM_LENGTH intersects the vertical line at bBase[n] twice.
-			// The first intersection traveling up is the one we want.
-			Ray ray = new Ray();
-			ray.direction.set(0,0,1);
-			for(int i=0;i<6;++i) {
-				ray.start.set(pBase[i]);
-				linearPosition[i] = IntersectionHelper.raySphere(ray, pEE2[i], ARM_LENGTH);
-				
-				pSlide[i].set(pBase[i].x,
-							  pBase[i].y,
-							  pBase[i].z+linearPosition[i]);
-			}
-
-			// draw updated ee points.
-			Vector3d eeCenter = ee.getPosition();
-			gl2.glColor3d(1, 0, 0);
-			gl2.glBegin(GL2.GL_LINES);
-			for(int i=0;i<6;++i) {
-				eeMatrix.transform(pEE[i], pEE2[i]);
-				gl2.glVertex3d(eeCenter.x,eeCenter.y,eeCenter.z);
-				gl2.glVertex3d(pEE2[i].x,
-							   pEE2[i].y,
-							   pEE2[i].z);
-				gl2.glColor3d(0, 0, 0);
-			}
-			gl2.glEnd();
-			
-			// draw linear slides
-			for(int i=0;i<6;++i) {
-				renderOneLinearSlide(gl2,
-						pSlide[i].x,
-						pSlide[i].y,
-						pSlide[i].z,
-						BASE_Z,
-						BASE_Z+SLIDE_TRAVEL);
-			}
-			
-			// draw arms
-			gl2.glColor3d(1, 0, 0);
-			gl2.glBegin(GL2.GL_LINES);
-			for(int i=0;i<6;++i) {
-				gl2.glVertex3d(pEE2[i].x,
-							   pEE2[i].y,
-							   pEE2[i].z);
-				gl2.glVertex3d(pSlide[i].x,
-								pSlide[i].y,
-								pSlide[i].z);
-				gl2.glColor3d(0, 0, 0);
-			}
-			gl2.glEnd();
-			
-		gl2.glPopMatrix();
-
-		OpenGLHelper.disableLightingEnd(gl2,wasLit);
-	}
-	
-	private void renderOneLinearSlide(GL2 gl2,double x,double y,double h,double min,double max) {
-		gl2.glBegin(GL2.GL_LINES);
-		gl2.glColor3d(1, 1, 1);		gl2.glVertex3d(x, y, min);		gl2.glVertex3d(x, y, h);
-		gl2.glColor3d(0, 0, 1);		gl2.glVertex3d(x, y, h);		gl2.glVertex3d(x, y, max);
-		gl2.glEnd();
+		}			
 	}
 	
 	@Override
 	public void update(double dt) {
 		//super.update(dt);
+
+		Matrix4d eeMatrix = ee.getPose();
+
+		// use calculated end effector points to find same points after EE moves.
+		for(int i=0;i<6;++i) {
+			eeMatrix.transform(pEE[i], pEE2[i]);
+		}
+
+		// We have pEE2 and pBase.  one end of the rod is at pEE2[n].  
+		// The sphere formed by pDD2[n] and ARM_LENGTH intersects the vertical line at bBase[n] twice.
+		// The first intersection traveling up is the one we want.
+		Ray ray = new Ray();
+		ray.direction.set(0,0,1);
+		for(int i=0;i<6;++i) {
+			ray.start.set(pBase[i]);
+			linearPosition[i] = IntersectionHelper.raySphere(ray, pEE2[i], ARM_LENGTH);
+			
+			pSlide[i].set(pBase[i].x,
+						  pBase[i].y,
+						  pBase[i].z+linearPosition[i]);
+		}
+	}
+	
+	@Override
+	public void render(GL2 gl2) {
+		super.render(gl2);
+
+		gl2.glPushMatrix();
+			// draw the base
+			MatrixHelper.applyMatrix(gl2, pose);
+			baseModel.render(gl2);
+			
+			// draw the end effector
+			gl2.glPushMatrix();
+			MatrixHelper.applyMatrix(gl2, ee.getPose());
+			eeModel.render(gl2);
+			gl2.glPopMatrix();
+
+			// draw the arms (some work to get each matrix...)
+			Matrix4d m = new Matrix4d();
+			for(int i=0;i<6;++i) {
+				// we need the pose of each bone to draw the mesh.
+				// a matrix is 3 orthogonal (right angle) vectors and a position. 
+				// z (up) is from one ball to the next
+				Vector3d z = new Vector3d(
+						pEE2[i].x-pSlide[i].x,
+						pEE2[i].y-pSlide[i].y,
+						pEE2[i].z-pSlide[i].z);
+				z.normalize();
+				// x is a vector that is guaranteed not parallel to z.
+				Vector3d x = new Vector3d(
+						pSlide[i].x,
+						pSlide[i].y,
+						pSlide[i].z);
+				x.normalize();
+				// y is orthogonal to x and z.  
+				Vector3d y = new Vector3d();
+				y.cross(z, x);
+				y.normalize();
+				// x was not orthogonal to z.
+				// y and z are orthogonal, so use them. 
+				x.cross(y, z);
+				x.normalize();
+				
+				// fill in the matrix
+				m.m00=x.x;
+				m.m10=x.y;
+				m.m20=x.z;
+                    
+				m.m01=y.x;
+				m.m11=y.y;
+				m.m21=y.z;
+				    
+				m.m02=z.x;
+				m.m12=z.y;
+				m.m22=z.z;
+				
+				m.m03=pSlide[i].x;
+				m.m13=pSlide[i].y;
+				m.m23=pSlide[i].z;
+				m.m33=1;
+						
+				gl2.glPushMatrix();
+				MatrixHelper.applyMatrix(gl2, m);
+				armModel.render(gl2);
+				gl2.glPopMatrix();
+			}
+			
+			// debug info
+			boolean wasLit = OpenGLHelper.disableLightingStart(gl2);
+			
+			// draw updated ee points.
+			boolean debugEEPoints=false;
+			if(debugEEPoints) {
+				Vector3d eeCenter = ee.getPosition();
+				gl2.glColor3d(1, 0, 0);
+				gl2.glBegin(GL2.GL_LINES);
+				for(int i=0;i<6;++i) {
+					gl2.glVertex3d(eeCenter.x,eeCenter.y,eeCenter.z);
+					gl2.glVertex3d(pEE2[i].x,
+								   pEE2[i].y,
+								   pEE2[i].z);
+					gl2.glColor3d(0, 0, 0);
+				}
+				gl2.glEnd();
+			}
+			
+			// draw linear slides
+			boolean debugSlides=true;
+			if(debugSlides) {
+				for(int i=0;i<6;++i) {
+					renderOneLinearSlide(gl2,
+							pSlide[i].x,
+							pSlide[i].y,
+							pSlide[i].z,
+							BASE_Z,
+							BASE_Z+SLIDE_TRAVEL);
+				}
+			}
+			
+			// draw arms
+			boolean debugArms=false;
+			if(debugArms) {
+				gl2.glColor3d(1, 0, 0);
+				gl2.glBegin(GL2.GL_LINES);
+				for(int i=0;i<6;++i) {
+					gl2.glVertex3d(pEE2[i].x,
+								   pEE2[i].y,
+								   pEE2[i].z);
+					gl2.glVertex3d(pSlide[i].x,
+									pSlide[i].y,
+									pSlide[i].z);
+					gl2.glColor3d(0, 0, 0);
+				}
+				gl2.glEnd();
+			}
+			OpenGLHelper.disableLightingEnd(gl2,wasLit);
+			
+		gl2.glPopMatrix();
+
+	}
+	
+	private void renderOneLinearSlide(GL2 gl2,double x,double y,double z,double min,double max) {
+		gl2.glBegin(GL2.GL_LINES);
+		gl2.glColor3d(1, 1, 1);		gl2.glVertex3d(x, y, min);		gl2.glVertex3d(x, y, z);
+		gl2.glColor3d(0, 0, 1);		gl2.glVertex3d(x, y, z);		gl2.glVertex3d(x, y, max);
+		gl2.glEnd();
 	}
 }
