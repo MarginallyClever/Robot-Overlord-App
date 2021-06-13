69fd1627d91b73c657bfc22d13aaf53a8b691809
Dan Royer <dan@marginallyclever.com>
2021-06-12 17:20:21 -0700

    fixed view cube

--- src/main/java/com/marginallyclever/robotOverlord/ViewCube.java
+++ src/main/java/com/marginallyclever/robotOverlord/ViewCube.java
@@ -9,6 +9,7 @@ import com.marginallyclever.convenience.MatrixHelper;
 import com.marginallyclever.robotOverlord.shape.Shape;
 import com.marginallyclever.robotOverlord.swingInterface.view.ViewPanel;
 import com.marginallyclever.robotOverlord.uiExposedTypes.DoubleEntity;
+import com.marginallyclever.robotOverlord.uiExposedTypes.MaterialEntity;
 
 public class ViewCube extends Entity {
 	/**
@@ -16,80 +17,92 @@ public class ViewCube extends Entity {
 	 */
 	private static final long serialVersionUID = 2625823417579183587L;
 	protected Shape model = new Shape();
-	protected DoubleEntity cubeSize = new DoubleEntity("size",6);
+	protected DoubleEntity cubeSize = new DoubleEntity("size",32);
 	
     public ViewCube() {
     	super();
     	setName("ViewCube");
     	addChild(cubeSize);
     	model.setShapeFilename("/viewCube.obj");
-    	model.getMaterial().setTextureFilename("/images/viewCube.png");
-    	model.getMaterial().setDiffuseColor(1, 1, 1, 1);
-    	model.getMaterial().setAmbientColor(1, 1, 1, 1);
-    	model.getMaterial().setLit(false);
+    	MaterialEntity mat = model.getMaterial(); 
+    	mat.setTextureFilename("/images/viewCube.png");
+    	mat.setDiffuseColor(1, 1, 1, 1);
+    	mat.setAmbientColor(1, 1, 1, 1);
+    	mat.setLit(false);
     }
 		
-	public void render(GL2 gl2) {
-		RobotOverlord ro = (RobotOverlord)getRoot();
-		Viewport cameraView = ro.viewport;
-		
-		gl2.glClear(GL2.GL_DEPTH_BUFFER_BIT);
-
-		gl2.glEnable(GL2.GL_DEPTH_TEST);
-		gl2.glEnable(GL2.GL_CULL_FACE);
-		
-		gl2.glBlendFunc(GL2.GL_SRC_ALPHA,GL2.GL_ONE_MINUS_SRC_ALPHA);
-
+    public Matrix4d getInverseCameraMatrix(PoseEntity camera) {
+		Matrix4d m = new Matrix4d();
+		camera.getPoseWorld(m);
+		m.invert();
+		m.setTranslation(new Vector3d(0,0,0));
+		return m;
+    }
+    
+    private void startProjection(GL2 gl2,Viewport viewport) {
     	gl2.glMatrixMode(GL2.GL_PROJECTION);
 		gl2.glPushMatrix();
-		cameraView.renderOrtho(gl2,1);
+		MatrixHelper.setMatrix(gl2, MatrixHelper.createIdentityMatrix4());
+		viewport.renderOrtho(gl2,0.2);
 		gl2.glMatrixMode(GL2.GL_MODELVIEW);
-		
-		gl2.glPushMatrix();			
-			double c = cubeSize.get();			
-			PoseEntity camera = cameraView.getAttachedTo();
-			Matrix4d m = new Matrix4d();
-			camera.getPoseWorld(m);
-			Vector3d p = camera.getPosition();
-			Vector3d vx = MatrixHelper.getXAxis(m);
-			Vector3d vy = MatrixHelper.getYAxis(m);
-			Vector3d vz = MatrixHelper.getZAxis(m);
-		
-			vz.scale(-100);
-			vx.scale(cameraView.getCanvasWidth() /10 -c*2);
-			vy.scale(cameraView.getCanvasHeight()/10 -c*2);
-			p.add(vx);
-			p.add(vy);
-			p.add(vz);
-			
-			gl2.glTranslated(p.x, p.y, p.z);
-			gl2.glScaled(c,c,c);
-
-			model.render(gl2);
-
-			gl2.glDisable(GL2.GL_LIGHTING);
-			gl2.glDisable(GL2.GL_COLOR_MATERIAL);
-			gl2.glDisable(GL2.GL_TEXTURE_2D);
-			
-			// the big lines
-			gl2.glLineWidth(4);
-			gl2.glPushMatrix();
-				gl2.glTranslated(-1.05,-1.05,-0.95);
-				gl2.glBegin(GL2.GL_LINES);
-				gl2.glColor3d(1, 0, 0);		gl2.glVertex3d(0, 0, 0);		gl2.glVertex3d(2.5, 0, 0);
-				gl2.glColor3d(0, 1, 0);		gl2.glVertex3d(0, 0, 0);		gl2.glVertex3d(0, 2.5, 0);
-				gl2.glColor3d(0, 0, 1);		gl2.glVertex3d(0, 0, 0);		gl2.glVertex3d(0, 0, 2.5);
-				gl2.glEnd();
-			gl2.glPopMatrix();
-			gl2.glLineWidth(1);
-						
-		gl2.glPopMatrix();
-
+    }
+    
+    private void endProjection(GL2 gl2) {
     	gl2.glMatrixMode(GL2.GL_PROJECTION);
 		gl2.glPopMatrix();
 		gl2.glMatrixMode(GL2.GL_MODELVIEW);
+    }
+    
+	public void render(GL2 gl2) {
+		Viewport viewport = ((RobotOverlord)getRoot()).viewport;
+		
+		startProjection(gl2,viewport);
+		
+		gl2.glPushMatrix();
+			positionCubeModel(gl2,viewport);
+			renderCubeModel(gl2);
+			renderMajorAxies(gl2);
+		gl2.glPopMatrix();
+
+		endProjection(gl2);
 	}
 	
+	private void positionCubeModel(GL2 gl2, Viewport viewport) {
+		double c = cubeSize.get();
+        double w2 = viewport.getCanvasWidth()/2;
+        double h2 = viewport.getCanvasHeight()/2;
+		MatrixHelper.setMatrix(gl2, MatrixHelper.createIdentityMatrix4());
+		gl2.glTranslated(w2-c*2,h2-c*2,-c*2);
+		gl2.glScaled(c,c,c);
+		MatrixHelper.applyMatrix(gl2, getInverseCameraMatrix(viewport.getAttachedTo()));
+	}
+
+	private void renderCubeModel(GL2 gl2) {
+		gl2.glClear(GL2.GL_DEPTH_BUFFER_BIT);
+		gl2.glEnable(GL2.GL_DEPTH_TEST);
+		gl2.glEnable(GL2.GL_CULL_FACE);
+		gl2.glBlendFunc(GL2.GL_SRC_ALPHA,GL2.GL_ONE_MINUS_SRC_ALPHA);
+		model.render(gl2);
+	}
+
+	private void renderMajorAxies(GL2 gl2) {
+		gl2.glDisable(GL2.GL_LIGHTING);
+		gl2.glDisable(GL2.GL_COLOR_MATERIAL);
+		gl2.glDisable(GL2.GL_TEXTURE_2D);
+		gl2.glLineWidth(4);
+		
+		gl2.glPushMatrix();
+			gl2.glTranslated(-1.05,-1.05,-0.95);
+			gl2.glBegin(GL2.GL_LINES);
+			gl2.glColor3d(1, 0, 0);		gl2.glVertex3d(0, 0, 0);		gl2.glVertex3d(2.5, 0, 0);
+			gl2.glColor3d(0, 1, 0);		gl2.glVertex3d(0, 0, 0);		gl2.glVertex3d(0, 2.5, 0);
+			gl2.glColor3d(0, 0, 1);		gl2.glVertex3d(0, 0, 0);		gl2.glVertex3d(0, 0, 2.5);
+			gl2.glEnd();
+		gl2.glPopMatrix();
+		
+		gl2.glLineWidth(1);
+	}
+
 	@Override
 	public void getView(ViewPanel view) {
 		view.pushStack("VC", "View Cube");
