03aeab8d1d7528bf813fff147949825856e7a52d
Dan Royer <dan@marginallyclever.com>
2021-06-12 17:21:51 -0700

    Create SelectedEntityPanel.java
    wraps behavior for the panel

---
+++ src/main/java/com/marginallyclever/robotOverlord/swingInterface/SelectedEntityPanel.java
@@ -0,0 +1,62 @@
+package com.marginallyclever.robotOverlord.swingInterface;
+
+import java.awt.BorderLayout;
+import java.util.ArrayList;
+
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+
+import com.marginallyclever.robotOverlord.Entity;
+import com.marginallyclever.robotOverlord.RobotOverlord;
+import com.marginallyclever.robotOverlord.swingInterface.view.ViewPanel;
+
+
+public class SelectedEntityPanel extends JPanel {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public SelectedEntityPanel() {
+		super();
+		setLayout(new BorderLayout());
+	}
+
+	/**
+	 * Collate all the {@link EntityPanel}s for a {@link Entity}.  
+	 * @param 
+	 */
+	public void update(ArrayList<Entity> entityList,RobotOverlord ro) {
+		removeAll();
+		
+		if(entityList != null ) {
+			if(entityList.size()==1) {
+				int size = entityList.size();
+				ViewPanel [] panels = new ViewPanel[size];
+				for( int i=0;i<size;++i) {
+					panels[i] = new ViewPanel(ro);
+					entityList.get(i).getView(panels[i]);
+				}
+				
+				// keep the first panel.
+				// TODO compare panels and keep only the matching elements and - if possible - the data in those elements.
+				ViewPanel combined = panels[0];
+				
+				// TODO throw away panels that have no elements left.
+	
+				add(combined.getFinalView(),BorderLayout.PAGE_START);
+			} else {
+				// TODO display values shared across all selected entities
+			}
+		}
+		
+		repaint();
+		revalidate();
+
+		if( this.getParent() instanceof JScrollPane ) {
+			JScrollPane scroller = (JScrollPane)getParent();
+			scroller.getVerticalScrollBar().setValue(0);
+		}
+	}
+
+}
