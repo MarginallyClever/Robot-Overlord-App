164ec423dc66930a7d8c4eac9a928a56fa0b29d3
Dan Royer <dan@marginallyclever.com>
2021-07-28 09:14:45 -0700

    cleaning up names

--- src/main/com/marginallyclever/convenience/Units.java
+++ src/main/com/marginallyclever/convenience/Units.java
@@ -1,9 +1,11 @@
 package com.marginallyclever.convenience;
 
 public class Units {
-	public double to_inch(double x) {	return x/25.4;	}
-	public double to_mm  (double x) {	return x*25.4;	}
+	public double mm2inch(double x) {	return x/25.4;	}
+	public double inch2mm(double x) {	return x*25.4;	}
 	
-	public double to_color  (double x) {	return x*255;	}
-	public double from_color(double x) {	return x/255;	}
+	// from 0..1 to 0...255
+	public double unit2oneByte(double x) {	return x*255;	}
+	// from 0...255 to 0..1
+	public double oneByte2Unit(double x) {	return x/255;	}
 }
