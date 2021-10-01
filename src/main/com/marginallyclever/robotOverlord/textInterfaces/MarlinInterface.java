a62a2cf5c730c37a3273ded42b8cbf2a437755ac
Dan Royer <dan@marginallyclever.com>
2021-09-30 17:28:25 -0700

    Replaced RobotUI with MarlinInterface

--- src/main/com/marginallyclever/robotOverlord/textInterfaces/MarlinInterface.java
+++ src/main/com/marginallyclever/robotOverlord/textInterfaces/MarlinInterface.java
@@ -1,48 +1,34 @@
 package com.marginallyclever.robotOverlord.textInterfaces;
 
 import java.awt.Font;
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
 import java.awt.BorderLayout;
 
-import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JToolBar;
 import javax.swing.UIManager;
-import javax.vecmath.Matrix4d;
 
 import com.marginallyclever.communications.NetworkSessionEvent;
-import com.marginallyclever.convenience.MatrixHelper;
 import com.marginallyclever.convenience.StringHelper;
 import com.marginallyclever.convenience.log.Log;
-import com.marginallyclever.robotOverlord.robots.sixi3.ApproximateJacobian;
 import com.marginallyclever.robotOverlord.robots.sixi3.Sixi3Bone;
 import com.marginallyclever.robotOverlord.robots.sixi3.Sixi3IK;
 
-public class RobotUI extends JPanel {
+public class MarlinInterface extends JPanel {
 	private static final long serialVersionUID = -6388563393882327725L;
 
-	private Sixi3IK mySixi3 = new Sixi3IK();
+	private Sixi3IK mySixi3;
 	private TextInterfaceToNetworkSession chatInterface = new TextInterfaceToNetworkSession();
-	private JToolBar toolBar = getToolBar();
-	private CartesianReportPanel eeReport, eeTargetReport;
 
-	public RobotUI() {
-		this(new Sixi3IK());
-	}
-
-	public RobotUI(Sixi3IK sixi3) {
+	public MarlinInterface(Sixi3IK sixi3) {
 		super();
 
 		mySixi3 = sixi3;
 
-		JPanel interior = getInteriorPanel();
-
 		this.setLayout(new BorderLayout());
-		this.add(toolBar, BorderLayout.PAGE_START);
-		this.add(interior, BorderLayout.CENTER);
+		this.add(getToolBar(), BorderLayout.PAGE_START);
+		this.add(chatInterface, BorderLayout.CENTER);
 
 		sixi3.addPropertyChangeListener((e) -> {
 			if(e.getPropertyName().contentEquals("ee")) sendGoto();	
@@ -55,73 +41,6 @@ public class RobotUI extends JPanel {
 				break;
 			}
 		});
-	}
-
-	private JPanel getInteriorPanel() {
-		JPanel panel = new JPanel();
-		panel.setLayout(new GridBagLayout());
-
-		GridBagConstraints c = new GridBagConstraints();
-		c.gridx = 0;
-		c.gridy = 0;
-		c.weightx = 1;
-		c.weighty = 0.75;
-		c.gridheight = 6;
-		c.gridwidth = 1;
-		c.fill = GridBagConstraints.BOTH;
-		c.anchor = GridBagConstraints.NORTHWEST;
-
-		panel.add(chatInterface, c);
-
-		chatInterface.setBorder(BorderFactory.createTitledBorder("RobotUI.chatInterface"));
-
-		c.gridheight = 1;
-		c.gridwidth = 1;
-		c.gridx = 1;
-		c.weightx = 0;
-		c.weighty = 0;
-		c.gridy = 0;
-		panel.add(new AngleReportPanel(mySixi3), c);
-		c.gridx++;
-		panel.add(new AngleDrivePanel(mySixi3), c);
-		c.gridx--;
-		c.gridy++;
-		panel.add(eeReport=new CartesianReportPanel("RobotUI.EndEffector"), c);
-		c.gridy++;
-		panel.add(eeTargetReport=new CartesianReportPanel("RobotUI.EndEffectorTarget"), c);
-		c.gridy--;
-		c.gridx++;
-		c.gridheight=2;
-		panel.add(new CartesianDrivePanel(mySixi3), c);
-		c.gridheight=1;
-		c.gridx--;
-		c.gridy+=2;
-		c.gridwidth = 2;
-		panel.add(new JacobianReportPanel(mySixi3), c);
-		c.gridy++;
-		c.weighty = 1;
-		panel.add(new JPanel(), c);
-
-		mySixi3.addPropertyChangeListener( (e)-> updateReports() );
-		
-		updateReports();
-		
-		return panel;
-	}
-	
-	private void updateReports() {
-		Matrix4d m0=mySixi3.getEndEffector();
-		eeReport.updateReport(m0);
-		Matrix4d m1=mySixi3.getEndEffectorTarget();
-		eeTargetReport.updateReport(m1);
-		double [] cartesianDistance = MatrixHelper.getCartesianBetweenTwoMatrixes(m0, m1);
-		ApproximateJacobian aj = mySixi3.getApproximateJacobian();
-		try {
-			double [] jointDistance = aj.getJointFromCartesian(cartesianDistance);
-			System.out.println(jointDistance.toString());
-		} catch(Exception e) {
-			System.out.println("Failed to calculate jointDistance.");
-		}
 	}
 
 	private void onConnect() {
@@ -232,9 +151,9 @@ public class RobotUI extends JPanel {
 		} catch (Exception e) {
 		}
 
-		JFrame frame = new JFrame("RobotUI");
+		JFrame frame = new JFrame("MarlinInterface");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.add(new RobotUI());
+		frame.add(new MarlinInterface(new Sixi3IK()));
 		frame.pack();
 		frame.setVisible(true);
 	}
