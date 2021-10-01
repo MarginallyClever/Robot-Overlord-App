9e8dbe16b7cbeb175b5253eb93ddd80fdc6ff054
Dan Royer <dan@marginallyclever.com>
2021-09-30 17:27:33 -0700

    added main() and improved look

--- src/main/com/marginallyclever/robotOverlord/textInterfaces/JacobianReportPanel.java
+++ src/main/com/marginallyclever/robotOverlord/textInterfaces/JacobianReportPanel.java
@@ -1,12 +1,19 @@
 package com.marginallyclever.robotOverlord.textInterfaces;
 
+import java.awt.BorderLayout;
+
 import javax.swing.BorderFactory;
+import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JTable;
 import javax.swing.SwingConstants;
+import javax.swing.UIManager;
 import javax.swing.table.DefaultTableCellRenderer;
 import javax.swing.table.TableCellRenderer;
+import javax.swing.table.TableColumn;
+import javax.swing.table.TableColumnModel;
 
+import com.marginallyclever.convenience.log.Log;
 import com.marginallyclever.robotOverlord.robots.sixi3.ApproximateJacobian;
 import com.marginallyclever.robotOverlord.robots.sixi3.Sixi3IK;
 
@@ -18,30 +25,46 @@ public class JacobianReportPanel extends JPanel {
 		super();
 
 		ApproximateJacobian aj = sixi3.getApproximateJacobian();
-		
+
+		DefaultTableCellRenderer renderRight = new DefaultTableCellRenderer();
+        renderRight.setHorizontalAlignment(SwingConstants.RIGHT);
+        
 		table = new JTable(aj.jacobian.length,aj.jacobian[0].length) {
 			private static final long serialVersionUID = 1L;
-			DefaultTableCellRenderer renderRight = new DefaultTableCellRenderer();
-
-		    { // initializer block
-		        renderRight.setHorizontalAlignment(SwingConstants.RIGHT);
-		    }
 
 		    @Override
 		    public TableCellRenderer getCellRenderer (int arg0, int arg1) {
 		        return renderRight;
 		    }
 		};
+
+		//setColumnNames(sixi3);
 		
-		this.setBorder(BorderFactory.createTitledBorder("Jacobian"));
-		this.add(
-				(table));
+		this.setBorder(BorderFactory.createTitledBorder(/*BorderFactory.createEmptyBorder(),*/"Jacobian"));
+		this.setLayout(new BorderLayout());
+		this.add(/*new JScrollPane*/(table),BorderLayout.CENTER);
+		
+		table.setPreferredScrollableViewportSize(table.getPreferredSize());
+		table.setFillsViewportHeight(false);
 		
 		sixi3.addPropertyChangeListener( (e)-> updateReport(sixi3) );
 		
 		updateReport(sixi3);
 	}
 	
+	@SuppressWarnings("unused")
+	private void setColumnNames(Sixi3IK sixi3) {
+		DefaultTableCellRenderer renderHeaderRight = (DefaultTableCellRenderer)table.getTableHeader().getDefaultRenderer();
+		renderHeaderRight.setHorizontalAlignment(SwingConstants.RIGHT);
+		table.getTableHeader().setDefaultRenderer(renderHeaderRight);
+		
+		TableColumnModel tcm = table.getColumnModel();
+		for(int i=0;i<sixi3.getNumBones();++i) {
+			TableColumn tc = tcm.getColumn(i);
+			tc.setHeaderValue( sixi3.getBone(i).getName() );
+		}
+	}
+
 	private void updateReport(Sixi3IK sixi3) {
 		ApproximateJacobian aj = sixi3.getApproximateJacobian();
 		for(int y=0;y<aj.jacobian.length;++y) {
@@ -49,5 +72,22 @@ public class JacobianReportPanel extends JPanel {
 				table.setValueAt(String.format("%.5f", aj.jacobian[y][x]), y, x);
 			}
 		}
+	}
+
+	// TEST
+
+	public static void main(String[] args) {
+		Log.start();
+
+		try {
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		} catch (Exception e) {
+		}
+
+		JFrame frame = new JFrame("JacobianReportPanel");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.add(new JacobianReportPanel(new Sixi3IK()));
+		frame.pack();
+		frame.setVisible(true);
 	}
 }
