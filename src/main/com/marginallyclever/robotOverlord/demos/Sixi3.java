2e6a5456605cc27785676a5266e21e36e77a23c1
Dan Royer <dan@marginallyclever.com>
2021-10-02 16:40:12 -0700

    Create Sixi3.java

---
+++ src/main/com/marginallyclever/robotOverlord/demos/Sixi3.java
@@ -0,0 +1,29 @@
+package com.marginallyclever.robotOverlord.demos;
+
+import com.marginallyclever.robotOverlord.robots.sixi3.RobotArmBone;
+import com.marginallyclever.robotOverlord.robots.sixi3.RobotArmFK;
+import com.marginallyclever.robotOverlord.shape.Shape;
+
+public class Sixi3 extends RobotArmFK {
+	private static final long serialVersionUID = 1L;
+
+	public Sixi3() {
+		super();
+		setName("Sixi3");
+	}
+	
+	@Override
+	protected void loadModel() {
+		setBaseShape(new Shape("Base","/Sixi3b/base.3mf"));
+		
+		// name d r a t max min file
+		addBone(new RobotArmBone("X", 8.01,     0,270,  0,170    ,-170   ,"/Sixi3b/j0.3mf"));
+		addBone(new RobotArmBone("Y",9.131,17.889,  0,270,270+100,270-100,"/Sixi3b/j1.3mf"));
+		addBone(new RobotArmBone("Z",    0,12.435,  0,  0,0+150  ,0-150  ,"/Sixi3b/j2.3mf"));
+		addBone(new RobotArmBone("U",    0,     0,270,270,270+170,270-170,"/Sixi3b/j3.3mf"));
+		addBone(new RobotArmBone("V", 5.12,     0,  0,180,360    ,0      ,"/Sixi3b/j4.3mf"));
+		//addBone(new RobotArmBone("W",    0,  5.12,  0,  0,350    ,10     ,"/Sixi3b/j5.3mf"));
+		
+		adjustModelOriginsToDHLinks();		
+	}
+}
