a8b049698ec6907db38ee081b8c2203fb69e04d1
Dan Royer <dan@marginallyclever.com>
2021-10-03 09:17:21 -0700

    fixed offsets

--- src/main/com/marginallyclever/robotOverlord/demos/Sixi2.java
+++ src/main/com/marginallyclever/robotOverlord/demos/Sixi2.java
@@ -17,12 +17,12 @@ public class Sixi2 extends RobotArmFK {
 		setBaseShape(new Shape("Base","/Sixi2/anchor.obj"));
 		
 		// name d r a t max min file
-		addBone(new RobotArmBone("X",18.8452+0.9,0     ,-90,  0,120,-120,   "/Sixi2/shoulder.obj"));
+		addBone(new RobotArmBone("X",18.8452    ,0     ,-90,  0,120,-120,   "/Sixi2/shoulder.obj"));
 		addBone(new RobotArmBone("Y",0          ,35.796,  0,-90,  0,-170,   "/Sixi2/bicep.obj"));
 		addBone(new RobotArmBone("Z",0          ,6.4259,-90,  0, 86,-83.369,"/Sixi2/forearm.obj"));
 		addBone(new RobotArmBone("U",29.355+9.35,0     , 90,  0,175,-175,   "/Sixi2/tuningFork.obj"));
 		addBone(new RobotArmBone("V",0          ,0     ,-90,  0,120,-120,   "/Sixi2/picassoBox.obj"));
-		addBone(new RobotArmBone("W",3.795      ,0     ,  0,  0,170,-170,   "/Sixi2/hand.obj"));
+		addBone(new RobotArmBone("W",6.795      ,0     ,  0,  0,170,-170,   "/Sixi2/hand.obj"));
 		
 		adjustModelOriginsToDHLinks();		
 		
