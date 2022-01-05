d5247690a4b284b1053081d2c944ee68127ca100
Dan Royer <dan@marginallyclever.com>
2022-01-05 14:24:47 -0800

    load basic demo at program start

--- src/main/java/com/marginallyclever/robotOverlord/RobotOverlord.java
+++ src/main/java/com/marginallyclever/robotOverlord/RobotOverlord.java
@@ -159,7 +159,8 @@ public class RobotOverlord extends Entity {
         layoutComponents();
         startAnimationSystem();
         
-        newScene();
+        BasicDemo bd = new BasicDemo();
+        bd.execute(this);
         
 		Log.message("** READY **");
     }
