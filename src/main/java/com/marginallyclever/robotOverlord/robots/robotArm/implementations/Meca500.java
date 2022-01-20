ff16c8d3b28befcf5b997a3c4b587b0b4e6caa29
Dan Royer <dan@marginallyclever.com>
2022-01-20 13:26:18 -0800

    improve range limit

--- src/main/java/com/marginallyclever/robotOverlord/robots/robotArm/implementations/Meca500.java
+++ src/main/java/com/marginallyclever/robotOverlord/robots/robotArm/implementations/Meca500.java
@@ -21,11 +21,11 @@ public class Meca500 extends RobotArmIK {
 
 		// name d r alpha theta thetaMax thetaMin modelFile
 		addBone(new RobotArmBone("X",  13.5,     0,-90,  0,175,-175,"/meca500/j1.obj"));
-		addBone(new RobotArmBone("Y",     0,  13.5,  0,-90,290, -270,"/meca500/j2.obj"));
-		addBone(new RobotArmBone("Z",     0,   3.8, 90,  0,150,-150,"/meca500/j3.obj"));
-		addBone(new RobotArmBone("U",  12.0,     0, 90,  0,440, -100,"/meca500/j4.obj"));
-		addBone(new RobotArmBone("V",     0,     0,-90,  0,90+180,90-180,"/meca500/j5.obj"));
-		addBone(new RobotArmBone("W",   7.0,     0,  0,180,360,   0,"/meca500/j6.obj"));
+		addBone(new RobotArmBone("Y",     0,  13.5,  0,-90,90-90,-90-70,"/meca500/j2.obj"));
+		addBone(new RobotArmBone("Z",     0,   3.8, 90,  0,55+70,55-135,"/meca500/j3.obj"));
+		addBone(new RobotArmBone("U",  12.0,     0, 90,  0,170, -170,"/meca500/j4.obj"));
+		addBone(new RobotArmBone("V",     0,     0,-90,  0,115,-115,"/meca500/j5.obj"));
+		addBone(new RobotArmBone("W",   7.0,     0,  0,180,720,-720,"/meca500/j6.obj"));
 		
 		adjustModelOriginsToDHLinks();
 		adjustCenterOfMassToDHLinks();
