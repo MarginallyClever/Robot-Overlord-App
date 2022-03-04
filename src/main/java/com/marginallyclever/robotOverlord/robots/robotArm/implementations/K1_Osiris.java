a18147d758ef316ba94320f71500ab16fab39b6c
Dan Royer <dan@marginallyclever.com>
2022-03-04 13:15:40 -0800

    added k1 osiris robot

---
+++ src/main/java/com/marginallyclever/robotOverlord/robots/robotArm/implementations/K1_Osiris.java
@@ -0,0 +1,40 @@
+package com.marginallyclever.robotOverlord.robots.robotArm.implementations;
+
+import com.marginallyclever.robotOverlord.robots.robotArm.RobotArmBone;
+import com.marginallyclever.robotOverlord.robots.robotArm.RobotArmIK;
+import com.marginallyclever.robotOverlord.shape.Shape;
+
+public class K1_Osiris extends RobotArmIK {
+	private static final long serialVersionUID = 1L;
+
+	public K1_Osiris() {
+		super();
+		setName(K1_Osiris.class.getSimpleName());
+	}
+	
+	@Override
+	protected void loadModel() {
+		setBaseShape(new Shape("Base","/K1/Base v1.obj"));
+		// base 
+		// Mass	201.686 g
+		// Center of Mass	-1.113, -0.011, 14.664
+
+		// name d r alpha theta thetaMax thetaMin modelFile
+		addBone(new RobotArmBone("X",25.625,     0,-90,180,270.0,  10.0,"/K1/A1 v1.obj"));
+		addBone(new RobotArmBone("Y",   0.0,  17.5,  0,-90,180.0,-180.0,"/K1/A2 v1.obj"));
+		addBone(new RobotArmBone("Z",   0.0,   0.0, 90,  0,180.0,-180.0,"/K1/A3 v1.obj"));
+		addBone(new RobotArmBone("U", 21.60,   0.0, 90,  0,180.0,-180.0,"/K1/A4 v1.obj"));
+		addBone(new RobotArmBone("V",     0,   6.3-1.1411,-90,  90,180.0,-180.0,"/K1/A5 v1.obj"));
+		//addBone(new RobotArmBone("W",   6.3-1.1411,   0.0,  0,  180,  180.0,   180.0,null));
+
+		setTextureFilename("/K1/K1.png");
+
+		adjustModelOriginsToDHLinks();
+		adjustCenterOfMassToDHLinks();
+	}
+
+	@Override
+	public void update(double dt) {
+		super.update(dt);
+	}
+}
