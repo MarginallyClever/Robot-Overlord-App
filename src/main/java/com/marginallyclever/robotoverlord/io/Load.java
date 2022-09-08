d0a051607d5f9c41660487e49b68c306f1b47113
Dan Royer <dan@marginallyclever.com>
2022-09-08 10:52:06 -0700

    removed save/load interface

--- src/main/java/com/marginallyclever/robotoverlord/io/Load.java
+++
@@ -1,7 +0,0 @@
-package com.marginallyclever.robotoverlord.io;
-
-import com.marginallyclever.robotoverlord.Entity;
-
-public interface Load {
-	public Entity load(String filename);
-}
--- src/main/java/com/marginallyclever/robotoverlord/io/Save.java
+++
@@ -1,7 +0,0 @@
-package com.marginallyclever.robotoverlord.io;
-
-import com.marginallyclever.robotoverlord.Entity;
-
-public interface Save {
-	public void save(String filename,Entity ent);
-}
