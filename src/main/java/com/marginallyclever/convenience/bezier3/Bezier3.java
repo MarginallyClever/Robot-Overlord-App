ba68f1d96991ecc49aeaafcc72e867862a650b26
Dan Royer <dan@marginallyclever.com>
2022-11-09 11:50:40 -0800

    added comments

--- src/main/java/com/marginallyclever/convenience/bezier3/Bezier3.java
+++ src/main/java/com/marginallyclever/convenience/bezier3/Bezier3.java
@@ -4,7 +4,7 @@ import javax.vecmath.Vector3d;
 
 /**
  * 3D Bezier curve implementation.  All points are in the same space - p1 and p2 are not relative to p0 and p3, respectively.
- * @See <a href='https://en.wikipedia.org/wiki/B%C3%A9zier_curve'>Wikipedia</a>
+ * See <a href="https://en.wikipedia.org/wiki/B%C3%A9zier_curve">Wikipedia</a>
  * @author Dan Royer
  */
 public class Bezier3 {
@@ -35,11 +35,11 @@ public class Bezier3 {
 	}
 	
 	/**
-	 * Interpolate between two {@link Vector3d}
+	 * Interpolate between two {@link Vector3d} points.
 	 * @param a from here
 	 * @param b to here
 	 * @param i a value from 0...1, inclusive.
-	 * @return
+	 * @return the interpolated {@link Vector3d} between a and b.
 	 */
 	private Vector3d interpolate(Vector3d a,Vector3d b,double i) {
 		Vector3d c = new Vector3d(b);
