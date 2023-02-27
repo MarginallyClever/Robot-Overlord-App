383a5a06f20fe838a6e38e4b9e3669efe15454d6
Dan Royer <dan@marginallyclever.com>
2023-02-27 14:11:04 -0800

    breakout adjust method

--- src/main/java/com/marginallyclever/robotoverlord/components/OriginAdjustComponent.java
+++ src/main/java/com/marginallyclever/robotoverlord/components/OriginAdjustComponent.java
@@ -13,14 +13,16 @@ public class OriginAdjustComponent extends Component {
     @Override
     public void getView(ViewPanel view) {
         super.getView(view);
-        view.addButton("Adjust").addActionEventListener((e)->{
-            PoseComponent myPose = getEntity().findFirstComponent(PoseComponent.class);
-            PoseComponent parentPose = getEntity().getParent().findFirstComponent(PoseComponent.class);
-            if(myPose==null || parentPose==null) return;
-            Matrix4d parentMat = parentPose.getWorld();
-            Matrix4d mat = myPose.getWorld();
-            parentMat.invert();
-            myPose.setLocalMatrix4(parentMat);
-        });
+        view.addButton("Adjust").addActionEventListener( e -> adjust() );
+    }
+
+    public void adjust() {
+        PoseComponent myPose = getEntity().findFirstComponent(PoseComponent.class);
+        PoseComponent parentPose = getEntity().getParent().findFirstComponent(PoseComponent.class);
+        if(myPose==null || parentPose==null) return;
+        Matrix4d parentMat = parentPose.getWorld();
+        Matrix4d mat = myPose.getWorld();
+        parentMat.invert();
+        myPose.setLocalMatrix4(parentMat);
     }
 }
