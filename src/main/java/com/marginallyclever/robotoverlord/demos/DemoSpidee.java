511820bf5d33b47b461b0b1a591ce35dff762b2e
Dan Royer <dan@marginallyclever.com>
2023-02-28 15:31:38 -0800

    add demo files

--- src/main/java/com/marginallyclever/robotoverlord/demos/DemoSpidee.java
+++ src/main/java/com/marginallyclever/robotoverlord/demos/DemoSpidee.java
@@ -30,79 +30,83 @@ public class DemoSpidee implements Demo {
 	@Override
 	public void execute(RobotOverlord ro) {
 		Entity sc = ro.getScene();
-		
+
 		// adjust default camera
 		CameraComponent camera = ro.getCamera();
 		PoseComponent pose = camera.getEntity().findFirstComponent(PoseComponent.class);
 		pose.setPosition(new Vector3d(40/4f,-91/4f,106/4f));
 		camera.lookAt(new Vector3d(0,0,0));
 		camera.setOrbitDistance(20);
-		
-		// add some lights
-		LightComponent light;
-		Entity light0 = new Entity("Light2");
-		sc.addEntity(light0);
-		light0.addComponent(pose = new PoseComponent());
-		light0.addComponent(light = new LightComponent());
-    	pose.setPosition(new Vector3d(60,-60,160));
-    	light.setDiffuse(1,1,1,1);
-    	light.setSpecular(0.5f, 0.5f, 0.5f, 1.0f);
-    	light.setAttenuationLinear(0.0014);
-    	light.setAttenuationQuadratic(7*1e-6);
-    	light.setDirectional(true);
 
 		Entity spidee = new Entity("Spidee-1");
 		sc.addEntity(spidee);
 
 		spidee.addEntity(createMesh("/Spidee/body.stl"));
-
-		spidee.addEntity(createLimb("RF",   45,true));
-		spidee.addEntity(createLimb("RM",   90,true));
-		spidee.addEntity(createLimb("RB",  135,true));
-		spidee.addEntity(createLimb("LF",  -45,false));
-		spidee.addEntity(createLimb("LM",  -90,false));
-		spidee.addEntity(createLimb("LB", -135,false));
+/*
+		spidee.addEntity(positionLimb(createLimb("RF",true ),   45));
+		spidee.addEntity(positionLimb(createLimb("RM",true ),   0));
+		spidee.addEntity(positionLimb(createLimb("RB",true ),  -45));
+		spidee.addEntity(positionLimb(createLimb("LF",false),  135));
+		spidee.addEntity(positionLimb(createLimb("LM",false),  180));
+		spidee.addEntity(positionLimb(createLimb("LB",false), -135));
+ */
+		spidee.addEntity(createLimb("a",true));
 	}
 
-	private Entity createLimb(String name,double degrees,boolean isRight) {
-		DHComponent [] dh = new DHComponent[4];
+	private Entity positionLimb(Entity limb,float degrees) {
+		PoseComponent pose = limb.findFirstComponent(PoseComponent.class);
+		double r = Math.toRadians(degrees);
+		pose.setPosition(new Vector3d(Math.cos(r)*10,Math.sin(r)*10,2.6));
+		pose.setRotation(new Vector3d(0,0,degrees));
+		return limb;
+	}
+
+	private Entity createLimb(String name,boolean isRight) {
+		DHComponent [] dh = new DHComponent[3];
 		for(int i=0;i<dh.length;++i) {
 			dh[i] = new DHComponent();
-			dh[i].set(0,5,0,0,120,-120);
 			dh[i].setVisible(true);
 		}
 		Entity limb = new Entity(name);
 		limb.addComponent(new PoseComponent());
 		limb.addComponent(new RobotComponent());
 
+		OriginAdjustComponent oac = new OriginAdjustComponent();
+
 		Entity hip = new Entity("Hip");
-		hip.addComponent(new PoseComponent());
-		hip.addComponent(dh[0]);
-		dh[0].set(4.5,11,90,degrees,degrees+30,degrees-30);
-		if(isRight) hip.addEntity(createMesh("/Spidee/shoulder_right.stl"));
-		else        hip.addEntity(createMesh("/Spidee/shoulder_left.stl"));
-
-		Entity thigh = new Entity("Thigh");
-		thigh.addComponent(new PoseComponent());
-		thigh.addComponent(dh[1]);
-		thigh.addEntity(createMesh("/Spidee/thigh.stl"));
-
-		Entity calf = new Entity("Calf");
-		calf.addComponent(new PoseComponent());
-		calf.addComponent(dh[2]);
-		if(isRight) calf.addEntity(createMesh("/Spidee/shin_right.stl"));
-		else		calf.addEntity(createMesh("/Spidee/shin_left.stl"));
-
-		Entity foot = new Entity("Foot");
-		foot.addComponent(new PoseComponent());
-		foot.addComponent(dh[3]);
-		dh[3].setR(0);
-		foot.addComponent(new ArmEndEffectorComponent());
-
 		limb.addEntity(hip);
+		Entity thigh = new Entity("Thigh");
 		hip.addEntity(thigh);
+		Entity calf = new Entity("Calf");
 		thigh.addEntity(calf);
+		Entity foot = new Entity("Foot");
 		calf.addEntity(foot);
+
+		hip.addComponent(dh[0]);
+		dh[0].set(0,2.2,90,0,30,-30);
+		if(isRight) hip.addEntity(createMesh("/Spidee/shoulder_right.obj"));
+		else        hip.addEntity(createMesh("/Spidee/shoulder_left.obj"));
+		hip.addComponent(oac);
+		oac.adjust();
+		hip.removeComponent(oac);
+
+		thigh.addComponent(dh[1]);
+		dh[1].set( 0,8.5,0,0,120,-120);
+		thigh.addEntity(createMesh("/Spidee/thigh.obj"));
+		thigh.addComponent(oac);
+		oac.adjust();
+		thigh.removeComponent(oac);
+
+		calf.addComponent(dh[2]);
+		dh[2].set(0,10.5,0,0,120,-120);
+		if(isRight) calf.addEntity(createMesh("/Spidee/calf_right.obj"));
+		else		calf.addEntity(createMesh("/Spidee/calf_left.obj"));
+		calf.addComponent(oac);
+		oac.adjust();
+		calf.removeComponent(oac);
+
+		foot.addComponent(new PoseComponent());
+		foot.addComponent(new ArmEndEffectorComponent());
 
 		return limb;
 	}
@@ -113,8 +117,9 @@ public class DemoSpidee implements Demo {
 
 		Entity mesh = new Entity("Mesh");
 		mesh.addComponent(new PoseComponent());
-		mesh.addComponent(mff);
+		mesh.addComponent(new OriginAdjustComponent());
 		mesh.addComponent(new MaterialComponent());
+		mesh.addComponent(mff);
 
 		return mesh;
 	}
