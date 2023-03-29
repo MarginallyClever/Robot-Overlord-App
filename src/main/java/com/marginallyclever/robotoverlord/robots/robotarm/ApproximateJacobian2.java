8a89baa834072003aa8ba6ae2f7ddca214c74bb9
Dan Royer <dan@marginallyclever.com>
2023-03-28 18:57:49 -0700

    Update ApproximateJacobian2.java

--- src/main/java/com/marginallyclever/robotoverlord/robots/robotarm/ApproximateJacobian2.java
+++ src/main/java/com/marginallyclever/robotoverlord/robots/robotarm/ApproximateJacobian2.java
@@ -14,7 +14,7 @@ import javax.vecmath.Matrix4d;
  *      Academy tutorial</a>
  */
 public class ApproximateJacobian2 {
-	static public final double ANGLE_STEP_SIZE_DEGREES = 0.01; // degrees
+	static public final double ANGLE_STEP_SIZE_DEGREES = 0.1; // degrees
 	private final RobotComponent myArm;
 
 	/**
