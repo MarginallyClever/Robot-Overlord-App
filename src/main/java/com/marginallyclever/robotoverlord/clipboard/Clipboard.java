88e420ab8cdb0f9c1fdacd44032dd6e6424366db
Dan Royer <dan@marginallyclever.com>
2023-04-04 11:51:36 -0700

    create Clipboard and ClipboardListener

---
+++ src/main/java/com/marginallyclever/robotoverlord/clipboard/Clipboard.java
@@ -0,0 +1,79 @@
+package com.marginallyclever.robotoverlord.clipboard;
+
+import com.marginallyclever.robotoverlord.Component;
+import com.marginallyclever.robotoverlord.Entity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * A static class that stores {@link Entity} and {@link Component} data for copying and pasting.
+ * @author Dan Royer
+ * @since 2.4.0
+ */
+public class Clipboard {
+    /**
+     * The list of entities selected in the editor.  This list is used by Actions.
+     */
+    private static final List<Entity> selectedEntities = new ArrayList<>();
+
+    /**
+     * The list of entities copied to the clipboard.  This list is used by Actions.
+     */
+    private static Entity copiedEntities = new Entity();
+
+    /**
+     * The list of components copied to the clipboard.  This list is used by Actions.
+     */
+    private static Component copiedComponents = null;
+
+    private static final List<ClipboardListener> listeners = new ArrayList<>();
+
+    public static void setCopiedEntities(Entity container) {
+        copiedEntities=container;
+        fireClipboardChanged();
+    }
+
+    public static Entity getCopiedEntities() {
+        return copiedEntities;
+    }
+
+    public static void setCopiedComponents(Component container) {
+        copiedComponents=container;
+        fireClipboardChanged();
+    }
+
+    public static Component getCopiedComponents() {
+        return copiedComponents;
+    }
+
+    public static List<Entity> getSelectedEntities() {
+        return new ArrayList<>(selectedEntities);
+    }
+
+    public static void setSelectedEntity(Entity entity) {
+        List<Entity> list = new ArrayList<>();
+        if(entity!=null) list.add(entity);
+        setSelectedEntities(list);
+    }
+
+    public static void setSelectedEntities(List<Entity> list) {
+        selectedEntities.clear();
+        selectedEntities.addAll(list);
+        fireClipboardChanged();
+    }
+
+    public static void addListener(ClipboardListener l) {
+        listeners.add(l);
+    }
+
+    public static void removeListener(ClipboardListener l) {
+        listeners.remove(l);
+    }
+
+    private static void fireClipboardChanged() {
+        for(ClipboardListener l : listeners) {
+            l.onClipboardChanged();
+        }
+    }
+}
