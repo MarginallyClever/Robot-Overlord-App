4e05535724022030974ee1282f3a43b24ac0ea25
Dan Royer <dan@marginallyclever.com>
2023-04-11 09:43:59 -0700

    document interface

--- src/main/java/com/marginallyclever/robotoverlord/tools/EditorTool.java
+++ src/main/java/com/marginallyclever/robotoverlord/tools/EditorTool.java
@@ -1,10 +1,11 @@
 package com.marginallyclever.robotoverlord.tools;
 
 import com.jogamp.opengl.GL2;
+import com.marginallyclever.robotoverlord.Viewport;
+import com.marginallyclever.robotoverlord.components.CameraComponent;
 
 import java.awt.event.KeyEvent;
 import java.awt.event.MouseEvent;
-import java.awt.event.MouseWheelEvent;
 
 /**
  * Interface for tools that can be used to visually edit the scene.
@@ -12,12 +13,47 @@ import java.awt.event.MouseWheelEvent;
  * @author Dan Royer
  */
 public interface EditorTool {
-    public void mousePressed(MouseEvent e);
-    public void mouseReleased(MouseEvent e);
-    public void mouseDragged(MouseEvent e);
-    public void mouseMoved(MouseEvent e);
-    public void keyPressed(KeyEvent e);
-    public void keyReleased(KeyEvent e);
 
-    void mouseWheelMoved(MouseWheelEvent e);
+    /**
+     * This method is called when the tool is activated. It receives the SelectedItems object containing the selected
+     * entities and their initial world poses.
+     *
+     * @param selectedItems The selected items to be manipulated by the tool.
+     */
+    void activate(SelectedItems selectedItems);
+
+    /**
+     * This method is called when the tool is deactivated. It allows the tool to perform any necessary cleanup
+     * actions before another tool takes over.
+     */
+    void deactivate();
+
+
+    /**
+     * Handles mouse input events for the tool.
+     *
+     * @param event The MouseEvent object representing the input event.
+     */
+    public void handleMouseEvent(MouseEvent event);
+
+    /**
+     * Handles keyboard input events for the tool.
+     *
+     * @param event The KeyEvent object representing the input event.
+     */
+    public void handleKeyEvent(KeyEvent event);
+
+    /**
+     * Updates the tool's internal state, if necessary.
+     *
+     * @param deltaTime Time elapsed since the last update.
+     */
+    void update(double deltaTime);
+
+    /**
+     * Renders any tool-specific visuals to the 3D scene.
+     */
+    void render(GL2 gl2);
+
+    void setViewport(Viewport viewport);
 }
