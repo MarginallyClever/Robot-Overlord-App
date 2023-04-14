8a78a3403838fae3d13621cdc04d61a48a3a4823
Dan Royer <dan@marginallyclever.com>
2023-04-14 11:08:27 -0700

    Update MaterialLoader.java

--- src/main/java/com/marginallyclever/robotoverlord/components/material/MaterialLoader.java
+++ src/main/java/com/marginallyclever/robotoverlord/components/material/MaterialLoader.java
@@ -1,4 +1,16 @@
 package com.marginallyclever.robotoverlord.components.material;
 
+import java.io.BufferedInputStream;
+
 public interface MaterialLoader {
+    String getEnglishName();
+    String[] getValidExtensions();
+
+    /**
+     * Load data from stream
+     * @param inputStream source of data
+     * @param material material into which data will be loaded
+     * @throws Exception if something goes wrong
+     */
+    void load(BufferedInputStream inputStream, MaterialComponent material) throws Exception;
 }
