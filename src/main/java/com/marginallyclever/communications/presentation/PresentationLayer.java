2dacdc895ef5b9783de6f0a45de7c3ab5c65d881
Dan Royer <dan@marginallyclever.com>
2023-04-18 09:36:56 -0700

    flesh out PresentationLayer

--- src/main/java/com/marginallyclever/communications/presentation/PresentationLayer.java
+++ src/main/java/com/marginallyclever/communications/presentation/PresentationLayer.java
@@ -1,4 +1,21 @@
 package com.marginallyclever.communications.presentation;
 
+import com.marginallyclever.communications.SessionLayer;
+
+/**
+ * In the <a href="https://en.wikipedia.org/wiki/OSI_model">, the Presentation layer is responsible for translating
+ * data between the application layer and the session.
+ *
+ * @author Dan Royer
+ * @since 2.5.0
+ */
 public interface PresentationLayer {
+
+    void onConnect(SessionLayer sessionLayer);
+
+    void onDisconnect(SessionLayer sessionLayer);
+
+    void processIncomingMessage(String message);
+
+    void processOutgoingMessage(String message);
 }
