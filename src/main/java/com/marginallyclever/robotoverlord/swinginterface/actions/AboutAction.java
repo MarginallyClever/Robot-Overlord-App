d915d04590fbd1716f16d424b0f91154275a5c43
Dan Royer <dan@marginallyclever.com>
2023-04-25 12:59:58 -0700

    added "testers"

--- src/main/java/com/marginallyclever/robotoverlord/swinginterface/actions/AboutAction.java
+++ src/main/java/com/marginallyclever/robotoverlord/swinginterface/actions/AboutAction.java
@@ -15,11 +15,11 @@ import java.io.Serial;
  */
 public class AboutAction extends AbstractAction implements ActionListener {
 	/**
-	 * 
+	 *
 	 */
 	@Serial
 	private static final long serialVersionUID = 1L;
-	
+
 	public AboutAction() {
 		super(Translator.get("About"));
         putValue(SHORT_DESCRIPTION, Translator.get("About this program"));
@@ -27,13 +27,22 @@ public class AboutAction extends AbstractAction implements ActionListener {
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		JOptionPane.showMessageDialog(null,
-			"<html><body>"
-			+"<h1>"+RobotOverlord.APP_TITLE+" "+RobotOverlord.VERSION+"</h1>"
-			+"<h3><a href='http://www.marginallyclever.com/'>http://www.marginallyclever.com/</a></h3>"
-			+"<p>Created by Dan Royer (dan@marginallyclever.com).</p><br>"
-			+"<p>To get the latest version please visit<br><a href='"+RobotOverlord.APP_URL+"'>"+RobotOverlord.APP_URL+"</a></p><br>"
-			+"<p>This program is open source and free.  If this was helpful<br> to you, please buy me a thank you beer through Paypal.</p>"
-			+"</body></html>");
+		JOptionPane.showMessageDialog(
+				null,
+				"<html><body>"
+						+"<h1>"+RobotOverlord.APP_TITLE+" "+RobotOverlord.VERSION+"</h1>"
+						+"<h3><a href='http://www.marginallyclever.com/'>http://www.marginallyclever.com/</a></h3>"
+						+"<h4>Created by</h4>"
+						+"<p>Dan Royer (dan@marginallyclever.com).</p>"
+						+"<h4>Testers</h4>"
+						+"<p>ArcRobotics</p>"
+						+"<h4>More info</h4>"
+						+"<p>To get the latest version please visit<br><a href='"+RobotOverlord.APP_URL+"'>"+RobotOverlord.APP_URL+"</a></p><br>"
+						+"<p>This program is open source and free.  If this was helpful<br> to you, please buy me a thank you beer through Paypal.</p>"
+						+"</body></html>");
+	}
+
+	public static void main(String[] args) {
+		new AboutAction().actionPerformed(null);
 	}
 }
