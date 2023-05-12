f9e5fdbb290dc436c66dd43642d254574fbaf701
Dan Royer <dan@marginallyclever.com>
2023-05-12 08:37:03 -0700

    improved utilities

--- src/main/java/com/marginallyclever/robotoverlord/ShaderProgram.java
+++ src/main/java/com/marginallyclever/robotoverlord/ShaderProgram.java
@@ -1,21 +1,35 @@
 package com.marginallyclever.robotoverlord;
-import com.jogamp.opengl.GL2;
-import java.util.Arrays;
 
+import com.jogamp.opengl.GL2;
+import com.marginallyclever.convenience.MatrixHelper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.vecmath.Matrix4d;
+import javax.vecmath.Vector3d;
+import java.nio.FloatBuffer;
+
+/**
+ * A wrapper for vertex and fragment shader pairs that provides a simple interface for setting uniforms.
+ *
+ * @author Dan Royer
+ * @since 2.5.9
+ */
 public class ShaderProgram {
+    private static final Logger logger = LoggerFactory.getLogger(ShaderProgram.class);
     private final int programId;
     private final int vertexShaderId;
     private final int fragmentShaderId;
 
     public ShaderProgram(GL2 gl, String[] vertexCode, String[] fragmentCode) {
-        vertexShaderId = loadShader(gl, GL2.GL_VERTEX_SHADER, vertexCode);
-        fragmentShaderId = loadShader(gl, GL2.GL_FRAGMENT_SHADER, fragmentCode);
+        vertexShaderId = loadShader(gl, GL2.GL_VERTEX_SHADER, vertexCode,"vertex");
+        fragmentShaderId = loadShader(gl, GL2.GL_FRAGMENT_SHADER, fragmentCode,"fragment");
         programId = gl.glCreateProgram();
         gl.glAttachShader(programId, vertexShaderId);
         gl.glAttachShader(programId, fragmentShaderId);
         gl.glLinkProgram(programId);
         if (!checkStatus(gl, programId, GL2.GL_LINK_STATUS)) {
-            showProgramError(gl,"Failed to link shader program: ");
+            showProgramError(gl, "Failed to link shader program: ");
         } else {
             gl.glValidateProgram(programId);
             if (!checkStatus(gl, programId, GL2.GL_VALIDATE_STATUS)) {
@@ -31,10 +45,11 @@ public class ShaderProgram {
         byte[] log = new byte[logLength[0]];
         gl.glGetProgramInfoLog(programId, logLength[0], null, 0, log, 0);
 
-        System.err.println(message+ new String(log));
+        System.err.println(message + new String(log));
+        logger.error(message + new String(log));
     }
 
-    private int loadShader(GL2 gl, int type, String[] shaderCode) {
+    private int loadShader(GL2 gl, int type, String[] shaderCode,String name) {
         int shaderId = gl.glCreateShader(type);
         gl.glShaderSource(shaderId, shaderCode.length, shaderCode, null, 0);
         gl.glCompileShader(shaderId);
@@ -45,13 +60,15 @@ public class ShaderProgram {
             byte[] log = new byte[logLength[0]];
             gl.glGetShaderInfoLog(shaderId, logLength[0], null, 0, log, 0);
 
-            System.err.println("Failed to compile shader code: "+ new String(log));
+            System.err.println("Failed to compile "+name+" shader code: " + new String(log));
+            logger.error("Failed to compile "+name+" shader code: " + new String(log));
         }
         return shaderId;
     }
 
     /**
      * Check the status of a shader or program.
+     *
      * @param gl    The OpenGL context
      * @param id    The shader or program id
      * @param param The parameter to check
@@ -83,7 +100,31 @@ public class ShaderProgram {
         return programId;
     }
 
-    public int getUniformLocation(GL2 gl, String name) {
-        return gl.glGetUniformLocation(programId, name);
+    public int getUniformLocation(GL2 gl2, String name) {
+        return gl2.glGetUniformLocation(programId, name);
+    }
+
+    public void set1f(GL2 gl2, String name, float v0) {
+        gl2.glUniform1f(getUniformLocation(gl2, name), v0);
+    }
+
+    public void set2f(GL2 gl2, String name, float v0, float v1) {
+        gl2.glUniform2f(getUniformLocation(gl2, name), v0, v1);
+    }
+
+    public void set3f(GL2 gl2, String name, float v0, float v1, float v2) {
+        gl2.glUniform3f(getUniformLocation(gl2, name), v0, v1, v2);
+    }
+
+    public void set4f(GL2 gl2, String name, float v0, float v1, float v2, float v3) {
+        gl2.glUniform4f(getUniformLocation(gl2, name), v0, v1, v2, v3);
+    }
+
+    public void setVector3d(GL2 gl2, String name, Vector3d v) {
+        gl2.glUniform3f(getUniformLocation(gl2, name), (float) v.x, (float) v.y, (float) v.z);
+    }
+
+    public void setMatrix4d(GL2 gl2, String name, Matrix4d matrix4d) {
+        gl2.glUniformMatrix4fv(getUniformLocation(gl2, name), 1, false, MatrixHelper.matrixToFloatBuffer(matrix4d));
     }
 }
