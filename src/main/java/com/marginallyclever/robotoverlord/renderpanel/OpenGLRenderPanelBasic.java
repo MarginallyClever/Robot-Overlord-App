d96d0777fb3eb06f326c714564dcaa943821d7be
Dan Royer <dan@marginallyclever.com>
2023-05-12 12:42:10 -0700

    renders OK with VAO

--- src/main/java/com/marginallyclever/robotoverlord/renderpanel/OpenGLRenderPanelBasic.java
+++ src/main/java/com/marginallyclever/robotoverlord/renderpanel/OpenGLRenderPanelBasic.java
@@ -107,8 +107,8 @@ public class OpenGLRenderPanelBasic implements RenderPanel {
 
                 createFragmentShader(gl2);
 
-                myVertexBuffer = rawSetupVBO(gl2);
                 myArrayBuffer = rawSetupVAO(gl2);
+                myVertexBuffer = rawSetupVBO(gl2);
             }
 
             @Override
@@ -132,11 +132,10 @@ public class OpenGLRenderPanelBasic implements RenderPanel {
 
                 //testRaw(gl2);
                 //testRawWithShader(gl2);
-                testRawWithShaderAndSetup(gl2);
-                //testRawWithShaderAndSetupVAO(gl2);
+                //testRawWithShaderAndSetup(gl2);
+                testRawWithShaderAndSetupVAO(gl2);
                 //testShaderAndMesh(gl2);
             }
-
         });
     }
 
@@ -149,12 +148,22 @@ public class OpenGLRenderPanelBasic implements RenderPanel {
     private void testRawWithShaderAndSetupVAO(GL2 gl2) {
         shaderDefault.use(gl2);
 
-        gl2.glEnableVertexAttribArray(0);
-        gl2.glEnableVertexAttribArray(1);
-        //gl2.glBindVertexArray(myArrayBuffer[0]);
-        gl2.glDrawArrays(GL.GL_TRIANGLES, 0, 3);
-        gl2.glDisableVertexAttribArray(1);
-        gl2.glDisableVertexAttribArray(0);
+        int posAttribLocation = gl2.glGetAttribLocation(shaderDefault.getProgramId(), "aPosition");
+        int colorAttribLocation = gl2.glGetAttribLocation(shaderDefault.getProgramId(), "aColor");
+
+        gl2.glEnableVertexAttribArray(posAttribLocation);
+        gl2.glBindBuffer(GL.GL_ARRAY_BUFFER, myVertexBuffer[0]);
+        gl2.glVertexAttribPointer(posAttribLocation,3,GL2.GL_FLOAT,false,0,0);
+
+        gl2.glEnableVertexAttribArray(colorAttribLocation);
+        gl2.glBindBuffer(GL.GL_ARRAY_BUFFER, myVertexBuffer[1]);
+        gl2.glVertexAttribPointer(colorAttribLocation,4,GL2.GL_FLOAT,false,0,0);
+
+        // Draw the triangle !
+        gl2.glDrawArrays(GL2.GL_TRIANGLES, 0, 3);
+
+        gl2.glDisableVertexAttribArray(posAttribLocation);
+        gl2.glDisableVertexAttribArray(colorAttribLocation);
 
         gl2.glUseProgram(0);
     }
@@ -181,7 +190,7 @@ public class OpenGLRenderPanelBasic implements RenderPanel {
         rawCleanupVBO(gl2,vertexBuffer);
     }
 
-    private void rawRender(GL2 gl2,int[] vertexBuffer) {
+    private void rawRender(GL2 gl2,int[] vertexBuffer) {/*
         int posAttribLocation = gl2.glGetAttribLocation(shaderDefault.getProgramId(), "aPosition");
         int colorAttribLocation = gl2.glGetAttribLocation(shaderDefault.getProgramId(), "aColor");
 
@@ -197,12 +206,16 @@ public class OpenGLRenderPanelBasic implements RenderPanel {
         gl2.glDrawArrays(GL2.GL_TRIANGLES, 0, 3);
 
         gl2.glDisableVertexAttribArray(posAttribLocation);
-        gl2.glDisableVertexAttribArray(colorAttribLocation);
+        gl2.glDisableVertexAttribArray(colorAttribLocation);*/
     }
 
-    private void rawCleanupVBO(GL2 gl2, int[] vertexBuffer) {
-        gl2.glDeleteBuffers(vertexBuffer.length,vertexBuffer,0);
+    private int[] rawSetupVAO(GL2 gl2) {
+        int [] arrayBuffer = new int[1];
+        gl2.glGenVertexArrays(1, arrayBuffer,0);
+        gl2.glBindVertexArray(arrayBuffer[0]);
+        return arrayBuffer;
     }
+
     private void rawCleanupVAO(GL2 gl2, int[] arrayBuffer) {
         gl2.glDeleteVertexArrays(arrayBuffer.length,arrayBuffer,0);
     }
@@ -227,6 +240,10 @@ public class OpenGLRenderPanelBasic implements RenderPanel {
         return vertexBuffer;
     }
 
+    private void rawCleanupVBO(GL2 gl2, int[] vertexBuffer) {
+        gl2.glDeleteBuffers(vertexBuffer.length,vertexBuffer,0);
+    }
+
     private FloatBuffer createVertexData() {
         FloatBuffer vertexData = FloatBuffer.wrap(new float[]{
                 -1f,-1f,0f,
@@ -245,24 +262,6 @@ public class OpenGLRenderPanelBasic implements RenderPanel {
         });
         colorData.rewind();
         return colorData;
-    }
-
-    private int[] rawSetupVAO(GL2 gl2) {
-        int [] arrayBuffer = new int[1];
-        gl2.glGenVertexArrays(1, arrayBuffer,0);
-        gl2.glBindVertexArray(arrayBuffer[0]);
-
-        gl2.glEnableVertexAttribArray(0);
-        gl2.glBindBuffer(GL2.GL_ARRAY_BUFFER, myVertexBuffer[0]);
-        gl2.glVertexAttribPointer(0, 3, GL.GL_FLOAT, false, 0, 0);
-        gl2.glDisableVertexAttribArray(0);
-
-        gl2.glEnableVertexAttribArray(1);
-        gl2.glBindBuffer(GL2.GL_ARRAY_BUFFER, myVertexBuffer[1]);
-        gl2.glVertexAttribPointer(0, 4, GL.GL_FLOAT, false, 0, 0);
-        gl2.glDisableVertexAttribArray(1);
-
-        return arrayBuffer;
     }
 
     private void testShaderAndMesh(GL2 gl2) {
