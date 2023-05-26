fa74c6b181b3dfe62037e42a603ceff03b589f39
Dan Royer <dan@marginallyclever.com>
2023-05-26 13:22:05 -0700

    Create MatrixMaterialRender.java

---
+++ src/main/java/com/marginallyclever/robotoverlord/renderpanel/MatrixMaterialRender.java
@@ -0,0 +1,12 @@
+package com.marginallyclever.robotoverlord.renderpanel;
+
+import com.marginallyclever.robotoverlord.components.MaterialComponent;
+import com.marginallyclever.robotoverlord.components.RenderComponent;
+
+import javax.vecmath.Matrix4d;
+
+public class MatrixMaterialRender {
+    public Matrix4d matrix = new Matrix4d();
+    public RenderComponent renderComponent;
+    public MaterialComponent materialComponent;
+}
