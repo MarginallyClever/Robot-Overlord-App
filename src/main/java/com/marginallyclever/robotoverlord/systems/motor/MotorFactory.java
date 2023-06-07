246c58d11b6c22f39d15fa8353ea103b833f842b
Dan Royer <dan@marginallyclever.com>
2023-06-07 13:18:36 -0700

    added servo PID defaults

--- src/main/java/com/marginallyclever/robotoverlord/systems/motor/MotorFactory.java
+++ src/main/java/com/marginallyclever/robotoverlord/systems/motor/MotorFactory.java
@@ -2,6 +2,7 @@ package com.marginallyclever.robotoverlord.systems.motor;
 
 import com.marginallyclever.robotoverlord.components.motors.MotorComponent;
 import com.marginallyclever.robotoverlord.components.motors.ServoComponent;
+import com.marginallyclever.robotoverlord.components.motors.StepperMotorComponent;
 
 /**
  * produce common motors in robotics, prefilled with torque curve.
@@ -14,16 +15,37 @@ public class MotorFactory {
     }
 
     public static void setDefaultMotorCurve(MotorComponent mc) {
-        mc.setTorqueAtRPM(0, 3);
-        mc.setTorqueAtRPM(100, 2.5);
-        mc.setTorqueAtRPM(200, 2);
-        mc.setTorqueAtRPM(300, 1);
-        mc.setTorqueAtRPM(400, 0);
+        mc.setTorqueAtRPM( 0,16);
+        mc.setTorqueAtRPM(30,16);
+        mc.setTorqueAtRPM(60,15);
+        mc.setTorqueAtRPM(90,12);
+        mc.setTorqueAtRPM(180,6);
+        mc.setTorqueAtRPM(240,1.8);
     }
 
     public static ServoComponent createDefaultServo() {
         ServoComponent sc = new ServoComponent();
         MotorFactory.setDefaultMotorCurve(sc);
+
+        sc.kP.set(Math.E);
+        sc.kI.set(0.0);
+        sc.kD.set(70-Math.E);
+
         return sc;
+    }
+
+    /**
+     * Curve matches 17HS13-0404S1 200-step 0.4A NEMA 17 stepper motor.
+     * @return a stepper motor with a default torque curve.
+     */
+    public static StepperMotorComponent createDefaultStepperMotor() {
+        StepperMotorComponent smc = new StepperMotorComponent();
+        smc.setTorqueAtRPM( 0,16);
+        smc.setTorqueAtRPM(30,16);
+        smc.setTorqueAtRPM(60,15);
+        smc.setTorqueAtRPM(90,12);
+        smc.setTorqueAtRPM(180,6);
+        smc.setTorqueAtRPM(240,1.8);
+        return smc;
     }
 }
