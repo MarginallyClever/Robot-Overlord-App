855c0c87def2c4cfc4abce2038a6b4e084f2f53c
Dan Royer <dan@marginallyclever.com>
2023-06-07 13:18:03 -0700

    added PID terms

--- src/main/java/com/marginallyclever/robotoverlord/components/motors/ServoComponent.java
+++ src/main/java/com/marginallyclever/robotoverlord/components/motors/ServoComponent.java
@@ -13,11 +13,25 @@ import org.json.JSONObject;
  */
 public class ServoComponent extends MotorComponent {
     public final DoubleParameter desiredAngle = new DoubleParameter("Target angle", 0);
+    public final DoubleParameter minAngle = new DoubleParameter("Min angle", -90);
+    public final DoubleParameter maxAngle = new DoubleParameter("Max angle", 90);
+    public final DoubleParameter kP = new DoubleParameter("kP", 1.0);
+    public final DoubleParameter kI = new DoubleParameter("kI", 0.0);
+    public final DoubleParameter kD = new DoubleParameter("kD", 0.0);
+    public final DoubleParameter errorSum = new DoubleParameter("Integral", 0);
+    public final DoubleParameter lastError = new DoubleParameter("Last error", 0);
 
     @Override
     public JSONObject toJSON(SerializationContext context) {
         JSONObject json = super.toJSON(context);
         json.put("desiredAngle", desiredAngle.toJSON(context));
+        json.put("minAngle", minAngle.toJSON(context));
+        json.put("maxAngle", maxAngle.toJSON(context));
+        json.put("kP", kP.toJSON(context));
+        json.put("kI", kI.toJSON(context));
+        json.put("kD", kD.toJSON(context));
+        json.put("integral", errorSum.toJSON(context));
+        json.put("lastError", lastError.toJSON(context));
         return json;
     }
 
@@ -25,12 +39,29 @@ public class ServoComponent extends MotorComponent {
     public void parseJSON(JSONObject jo, SerializationContext context) throws JSONException {
         super.parseJSON(jo, context);
         desiredAngle.parseJSON(jo.getJSONObject("desiredAngle"), context);
+        minAngle.parseJSON(jo.getJSONObject("minAngle"), context);
+        maxAngle.parseJSON(jo.getJSONObject("maxAngle"), context);
+        kP.parseJSON(jo.getJSONObject("kP"), context);
+        kI.parseJSON(jo.getJSONObject("kI"), context);
+        kD.parseJSON(jo.getJSONObject("kD"), context);
+        errorSum.parseJSON(jo.getJSONObject("integral"), context);
+        lastError.parseJSON(jo.getJSONObject("lastError"), context);
     }
 
     @Override
     public String toString() {
         return super.toString()
                 + ", desiredAngle=" + desiredAngle
+                + ", minAngle=" + minAngle
+                + ", maxAngle=" + maxAngle
+                + ", kP=" + kP
+                + ", kI=" + kI
+                + ", kD=" + kD
                 + ",\n";
+    }
+
+    public void resetPIDMemory() {
+        errorSum.set(0.0);
+        lastError.set(0.0);
     }
 }
