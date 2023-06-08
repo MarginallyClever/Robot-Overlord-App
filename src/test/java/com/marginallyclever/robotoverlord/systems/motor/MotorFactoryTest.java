4e0151d8eb6bacf2a84387b04a844a8a5662fb6d
Dan Royer <dan@marginallyclever.com>
2023-06-08 12:18:13 -0700

    update test for new motor defaults

--- src/test/java/com/marginallyclever/robotoverlord/systems/motor/MotorFactoryTest.java
+++ src/test/java/com/marginallyclever/robotoverlord/systems/motor/MotorFactoryTest.java
@@ -9,12 +9,13 @@ public class MotorFactoryTest {
     public void testCreateDefaultMotor() {
         MotorComponent mc = MotorFactory.createDefaultMotor();
 
-        Assertions.assertEquals(3,mc.getTorqueAtRpm(0));
-        Assertions.assertEquals(2.5,mc.getTorqueAtRpm(100));
-        Assertions.assertEquals(2,mc.getTorqueAtRpm(200));
-        Assertions.assertEquals(1,mc.getTorqueAtRpm(300));
-        Assertions.assertEquals(0,mc.getTorqueAtRpm(400));
+        Assertions.assertEquals(16.0,mc.getTorqueAtRpm(  0));
+        Assertions.assertEquals(16.0,mc.getTorqueAtRpm( 30));
+        Assertions.assertEquals(15.0,mc.getTorqueAtRpm( 60));
+        Assertions.assertEquals(12.0,mc.getTorqueAtRpm( 90));
+        Assertions.assertEquals( 6.0,mc.getTorqueAtRpm(180));
+        Assertions.assertEquals( 1.8,mc.getTorqueAtRpm(240));
         // test interpolation
-        Assertions.assertEquals(1.5,mc.getTorqueAtRpm(250));
+        Assertions.assertEquals(13.5,mc.getTorqueAtRpm(75));
     }
 }
