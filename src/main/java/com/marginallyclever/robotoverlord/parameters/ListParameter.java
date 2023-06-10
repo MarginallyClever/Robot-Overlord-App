b8f1c1593c565a997c383425b7081e292b13e75e
Dan Royer <dan@marginallyclever.com>
2023-06-10 12:34:10 -0700

    implements List<> instead of Collection<>

--- src/main/java/com/marginallyclever/robotoverlord/parameters/ListParameter.java
+++ src/main/java/com/marginallyclever/robotoverlord/parameters/ListParameter.java
@@ -1,6 +1,5 @@
 package com.marginallyclever.robotoverlord.parameters;
 
-import com.fasterxml.jackson.databind.deser.impl.CreatorCandidate;
 import com.marginallyclever.robotoverlord.SerializationContext;
 import org.jetbrains.annotations.NotNull;
 import org.json.JSONObject;
@@ -8,18 +7,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Parameter;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.List;
+import java.util.*;
 import java.util.function.IntFunction;
 
 /**
  * a list of parameters
  * @param <T> the type of parameter
  */
-public class ListParameter<T extends AbstractParameter<?>> extends AbstractParameter<List<T>> implements Collection<T> {
+public class ListParameter<T extends AbstractParameter<?>> extends AbstractParameter<List<T>> implements List<T> {
     private static final Logger logger = LoggerFactory.getLogger(ListParameter.class);
     private final T instance;
 
@@ -71,8 +66,8 @@ public class ListParameter<T extends AbstractParameter<?>> extends AbstractParam
     }
 
     @Override
-    public boolean add(T referenceParameter) {
-        return get().add(referenceParameter);
+    public boolean add(T item) {
+        return get().add(item);
     }
 
     /**
@@ -97,7 +92,130 @@ public class ListParameter<T extends AbstractParameter<?>> extends AbstractParam
      */
     @Override
     public boolean remove(Object o) {
-        return false;
+        return get().remove(o);
+    }
+
+    @Override
+    public T remove(int index) {
+        return get().remove(index);
+    }
+
+    /**
+     * Returns the index of the first occurrence of the specified element
+     * in this list, or -1 if this list does not contain the element.
+     * More formally, returns the lowest index {@code i} such that
+     * {@code Objects.equals(o, get(i))},
+     * or -1 if there is no such index.
+     *
+     * @param o element to search for
+     * @return the index of the first occurrence of the specified element in
+     * this list, or -1 if this list does not contain the element
+     * @throws ClassCastException   if the type of the specified element
+     *                              is incompatible with this list
+     *                              (<a href="Collection.html#optional-restrictions">optional</a>)
+     * @throws NullPointerException if the specified element is null and this
+     *                              list does not permit null elements
+     *                              (<a href="Collection.html#optional-restrictions">optional</a>)
+     */
+    @Override
+    public int indexOf(Object o) {
+        return 0;
+    }
+
+    /**
+     * Returns the index of the last occurrence of the specified element
+     * in this list, or -1 if this list does not contain the element.
+     * More formally, returns the highest index {@code i} such that
+     * {@code Objects.equals(o, get(i))},
+     * or -1 if there is no such index.
+     *
+     * @param o element to search for
+     * @return the index of the last occurrence of the specified element in
+     * this list, or -1 if this list does not contain the element
+     * @throws ClassCastException   if the type of the specified element
+     *                              is incompatible with this list
+     *                              (<a href="Collection.html#optional-restrictions">optional</a>)
+     * @throws NullPointerException if the specified element is null and this
+     *                              list does not permit null elements
+     *                              (<a href="Collection.html#optional-restrictions">optional</a>)
+     */
+    @Override
+    public int lastIndexOf(Object o) {
+        return 0;
+    }
+
+    /**
+     * Returns a list iterator over the elements in this list (in proper
+     * sequence).
+     *
+     * @return a list iterator over the elements in this list (in proper
+     * sequence)
+     */
+    @NotNull
+    @Override
+    public ListIterator<T> listIterator() {
+        return get().listIterator();
+    }
+
+    /**
+     * Returns a list iterator over the elements in this list (in proper
+     * sequence), starting at the specified position in the list.
+     * The specified index indicates the first element that would be
+     * returned by an initial call to {@link ListIterator#next next}.
+     * An initial call to {@link ListIterator#previous previous} would
+     * return the element with the specified index minus one.
+     *
+     * @param index index of the first element to be returned from the
+     *              list iterator (by a call to {@link ListIterator#next next})
+     * @return a list iterator over the elements in this list (in proper
+     * sequence), starting at the specified position in the list
+     * @throws IndexOutOfBoundsException if the index is out of range
+     *                                   ({@code index < 0 || index > size()})
+     */
+    @NotNull
+    @Override
+    public ListIterator<T> listIterator(int index) {
+        return get().listIterator(index);
+    }
+
+    /**
+     * Returns a view of the portion of this list between the specified
+     * {@code fromIndex}, inclusive, and {@code toIndex}, exclusive.  (If
+     * {@code fromIndex} and {@code toIndex} are equal, the returned list is
+     * empty.)  The returned list is backed by this list, so non-structural
+     * changes in the returned list are reflected in this list, and vice-versa.
+     * The returned list supports all of the optional list operations supported
+     * by this list.<p>
+     * <p>
+     * This method eliminates the need for explicit range operations (of
+     * the sort that commonly exist for arrays).  Any operation that expects
+     * a list can be used as a range operation by passing a subList view
+     * instead of a whole list.  For example, the following idiom
+     * removes a range of elements from a list:
+     * <pre>{@code
+     *      list.subList(from, to).clear();
+     * }</pre>
+     * Similar idioms may be constructed for {@code indexOf} and
+     * {@code lastIndexOf}, and all of the algorithms in the
+     * {@code Collections} class can be applied to a subList.<p>
+     * <p>
+     * The semantics of the list returned by this method become undefined if
+     * the backing list (i.e., this list) is <i>structurally modified</i> in
+     * any way other than via the returned list.  (Structural modifications are
+     * those that change the size of this list, or otherwise perturb it in such
+     * a fashion that iterations in progress may yield incorrect results.)
+     *
+     * @param fromIndex low endpoint (inclusive) of the subList
+     * @param toIndex   high endpoint (exclusive) of the subList
+     * @return a view of the specified range within this list
+     * @throws IndexOutOfBoundsException for an illegal endpoint index value
+     *                                   ({@code fromIndex < 0 || toIndex > size ||
+     *                                   fromIndex > toIndex})
+     */
+    @NotNull
+    @Override
+    public List<T> subList(int fromIndex, int toIndex) {
+        return get().subList(fromIndex, toIndex);
     }
 
     /**
@@ -120,7 +238,7 @@ public class ListParameter<T extends AbstractParameter<?>> extends AbstractParam
      */
     @Override
     public boolean containsAll(@NotNull Collection<?> c) {
-        return false;
+        return get().containsAll(c);
     }
 
     /**
@@ -150,6 +268,38 @@ public class ListParameter<T extends AbstractParameter<?>> extends AbstractParam
     @Override
     public boolean addAll(@NotNull Collection<? extends T> c) {
         return get().addAll(c);
+    }
+
+    /**
+     * Inserts all of the elements in the specified collection into this
+     * list at the specified position (optional operation).  Shifts the
+     * element currently at that position (if any) and any subsequent
+     * elements to the right (increases their indices).  The new elements
+     * will appear in this list in the order that they are returned by the
+     * specified collection's iterator.  The behavior of this operation is
+     * undefined if the specified collection is modified while the
+     * operation is in progress.  (Note that this will occur if the specified
+     * collection is this list, and it's nonempty.)
+     *
+     * @param index index at which to insert the first element from the
+     *              specified collection
+     * @param c     collection containing elements to be added to this list
+     * @return {@code true} if this list changed as a result of the call
+     * @throws UnsupportedOperationException if the {@code addAll} operation
+     *                                       is not supported by this list
+     * @throws ClassCastException            if the class of an element of the specified
+     *                                       collection prevents it from being added to this list
+     * @throws NullPointerException          if the specified collection contains one
+     *                                       or more null elements and this list does not permit null
+     *                                       elements, or if the specified collection is null
+     * @throws IllegalArgumentException      if some property of an element of the
+     *                                       specified collection prevents it from being added to this list
+     * @throws IndexOutOfBoundsException     if the index is out of range
+     *                                       ({@code index < 0 || index > size()})
+     */
+    @Override
+    public boolean addAll(int index, @NotNull Collection<? extends T> c) {
+        return get().addAll(index, c);
     }
 
     /**
@@ -220,11 +370,59 @@ public class ListParameter<T extends AbstractParameter<?>> extends AbstractParam
         return get().get(index);
     }
 
+    /**
+     * Replaces the element at the specified position in this list with the
+     * specified element (optional operation).
+     *
+     * @param index   index of the element to replace
+     * @param element element to be stored at the specified position
+     * @return the element previously at the specified position
+     * @throws UnsupportedOperationException if the {@code set} operation
+     *                                       is not supported by this list
+     * @throws ClassCastException            if the class of the specified element
+     *                                       prevents it from being added to this list
+     * @throws NullPointerException          if the specified element is null and
+     *                                       this list does not permit null elements
+     * @throws IllegalArgumentException      if some property of the specified
+     *                                       element prevents it from being added to this list
+     * @throws IndexOutOfBoundsException     if the index is out of range
+     *                                       ({@code index < 0 || index >= size()})
+     */
+    @Override
+    public T set(int index, T element) {
+        return get().set(index, element);
+    }
+
+    /**
+     * Inserts the specified element at the specified position in this list
+     * (optional operation).  Shifts the element currently at that position
+     * (if any) and any subsequent elements to the right (adds one to their
+     * indices).
+     *
+     * @param index   index at which the specified element is to be inserted
+     * @param element element to be inserted
+     * @throws UnsupportedOperationException if the {@code add} operation
+     *                                       is not supported by this list
+     * @throws ClassCastException            if the class of the specified element
+     *                                       prevents it from being added to this list
+     * @throws NullPointerException          if the specified element is null and
+     *                                       this list does not permit null elements
+     * @throws IllegalArgumentException      if some property of the specified
+     *                                       element prevents it from being added to this list
+     * @throws IndexOutOfBoundsException     if the index is out of range
+     *                                       ({@code index < 0 || index > size()})
+     */
+    @Override
+    public void add(int index, T element) {
+        get().add(index, element);
+    }
+
     @Override
     public List<T> get() {
         return super.get();
     }
 
+    @Override
     public int size() {
         return get().size();
     }
@@ -296,7 +494,7 @@ public class ListParameter<T extends AbstractParameter<?>> extends AbstractParam
      */
     @NotNull
     @Override
-    public Object[] toArray() {
+    public Object @NotNull [] toArray() {
         return get().toArray();
     }
 
@@ -348,14 +546,20 @@ public class ListParameter<T extends AbstractParameter<?>> extends AbstractParam
      * <p>Note that {@code toArray(new Object[0])} is identical in function to
      * {@code toArray()}.
      */
-    @NotNull
     @Override
-    public <T1> T1[] toArray(@NotNull T1[] a) {
+    public <T1> T1 @NotNull [] toArray(T1 @NotNull [] a) {
         return get().toArray(a);
     }
 
     @Override
     public String toString() {
         return get().toString();
+    }
+
+    /**
+     * create one new element and add it to the tail of the list.
+     */
+    public void addOneToEnd() {
+        add(createNewElement());
     }
 }
