607a0d225bc446768cfd77fecc1176991188471a
Dan Royer <dan@marginallyclever.com>
2023-10-11 08:52:01 -0700

    move sortAlpha to class

--- src/main/java/com/marginallyclever/robotoverlord/renderpanel/MatrixMaterialRenderSet.java
+++ src/main/java/com/marginallyclever/robotoverlord/renderpanel/MatrixMaterialRenderSet.java
@@ -8,6 +8,7 @@ import com.marginallyclever.robotoverlord.components.ShapeComponent;
 import com.marginallyclever.robotoverlord.entity.Entity;
 import com.marginallyclever.robotoverlord.systems.render.mesh.Mesh;
 
+import javax.vecmath.Vector3d;
 import java.util.*;
 import java.util.function.Consumer;
 
@@ -45,5 +46,19 @@ public class MatrixMaterialRenderSet {
             else if(mmr.materialComponent.isAlpha()) alpha.add(mmr);
             else opaque.add(mmr);
         }
+    }
+
+    public void sortAlpha(Vector3d cameraPoint) {
+        Vector3d p1 = new Vector3d();
+        Vector3d p2 = new Vector3d();
+        alpha.sort((o1, o2) -> {
+            o1.matrix.get(p1);
+            o2.matrix.get(p2);
+            p1.sub(cameraPoint);
+            p2.sub(cameraPoint);
+            double d1 = p1.lengthSquared();
+            double d2 = p2.lengthSquared();
+            return (int) Math.signum(d2 - d1);
+        });
     }
 }
