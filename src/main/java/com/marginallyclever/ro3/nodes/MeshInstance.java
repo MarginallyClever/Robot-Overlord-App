76fc56f7281d5acd905046d554d3d5f2f6451295
Dan Royer <dan@marginallyclever.com>
2023-12-14 16:43:39 -0800

    add initial panels

--- src/main/java/com/marginallyclever/ro3/nodes/MeshInstance.java
+++ src/main/java/com/marginallyclever/ro3/nodes/MeshInstance.java
@@ -1,11 +1,47 @@
 package com.marginallyclever.ro3.nodes;
 
-public class MeshInstance extends Node {
+import com.marginallyclever.convenience.helpers.MatrixHelper;
+import com.marginallyclever.robotoverlord.swing.CollapsiblePanel;
+import com.marginallyclever.robotoverlord.systems.render.mesh.Mesh;
+
+import javax.swing.*;
+import javax.vecmath.Matrix4d;
+import java.awt.*;
+import java.util.List;
+
+public class MeshInstance extends Pose {
+    private Mesh mesh;
+
     public MeshInstance() {
         super("MeshInstance");
     }
 
     public MeshInstance(String name) {
         super(name);
+    }
+
+    /**
+     * Build a Swing Component that represents this Node.
+     * @param list the list to add components to.
+     */
+    public void getComponents(List<JComponent> list) {
+        CollapsiblePanel panel = new CollapsiblePanel(MeshInstance.class.getSimpleName());
+        list.add(panel);
+        JPanel pane = panel.getContentPane();
+
+        pane.setLayout(new FlowLayout(FlowLayout.LEADING));
+        JLabel label = new JLabel("Mesh");
+        JButton button = new JButton((mesh==null) ? "..." : mesh.getSourceName());
+        label.setLabelFor(button);
+        pane.add(label);
+        pane.add(button);
+
+        button.addActionListener(e -> {
+            // TODO run MeshFactoryPanel?
+            System.out.println("TODO run MeshFactoryPanel?");
+            
+        });
+
+        super.getComponents(list);
     }
 }
--- src/main/java/com/marginallyclever/ro3/nodes/Pose.java
+++ src/main/java/com/marginallyclever/ro3/nodes/Pose.java
@@ -1,6 +1,10 @@
 package com.marginallyclever.ro3.nodes;
 
+import com.marginallyclever.robotoverlord.swing.CollapsiblePanel;
+
+import javax.swing.*;
 import javax.vecmath.Matrix4d;
+import java.util.List;
 
 public class Pose extends Node {
     private final Matrix4d local = new Matrix4d();
@@ -31,5 +35,19 @@ public class Pose extends Node {
         Matrix4d parentWorld = p.getWorld();
         parentWorld.mul(local);
         return parentWorld;
+    }
+
+    /**
+     * Build a Swing Component that represents this Node.
+     * @param list the list to add components to.
+     */
+    public void getComponents(List<JComponent> list) {
+        CollapsiblePanel panel = new CollapsiblePanel(Pose.class.getSimpleName());
+        list.add(panel);
+        JPanel pane = panel.getContentPane();
+
+        pane.add(new JLabel("Pose stuff goes here"));
+
+        super.getComponents(list);
     }
 }
