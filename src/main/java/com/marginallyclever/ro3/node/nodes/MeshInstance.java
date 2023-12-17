2d0f7ec8aa00f7728e91631dba9d4348356a3eca
Dan Royer <dan@marginallyclever.com>
2023-12-16 21:15:07 -0800

    added mesh smoothing

--- src/main/java/com/marginallyclever/ro3/node/nodes/MeshInstance.java
+++ src/main/java/com/marginallyclever/ro3/node/nodes/MeshInstance.java
@@ -2,6 +2,7 @@ package com.marginallyclever.ro3.node.nodes;
 
 import com.marginallyclever.robotoverlord.swing.CollapsiblePanel;
 import com.marginallyclever.robotoverlord.systems.render.mesh.Mesh;
+import com.marginallyclever.robotoverlord.systems.render.mesh.MeshSmoother;
 
 import javax.swing.*;
 import java.awt.*;
@@ -30,21 +31,25 @@ public class MeshInstance extends Pose {
 
         pane.setLayout(new GridLayout(0,2));
 
-        JButton button = new JButton();
-        setMeshButtonLabel(button);
-        button.addActionListener(e -> {
+        JButton select = new JButton();
+        setMeshButtonLabel(select);
+        select.addActionListener(e -> {
             MeshFactoryPanel meshFactoryPanel = new MeshFactoryPanel();
             int result = meshFactoryPanel.run();
             if(result == JFileChooser.APPROVE_OPTION) {
                 mesh = meshFactoryPanel.getMesh();
-                setMeshButtonLabel(button);
+                setMeshButtonLabel(select);
             }
         });
-        addLabelAndComponent(pane,"Mesh",button);
+        addLabelAndComponent(pane,"Mesh",select);
 
         if(mesh!=null) {
             addLabelAndComponent(pane,"Vertices",new JLabel(""+mesh.getNumVertices()));
             addLabelAndComponent(pane,"Triangles",new JLabel(""+mesh.getNumTriangles()));
+
+            JButton smooth = new JButton("Smooth");
+            smooth.addActionListener(e -> MeshSmoother.smoothNormals(mesh,0.01f,0.25f) );
+            addLabelAndComponent(pane,"Smooth",smooth);
         }
 
         super.getComponents(list);
