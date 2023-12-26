2267ef5578bae265aabbbe330aaa26f57ec34b85
Dan Royer <dan@marginallyclever.com>
2023-12-25 17:32:07 -0800

    added snapshot button

--- src/main/java/com/marginallyclever/ro3/apps/webcampanel/WebCamPanel.java
+++ src/main/java/com/marginallyclever/ro3/apps/webcampanel/WebCamPanel.java
@@ -3,11 +3,20 @@ package com.marginallyclever.ro3.apps.webcampanel;
 import com.github.sarxos.webcam.Webcam;
 import com.github.sarxos.webcam.WebcamPanel;
 import com.github.sarxos.webcam.WebcamResolution;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.image.BufferedImage;
+import java.util.Objects;
 
+/**
+ * A panel that displays the default USB web camera.
+ */
 public class WebCamPanel extends JPanel {
+    private static final Logger logger = LoggerFactory.getLogger(WebCamPanel.class);
     private final WebcamPanel panel;
 
     public WebCamPanel() {
@@ -21,6 +30,27 @@ public class WebCamPanel extends JPanel {
         panel = new WebcamPanel(webcam,false);
         panel.setDrawMode(WebcamPanel.DrawMode.FIT);  // fit, fill, or none
         panel.setFPSDisplayed(true);
+
+        var toolBar = new JToolBar();
+        toolBar.setFloatable(false);
+
+        toolBar.add(new JButton(new AbstractAction() {
+            {
+                putValue(NAME, "Snapshot");
+                putValue(SHORT_DESCRIPTION, "Snapshot");
+                putValue(SMALL_ICON, new ImageIcon(Objects.requireNonNull(getClass().getResource("icons8-screenshot-16.png"))));
+            }
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                BufferedImage img = panel.getWebcam().getImage();
+                if(img==null) return;
+                logger.info("Snapshot {}x{}",img.getWidth(),img.getHeight());
+                logger.error("Not implemented yet.");
+            }
+        }));
+
+
+        add(toolBar, BorderLayout.NORTH);
         add(panel, BorderLayout.CENTER);
         panel.start();
         panel.pause();
