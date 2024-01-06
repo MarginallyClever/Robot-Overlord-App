b6a707410a575f97cbd6df14ec4f1a41888d8e1d
Dan Royer <dan@marginallyclever.com>
2024-01-06 13:39:09 -0800

    Create package-info.java

---
+++ src/main/java/com/marginallyclever/ro3/view/package-info.java
@@ -0,0 +1,12 @@
+package com.marginallyclever.ro3.view;
+/**
+ * Provides classes and interfaces for the view rendering system.
+ * This includes the {@link com.marginallyclever.ro3.view.View} annotation for marking classes
+ * that provide views for specific data types, the {@link com.marginallyclever.ro3.view.ViewProvider}
+ * interface for defining how these views are provided, and the
+ * {@link com.marginallyclever.ro3.view.ViewFinder} class for locating and instantiating view providers
+ * based on the type of data being rendered.
+ *
+ * <p>This system allows for modular and dynamic view creation for
+ * various data types in a Swing-based application.</p>
+ */
