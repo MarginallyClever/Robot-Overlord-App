3be0f44815cafc4cdb610fc7c9a451b8477c2dd2
Dan Royer <dan@marginallyclever.com>
2024-01-08 11:26:38 -0800

    add AttachmentPoint

--- src/main/java/com/marginallyclever/ro3/node/nodes/pose/AttachmentPoint.java
+++ src/main/java/com/marginallyclever/ro3/node/nodes/pose/AttachmentPoint.java
@@ -1,5 +1,6 @@
 package com.marginallyclever.ro3.node.nodes.pose;
 
+import com.marginallyclever.convenience.helpers.MatrixHelper;
 import com.marginallyclever.ro3.Registry;
 import com.marginallyclever.ro3.node.Node;
 import com.marginallyclever.ro3.node.nodes.Pose;
@@ -7,6 +8,8 @@ import org.json.JSONObject;
 
 import javax.swing.*;
 import javax.vecmath.Matrix4d;
+import java.awt.*;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -17,6 +20,7 @@ import java.util.List;
  */
 public class AttachmentPoint extends Pose {
     private boolean isAttached = false;
+    private double radius = 1.0;
 
     public AttachmentPoint() {
         super("Attachment Point");
@@ -57,10 +61,35 @@ public class AttachmentPoint extends Pose {
         }
     }
 
+    public void attemptAttach() {
+        if(!isAttached) return;
+
+        var myPosition = MatrixHelper.getPosition(getWorld());
+        double r2 = radius*radius;
+
+        var found = new ArrayList<Pose>();
+
+        List<Node> list = Registry.getScene().getChildren();
+        for(Node n : list) {
+            if(!(n instanceof Pose p)) continue;
+            if(p==this) continue;
+            if(p.hasParent(this)) continue;
+
+            var pos = MatrixHelper.getPosition(p.getWorld());
+            pos.sub(myPosition);
+            if( pos.lengthSquared() <= r2 ) {
+                found.add(p);
+            }
+        }
+
+        attach(found);
+    }
+
     @Override
     public JSONObject toJSON() {
         var json = super.toJSON();
         json.put("isAttached",isAttached);
+        json.put("radius",radius);
         return json;
     }
 
@@ -68,10 +97,25 @@ public class AttachmentPoint extends Pose {
     public void fromJSON(JSONObject from) {
         super.fromJSON(from);
         isAttached = from.getBoolean("isAttached");
+        radius = from.getDouble("radius");
     }
 
     @Override
     public void getComponents(List<JPanel> list) {
+        JPanel pane = new JPanel(new GridLayout(0,2));
+        list.add(pane);
+        pane.setName(AttachmentPoint.class.getSimpleName());
+
+        var attached = new JCheckBox("",isAttached);
+        addLabelAndComponent(pane,"isAttached",attached);
+        attached.addActionListener(e -> {
+            isAttached = attached.isSelected();
+            if(isAttached) {
+                attemptAttach();
+            } else {
+                release();
+            }
+        });
         super.getComponents(list);
     }
 }
