0f8138c380d4db27d2332832aab30159c796003b
Dan Royer <dan@marginallyclever.com>
2024-01-13 09:11:16 -0800

    add AttachmentPointPanel

--- src/main/java/com/marginallyclever/ro3/node/nodes/pose/AttachmentPoint.java
+++ src/main/java/com/marginallyclever/ro3/node/nodes/pose/AttachmentPoint.java
@@ -1,19 +1,15 @@
 package com.marginallyclever.ro3.node.nodes.pose;
 
 import com.marginallyclever.convenience.helpers.MatrixHelper;
-import com.marginallyclever.convenience.swing.NumberFormatHelper;
 import com.marginallyclever.ro3.Registry;
 import com.marginallyclever.ro3.node.Node;
-import com.marginallyclever.ro3.node.NodePanelHelper;
 import com.marginallyclever.ro3.node.nodes.Pose;
 import org.json.JSONObject;
 
 import javax.swing.*;
 import javax.vecmath.Matrix4d;
-import java.awt.*;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 
 /**
  * </p>{@link AttachmentPoint} is a point on a {@link Pose} that can be used to attach other nodes.</p>
@@ -107,49 +103,23 @@ public class AttachmentPoint extends Pose {
 
     @Override
     public void getComponents(List<JPanel> list) {
-        JPanel pane = new JPanel(new GridLayout(0,2));
-        list.add(pane);
-        pane.setName(AttachmentPoint.class.getSimpleName());
-
-        // radius
-        var formatter = NumberFormatHelper.getNumberFormatter();
-        formatter.setMinimum(0.0);  // no negative radius
-        var radiusField = new JFormattedTextField(formatter);
-        radiusField.setValue(radius);
-        radiusField.addPropertyChangeListener("value", e -> {
-            radius = ((Number)radiusField.getValue()).doubleValue();
-        });
-        NodePanelHelper.addLabelAndComponent(pane,"Radius",radiusField);
-
-        var attached = buildAttachToggle();
-        NodePanelHelper.addLabelAndComponent(pane,"Action",attached);
-
+        list.add(new AttachmentPointPanel(this));
         super.getComponents(list);
     }
-
-    /**
-     * Build the "attach/detach" toggle button.  Public so that it can be included in the control panel of other
-     * nodes like MarlinRobotArm.
-     * @return a new JToggleButton
-     */
-    public JComponent buildAttachToggle() {
-        // attach/detach toggle
-        var attached = new JToggleButton();
-        attached.setIcon(new ImageIcon(Objects.requireNonNull(getClass().getResource(
-                "/com/marginallyclever/ro3/apps/actions/icons8-disconnect-16.png"))));
-        attached.setSelected(isAttached);  // must come before action listener.
-        setAttachedText(attached);
-
-        attached.addActionListener(e -> {
-            isAttached = attached.isSelected();
-            if(isAttached) attemptAttach();
-            else release();
-            setAttachedText(attached);
-        });
-        return attached;
+    public double getRadius() {
+        return radius;
     }
 
-    private void setAttachedText(JToggleButton attached) {
-        attached.setText(isAttached ? "Release" : "Attach");
+    public void setRadius(double radius) {
+        if(radius<0) throw new IllegalArgumentException("radius must be >=0");
+        this.radius = radius;
+    }
+
+    public boolean getIsAttached() {
+        return isAttached;
+    }
+
+    public void setIsAttached(boolean isAttached) {
+        this.isAttached = isAttached;
     }
 }
