9bcae3b1ea197a88a5153556feeb2e3b41deffc2
Dan Royer <dan@marginallyclever.com>
2024-01-19 08:34:55 -0800

    remove deprecated classes

--- src/main/java/com/marginallyclever/robotoverlord/components/OriginAdjustComponent.java
+++
@@ -1,8 +0,0 @@
-package com.marginallyclever.robotoverlord.components;
-
-/**
- * Adjusts this Entity's pose to be relative to the parent's pose.
- */
-@ComponentDependency(components={PoseComponent.class})
-@Deprecated
-public class OriginAdjustComponent extends Component {}
--- src/test/java/com/marginallyclever/ro3/TestAllPanels.java
+++ src/test/java/com/marginallyclever/ro3/TestAllPanels.java
@@ -24,6 +24,7 @@ import java.util.Set;
  */
 public class TestAllPanels {
     private static final Logger logger = LoggerFactory.getLogger(TestAllPanels.class);
+
     public static void main(String[] args) {
         Registry.start();
 
