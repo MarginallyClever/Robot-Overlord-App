96c134c8a2a93fb578aee92af3a7f862eb6bcf49
Dan Royer <dan@marginallyclever.com>
2024-01-20 10:10:07 -0800

    more coverage

--- src/test/java/com/marginallyclever/ro3/apps/viewport/OpenGLPanelTest.java
+++ src/test/java/com/marginallyclever/ro3/apps/viewport/OpenGLPanelTest.java
@@ -1,9 +1,13 @@
 package com.marginallyclever.ro3.apps.viewport;
 
 import com.marginallyclever.ro3.Registry;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.DisabledIfEnvironmentVariable;
 
+@DisabledIfEnvironmentVariable(named = "CI", matches = "true")
 public class OpenGLPanelTest {
-    public static void main(String[] args) {
+    @Test
+    public void test() {
         Registry.start();
 
         OpenGLPanel panel = new OpenGLPanel();
@@ -15,5 +19,12 @@ public class OpenGLPanelTest {
         frame.setSize(500, 600);
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
+
+        // pause for 0.5 seconds
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException ignored) {}
+
+        frame.dispose();
     }
 }
