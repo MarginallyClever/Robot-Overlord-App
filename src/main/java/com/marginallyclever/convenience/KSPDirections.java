200474b6e56df4d468785919b81661686a617be6
Dan Royer <dan@marginallyclever.com>
2024-01-20 12:24:22 -0800

    Update KSPDirections.java

--- src/main/java/com/marginallyclever/convenience/KSPDirections.java
+++ src/main/java/com/marginallyclever/convenience/KSPDirections.java
@@ -1,10 +1,28 @@
 package com.marginallyclever.convenience;
 
 public enum KSPDirections {
+    /**
+     * Prograde is the direction of travel.
+     */
     PROGRADE,
+    /**
+     * Retrograde is the opposite direction of travel.
+     */
     RETROGRADE,
+    /**
+     * Normal is the direction of the orbit's normal vector.
+     */
     NORMAL,
+    /**
+     * Antinormal is the opposite direction of the orbit's normal vector.
+     */
     ANTINORMAL,
+    /**
+     * Radial is the direction of the orbit's radial vector.
+     */
     RADIAL_IN,
+    /**
+     * Antiradial is the opposite direction of the orbit's radial vector.
+     */
     RADIAL_OUT,
 }
