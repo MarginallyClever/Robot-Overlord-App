8d65b28c71f047cf19e136e2afcb11433480cbd9
Dan Royer <dan@marginallyclever.com>
2024-01-28 16:57:33 -0800

    added panel

--- src/main/java/com/marginallyclever/ro3/node/nodes/behavior/actions/LimbMoveToTarget.java
+++ src/main/java/com/marginallyclever/ro3/node/nodes/behavior/actions/LimbMoveToTarget.java
@@ -2,17 +2,29 @@ package com.marginallyclever.ro3.node.nodes.behavior.actions;
 
 import com.marginallyclever.ro3.node.NodePath;
 import com.marginallyclever.ro3.node.nodes.behavior.Action;
+import com.marginallyclever.ro3.node.nodes.behavior.BehaviorTreeRunnerPanel;
 import com.marginallyclever.ro3.node.nodes.limbsolver.LimbSolver;
 import com.marginallyclever.ro3.node.nodes.pose.Pose;
+import org.json.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.List;
 import java.util.Objects;
 
+/**
+ * <p>{@link LimbMoveToTarget} is a {@link Action} that moves a {@link LimbSolver} target to a {@link Pose}.</p>
+ * <p>While the solver is working the action returns RUNNING.  When the target arrives at the pose, the action
+ * returns SUCCESS.</p>
+ * <p>When the action is attached to the scene tree, it looks at all parents and </p>
+ */
 public class LimbMoveToTarget extends Action implements ActionListener {
-    private NodePath<Pose> target;
-    private NodePath<LimbSolver> solver;
+    private static final Logger logger = LoggerFactory.getLogger(LimbMoveToTarget.class);
+    private final NodePath<Pose> target = new NodePath<>(this,Pose.class);
+    private final NodePath<LimbSolver> solver = new NodePath<>(this,LimbSolver.class);
     private Status result = Status.RUNNING;
 
     public LimbMoveToTarget() {
@@ -25,16 +37,26 @@ public class LimbMoveToTarget extends Action implements ActionListener {
 
     @Override
     public Status tick() {
-        var mySolver = solver.getSubject();
-        var myTarget = target.getSubject();
-        if(mySolver==null || myTarget==null) return Status.FAILURE;
+        logger.debug("tick {}",getAbsolutePath());
 
-        if(mySolver.getTarget().getSubject() != myTarget) {
-            mySolver.setTarget(myTarget);
-            mySolver.addActionListener(this);
+        if(result==Status.RUNNING) {
+            var mySolver = solver.getSubject();
+            var myTarget = target.getSubject();
+            if(mySolver==null || myTarget==null) {
+                result = Status.FAILURE;
+            } else if (mySolver.getTarget().getSubject() != myTarget) {
+                mySolver.setTarget(myTarget);
+                mySolver.addActionListener(this);
+            }
         }
 
         return result;
+    }
+
+    public void reset() {
+        super.reset();
+        logger.debug("reset {}",getAbsolutePath());
+        result = Status.RUNNING;
     }
 
     @Override
@@ -69,5 +91,42 @@ public class LimbMoveToTarget extends Action implements ActionListener {
     public Icon getIcon() {
         return new ImageIcon(Objects.requireNonNull(getClass().getResource(
                 "icons8-enter-direction-arrow-towards-rightward-orientation-pointer-16.png")));
+    }
+
+    @Override
+    public JSONObject toJSON() {
+        var json = super.toJSON();
+        json.put("target",target.getUniqueID());
+        json.put("solver",solver.getUniqueID());
+        return json;
+    }
+
+    @Override
+    public void fromJSON(JSONObject from) {
+        super.fromJSON(from);
+        target.setUniqueID(from.getString("target"));
+        solver.setUniqueID(from.getString("solver"));
+    }
+
+    @Override
+    public void getComponents(List<JPanel> list) {
+        list.add(new LimbMoveToTargetPanel(this));
+        super.getComponents(list);
+    }
+
+    public Pose getTarget() {
+        return target.getSubject();
+    }
+
+    public void setTarget(Pose subject) {
+        target.setUniqueIDByNode(subject);
+    }
+
+    public LimbSolver getSolver() {
+        return solver.getSubject();
+    }
+
+    public void setSolver(LimbSolver subject) {
+        solver.setUniqueIDByNode(subject);
     }
 }
---
+++ src/main/java/com/marginallyclever/ro3/node/nodes/behavior/actions/LimbMoveToTargetPanel.java
@@ -0,0 +1,40 @@
+package com.marginallyclever.ro3.node.nodes.behavior.actions;
+
+import com.marginallyclever.ro3.PanelHelper;
+import com.marginallyclever.ro3.apps.nodeselector.NodeSelector;
+import com.marginallyclever.ro3.node.nodes.HingeJoint;
+import com.marginallyclever.ro3.node.nodes.Motor;
+import com.marginallyclever.ro3.node.nodes.limbsolver.LimbSolver;
+import com.marginallyclever.ro3.node.nodes.pose.Pose;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class LimbMoveToTargetPanel extends JPanel {
+    public LimbMoveToTargetPanel() {
+        this(new LimbMoveToTarget());
+    }
+
+    public LimbMoveToTargetPanel(LimbMoveToTarget limbMoveToTarget) {
+        super(new GridLayout(0,2));
+        this.setName(LimbMoveToTarget.class.getSimpleName());
+
+        // target
+        {
+            NodeSelector<Pose> selector = new NodeSelector<>(Pose.class, limbMoveToTarget.getTarget());
+            selector.addPropertyChangeListener("subject", (evt) -> {
+                limbMoveToTarget.setTarget(selector.getSubject());
+            });
+            PanelHelper.addLabelAndComponent(this, "Target", selector);
+        }
+
+        // solver
+        {
+            NodeSelector<LimbSolver> selector = new NodeSelector<>(LimbSolver.class, limbMoveToTarget.getSolver());
+            selector.addPropertyChangeListener("subject", (evt) -> {
+                limbMoveToTarget.setSolver(selector.getSubject());
+            });
+            PanelHelper.addLabelAndComponent(this, "LimbSolver", selector);
+        }
+    }
+}
