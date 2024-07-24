60b7442155e4fc4a43e6f1fb4578f09d2f550007
Dan Royer <dan@marginallyclever.com>
2024-07-24 11:03:35 -0700

    Update MeshMaterial.java

--- src/main/java/com/marginallyclever/ro3/apps/viewport/renderpasses/MeshMaterial.java
+++ src/main/java/com/marginallyclever/ro3/apps/viewport/renderpasses/MeshMaterial.java
@@ -3,5 +3,9 @@ package com.marginallyclever.ro3.apps.viewport.renderpasses;
 import com.marginallyclever.ro3.node.nodes.Material;
 import com.marginallyclever.ro3.node.nodes.pose.poses.MeshInstance;
 
-public record MeshMaterial(MeshInstance meshInstance, Material material) {
-}
+/**
+ * A mesh and the material to render it with.
+ * @param meshInstance The mesh to render.
+ * @param material The material with which to render the mesh.
+ */
+public record MeshMaterial(MeshInstance meshInstance, Material material) {}
