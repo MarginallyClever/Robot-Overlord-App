1b10e299654594fe5a9ba16c2b69f6ef82e62b39
Dan Royer <dan@marginallyclever.com>
2024-09-25 12:47:39 -0700

    Create MarlinRobot.java

---
+++ src/main/java/com/marginallyclever/ro3/node/nodes/marlinrobot/MarlinRobot.java
@@ -0,0 +1,50 @@
+package com.marginallyclever.ro3.node.nodes.marlinrobot;
+
+import com.marginallyclever.ro3.node.Node;
+import com.marginallyclever.ro3.node.nodes.marlinrobot.marlinrobotarm.MarlinRobotArmPanel;
+
+import javax.swing.*;
+import java.util.List;
+
+/**
+ * {@link MarlinRobot} represents a robot with Marlin firmware installed.
+ */
+public class MarlinRobot extends Node {
+    public MarlinRobot() {
+        this("Marlin Robot");
+    }
+
+    public MarlinRobot(String name) {
+        super(name);
+    }
+
+    @Override
+    public void getComponents(List<JPanel> list) {
+        list.add(new MarlinRobotPanel(this));
+        super.getComponents(list);
+    }
+
+    public void addMarlinListener(MarlinListener editorPanel) {
+        listeners.add(MarlinListener.class,editorPanel);
+    }
+
+    public void removeMarlinListener(MarlinListener editorPanel) {
+        listeners.remove(MarlinListener.class,editorPanel);
+    }
+
+    protected void fireMarlinMessage(String message) {
+        //logger.info(message);
+        for(MarlinListener listener : listeners.getListeners(MarlinListener.class)) {
+            listener.messageFromMarlin(message);
+        }
+    }
+
+    /**
+     * <p>Send a single gcode command to the marlin robot.  It will reply by firing a
+     * {@link MarlinListener#messageFromMarlin} event with the String response.</p>
+     * @param gcode GCode command
+     */
+    public void sendGCode(String gcode) {
+        fireMarlinMessage( "Error: unknown command "+gcode );
+    }
+}
