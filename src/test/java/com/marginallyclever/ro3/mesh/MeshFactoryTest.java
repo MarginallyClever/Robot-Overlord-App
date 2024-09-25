639453b9b098e58d357f115fe18c0a1c18f58122
Dan Royer <dan@marginallyclever.com>
2024-09-25 08:37:17 -0700

    Update MeshFactoryTest.java

--- src/test/java/com/marginallyclever/ro3/mesh/MeshFactoryTest.java
+++ src/test/java/com/marginallyclever/ro3/mesh/MeshFactoryTest.java
@@ -3,15 +3,16 @@ package com.marginallyclever.ro3.mesh;
 import org.junit.jupiter.api.Test;
 
 public class MeshFactoryTest {
+    public static final String path = "src/test/resources/com/marginallyclever/ro3/apps/node/nodes/marlinrobotarm/j0.obj";
     @Test
     public void test() {
-        var path = "src/test/resources/com/marginallyclever/ro3/apps/node/nodes/marlinrobotarm/j0.obj";
         MeshFactory factory = new MeshFactory();
         assert(factory.getPool().getList().isEmpty());
         assert(factory.getAllSourcesForExport().isEmpty());
         assert(factory.canLoad(path));
         var a = factory.load(path);
         assert(!factory.getPool().getList().isEmpty());
+        assert(a.getNumVertices()>0);
         var b = factory.load(path);
         assert(a==b);
         assert(!factory.getAllSourcesForExport().isEmpty());
