dea5a680c59f621f0b9aad46c6b9e51fb06fabb1
Dan Royer <dan@marginallyclever.com>
2024-09-26 13:07:24 -0700

    Update package-info.java

--- src/main/java/com/marginallyclever/communications/package-info.java
+++ src/main/java/com/marginallyclever/communications/package-info.java
@@ -1,8 +1,7 @@
-package com.marginallyclever.communications;
 /**
  * <p>This communication system follows the OSI model.</p>
  * <p>{@link com.marginallyclever.communications.application.ChooseConnectionPanel} is the basic "open/close" panel.</p>
- * <p>{@link com.marginallyclever.communications.application.TextInterfaceWithHistory} is a chat style interface with
+ * <p>{@link com.marginallyclever.communications.application.TextInterfaceToSessionLayer} is a chat style interface with
  * a history of bi-directional communication.</p>
- *
  */
+package com.marginallyclever.communications;
