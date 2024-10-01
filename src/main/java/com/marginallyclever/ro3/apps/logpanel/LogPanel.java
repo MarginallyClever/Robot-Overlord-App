f56a86ec8465c092f474f2e5c4b31a3d5505c6f4
Dan Royer <dan@marginallyclever.com>
2024-10-01 09:34:25 -0700

    cleaning

--- src/main/java/com/marginallyclever/ro3/apps/logpanel/LogPanel.java
+++ src/main/java/com/marginallyclever/ro3/apps/logpanel/LogPanel.java
@@ -2,21 +2,11 @@ package com.marginallyclever.ro3.apps.logpanel;
 
 import ch.qos.logback.classic.Logger;
 import ch.qos.logback.classic.LoggerContext;
-import ch.qos.logback.classic.spi.ILoggingEvent;
-import ch.qos.logback.core.Appender;
-import ch.qos.logback.core.FileAppender;
 import com.marginallyclever.ro3.apps.App;
-import com.marginallyclever.ro3.apps.DockingPanel;
 import org.slf4j.LoggerFactory;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.List;
-import java.io.File;
-import java.io.IOException;
-import java.lang.module.Configuration;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.*;
 
 /**
