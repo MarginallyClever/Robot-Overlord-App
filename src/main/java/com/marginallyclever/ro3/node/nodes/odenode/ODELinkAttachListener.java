58891b43f55443f61fb68bd33e9f51781ef281f2
Dan Royer <dan@marginallyclever.com>
2024-11-05 13:30:35 -0800

    comments

--- src/main/java/com/marginallyclever/ro3/node/nodes/odenode/ODELinkAttachListener.java
+++ src/main/java/com/marginallyclever/ro3/node/nodes/odenode/ODELinkAttachListener.java
@@ -3,7 +3,8 @@ package com.marginallyclever.ro3.node.nodes.odenode;
 import java.util.EventListener;
 
 /**
- * Listeners are notified when an ODENode is attached to the scene.
+ * Listeners are notified when an {@link ODENode} is attached to the scene.
+ * This is not the same as when {@link com.marginallyclever.ro3.node.NodeAttachListener} fires on the parent because the child is attached.
  */
 public interface ODELinkAttachListener extends EventListener {
     void linkAttached(ODENode node);
