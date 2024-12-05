d193a861b6fa91038164f0dda4f1818a797bd4cc
Dan Royer <dan@marginallyclever.com>
2024-12-05 14:18:56 -0800

    BrainPanel now displays a list of inputs and outputs

--- src/main/java/com/marginallyclever/ro3/node/nodes/neuralnetwork/BrainPanel.java
+++ src/main/java/com/marginallyclever/ro3/node/nodes/neuralnetwork/BrainPanel.java
@@ -1,8 +1,14 @@
 package com.marginallyclever.ro3.node.nodes.neuralnetwork;
 
+import com.marginallyclever.ro3.PanelHelper;
 import com.marginallyclever.ro3.apps.neuralnetworkview.BrainView;
+import com.marginallyclever.ro3.apps.nodedetailview.CollapsiblePanel;
+import com.marginallyclever.ro3.apps.nodeselector.NodeSelector;
+import com.marginallyclever.ro3.node.NodePath;
 
 import javax.swing.*;
+import java.util.List;
+import java.awt.*;
 
 /**
  * {@link BrainPanel} controls settings for a {@link Brain}.  This is different from a
@@ -10,12 +16,38 @@ import javax.swing.*;
  * contents.
  */
 public class BrainPanel extends JPanel {
+    private final Brain brain;
+
     public BrainPanel() {
         this(new Brain());
     }
 
     public BrainPanel(Brain brain) {
-        super();
+        super(new GridLayout(0,1));
+        this.brain = brain;
         setName("Brain");
+
+        addNeuronsPanel("Input",brain.inputs.getList());
+        addNeuronsPanel("Output",brain.outputs.getList());
+    }
+
+    private void addNeuronsPanel(String label,List<NodePath<Neuron>> list) {
+        var containerPanel = new CollapsiblePanel(label+" Neurons");
+        var outerPanel = containerPanel.getContentPane();
+        outerPanel.setLayout(new GridLayout(0, 1));
+
+        int i=0;
+        for (NodePath<Neuron> neuron : list) {
+            // add a selector
+            var motorSelector = new NodeSelector<>(Neuron.class, neuron.getSubject());
+            int jFinal = i;
+            motorSelector.addPropertyChangeListener("subject",(e)-> {
+                brain.inputs.getList().get(jFinal).setUniqueIDByNode((Neuron)e.getNewValue());
+            });
+            PanelHelper.addLabelAndComponent(outerPanel, ""+i, motorSelector);
+            i++;
+        }
+
+        this.add(containerPanel);
     }
 }
