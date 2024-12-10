41d3944a35c66a4ed3c6cbb4bfc60dda0c0e92b8
Dan Royer <dan@marginallyclever.com>
2024-12-10 10:34:35 -0800

    better localization

--- src/main/java/com/marginallyclever/convenience/swing/NumberFormatHelper.java
+++ src/main/java/com/marginallyclever/convenience/swing/NumberFormatHelper.java
@@ -7,16 +7,16 @@ public class NumberFormatHelper {
     static public NumberFormatter getNumberFormatterDouble() {
         NumberFormat format = NumberFormat.getNumberInstance();
         NumberFormatter formatter = new NumberFormatter(format);
-        formatter.setValueClass(Double.class);
+        //formatter.setValueClass(Double.class);
         formatter.setAllowsInvalid(true);
         formatter.setCommitsOnValidEdit(true);
         return formatter;
     }
 
     static public NumberFormatter getNumberFormatterInt() {
-        NumberFormat format = NumberFormat.getNumberInstance();
+        NumberFormat format = NumberFormat.getIntegerInstance();
         NumberFormatter formatter = new NumberFormatter(format);
-        formatter.setValueClass(Integer.class);
+        //formatter.setValueClass(Integer.class);
         formatter.setAllowsInvalid(true);
         formatter.setCommitsOnValidEdit(true);
         return formatter;
