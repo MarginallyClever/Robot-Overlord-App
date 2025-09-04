d93b20bc6708a6f035b03b1344ef07de5dc69310
Dan Royer <dan@marginallyclever.com>
2025-09-04 09:12:52 -0700

    use Shader

--- src/main/java/com/marginallyclever/ro3/apps/viewport/ShaderProgramFactory.java
+++ src/main/java/com/marginallyclever/ro3/apps/viewport/ShaderProgramFactory.java
@@ -1,7 +1,9 @@
 package com.marginallyclever.ro3.apps.viewport;
 
 import com.jogamp.opengl.GL3;
+import com.marginallyclever.ro3.factories.Resource;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -10,16 +12,38 @@ import java.util.Map;
  * destroy them when the OpenGL context goes away.
  */
 public class ShaderProgramFactory {
-    private final Map<String,ShaderProgram> shaderPrograms = new HashMap<>();
+    private final Map<String, Resource<Shader>> shaders = new HashMap<>();
+    private final Map<String, Resource<ShaderProgram>> shaderPrograms = new HashMap<>();
 
-    public ShaderProgram createShaderProgram(GL3 gl, String type) {
-        throw new RuntimeException("Not implemented");
+    public Shader createShader(int type, String[] shaderCode) {
+        String key = type+"-"+ Arrays.hashCode(shaderCode);
+        if( shaders.containsKey(key) ) return shaders.get(key);
+        // not in pool, create it
+        Shader shader = new Shader(type,shaderCode,key);
+        shaders.put(key,shader);
+        return shader;
+    }
+
+    public ShaderProgram createShaderProgram(String name, Shader... shaders) {
+        if( shaderPrograms.containsKey(name) ) return shaderPrograms.get(name);
+        // not in pool, create it
+        ShaderProgram sp = new ShaderProgram(Arrays.stream(shaders).toList());
+        shaderPrograms.put(name,sp);
+        return sp;
     }
 
     /**
      * Deletes (but does not clear) all the shader programs.
      */
-    public void deleteAll() {
+    public void unloadAll(GL3 gl3) {
+        for( ShaderProgram sp : shaderPrograms.values() ) {
+            sp.unload(gl3);
+        }
+        shaderPrograms.clear();
+    }
 
+    public void reset() {
+        shaders.clear();
+        shaderPrograms.clear();
     }
 }
