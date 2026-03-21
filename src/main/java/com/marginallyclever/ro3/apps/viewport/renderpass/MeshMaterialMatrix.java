30aefba26fb27f6c1519958e90d830d147bbd452
Dan Royer <dan@marginallyclever.com>
2026-03-21 13:30:40 -0700

    use MeshProvider

--- src/main/java/com/marginallyclever/ro3/apps/viewport/renderpass/MeshMaterialMatrix.java
+++ src/main/java/com/marginallyclever/ro3/apps/viewport/renderpass/MeshMaterialMatrix.java
@@ -1,14 +1,14 @@
 package com.marginallyclever.ro3.apps.viewport.renderpass;
 
 import com.marginallyclever.ro3.node.nodes.Material;
-import com.marginallyclever.ro3.node.nodes.pose.poses.MeshInstance;
+import com.marginallyclever.ro3.node.nodes.pose.poses.MeshProvider;
 
 import javax.vecmath.Matrix4d;
 
 /**
  * A mesh and the material to render it with.
- * @param meshInstance The mesh to render.
+ * @param meshProvider The mesh provider to render.
  * @param material The material with which to render the mesh.
  * @param matrix The transformation matrix to apply to the mesh.
  */
-public record MeshMaterialMatrix(MeshInstance meshInstance, Material material, Matrix4d matrix) {}
+public record MeshMaterialMatrix(MeshProvider meshProvider, Material material, Matrix4d matrix) {}
